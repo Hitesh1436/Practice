b3e3e070b9c96a04589a31676c0af2caa7434bd0
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-04-17 12:07:26 +0530

    Time: 2 ms (92.26%), Space: 42.1 MB (97.38%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.java
+++ 695-max-area-of-island/695-max-area-of-island.java
@@ -1,31 +1,32 @@
 class Solution {
-    public int garea;
-    public void getarea(int[][] grid,int row,int col,int count){
-        if(row<0 || row>=grid.length || col<0 || col>=grid[0].length || grid[row][col]!=1 ){
-            return;
+    public int area;
+    public void getArea(int [][]grid , int i,int j){
+        if(i>=0 && j>=0 && i<grid.length && j<grid[0].length && grid[i][j] ==1){
+            grid[i][j] = 0; // mark krdia humne 0 
+                
+                area++;
+            getArea(grid,i+1,j);
+            getArea(grid,i-1,j);
+            getArea(grid,i,j+1);
+            getArea(grid,i,j-1);
         }
-        grid[row][col]=0;
-        garea+=1;
-        getarea(grid,row+1,col,count+1);
-        getarea(grid,row-1,col,count+1);
-        getarea(grid,row,col-1,count+1);
-        getarea(grid,row,col+1,count+1);
-
     }
+    
     public int maxAreaOfIsland(int[][] grid) {
-       int area=0;
+        int n = grid.length;
+        int m = grid[0].length;
         
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                int val=grid[i][j];
-                if(val==1){
-                    int count=0;
-                    garea=0;
-                    getarea(grid,i,j,count);
-                    area=Math.max(area,garea);
+        int ans = 0;
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j] == 1){
+                    area=0;
+                    getArea(grid,i,j);
+                    ans = Math.max(ans,area);
                 }
             }
         }
-        return area;
+        return ans;
     }
 }
