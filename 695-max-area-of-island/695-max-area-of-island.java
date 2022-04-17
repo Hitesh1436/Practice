37cc2be3aa4f9cde92134c930e3998dce2e00ff0
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-04-17 11:12:42 +0530

    Time: 2 ms (92.26%), Space: 42.4 MB (89.38%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.java
+++ 695-max-area-of-island/695-max-area-of-island.java
@@ -1,21 +1,31 @@
 class Solution {
-public int maxAreaOfIsland(int[][] grid) {
-		int max = 0;
-		for (int i = 0; i < grid.length; i++) {
-			for (int j = 0; j < grid[i].length; j++) {
-				if (grid[i][j] != 0) {
-					max = Math.max(max, helper(grid, i, j));
-				}
-			}
-		}
-		return max;
-	}
+    public int garea;
+    public void getarea(int[][] grid,int row,int col,int count){
+        if(row<0 || row>=grid.length || col<0 || col>=grid[0].length || grid[row][col]!=1 ){
+            return;
+        }
+        grid[row][col]=0;
+        garea+=1;
+        getarea(grid,row+1,col,count+1);
+        getarea(grid,row-1,col,count+1);
+        getarea(grid,row,col-1,count+1);
+        getarea(grid,row,col+1,count+1);
 
-	private int helper(int[][] grid, int i, int j) {
-		if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == 0) {
-			return 0;
-		}
-		grid[i][j] = 0;
-		return 1+helper(grid, i - 1, j) + helper(grid, i + 1, j) + helper(grid, i, j - 1) + helper(grid, i, j + 1);
-	}
+    }
+    public int maxAreaOfIsland(int[][] grid) {
+       int area=0;
+        
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                int val=grid[i][j];
+                if(val==1){
+                    int count=0;
+                    garea=0;
+                    getarea(grid,i,j,count);
+                    area=Math.max(area,garea);
+                }
+            }
+        }
+        return area;
+    }
 }
