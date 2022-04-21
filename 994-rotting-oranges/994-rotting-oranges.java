26aca786c1fb11c74de7a148f2c8b65e24e638f3
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-04-21 23:31:02 +0530

    Time: 3 ms (66.10%), Space: 43.4 MB (27.46%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.java
+++ 994-rotting-oranges/994-rotting-oranges.java
@@ -1,59 +1,50 @@
 class Solution {
-    public int orangesRotting(int[][] mat) {
-        
-		Queue<int[]> q = new LinkedList<>();  //queue of arr to store {i,q} position of roton orange
-       
-	   int freshCount = 0;     //to count all fresh oranges
-        int m = mat.length;    //rows count to run loop 
-        int n = mat[0].length; //cols count to run loop
-        
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){    //iterate over matrix
-               if(mat[i][j]==1){
-                    freshCount++;   //count all fresh oranges present
-                }
-                else if(mat[i][j]==2){
-                    q.add(new int[] {i,j});  //add {i,q} position of rooten oranges in queue
-                }
+    public int orangesRotting(int[][] grid) {
+
+        if(grid == null || grid.length == 0)
+            return 0;
+
+        int numOfMinutes = 0;
+        int countFreshOranges = 0;
+        Queue<int[]> queue = new LinkedList<>();
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if(grid[i][j] == 1)
+                    countFreshOranges++;
+                else if (grid[i][j] == 2)
+                    queue.offer(new int [] {i,j});
             }
         }
-        
-        int minutes = 0;  //minutes which we have to return if all oranges rotten
-        
-        int[][] dirs = {     //Matrix of coordinate to which rotten orange can spread rotteness
-            { 0, 1}, //right
-            { 0,-1}, //left
-            {-1, 0}, //up
-            { 1, 0}  //down
-        };
-        
-        while(!q.isEmpty()){  //if queue is not empty rotten oranges is present in queue
-            int size = q.size();  //size count to runn a loop to chech adjecent of each rotten orange present in queue
-            for(int i=0;i<size;i++){  //check for adjecent fresh oranges of rotten orange
-                int[] curr = q.poll();  //remove rot orange from queue & consider as current orange
-                for(int[] arr: dirs){  //for each arr in dirs mat
-                    int x = curr[0]+arr[0];  //add possible rotten coordinate to curr cordinate
-                    int y = curr[1]+arr[1];  //for checking all direction of rotten orange
-                    
-                    if(x<0 || y<0 || x>=m || y>=n || mat[x][y]==2 || mat[x][y]==0){  
-                        continue;  //skip outOfBound & already rotten & empty cell
-                    }
-                    else{  //if fresh orange found in adjecent of rotten orange
-                        mat[x][y] = 2; //make it rotten
-                        freshCount--;  //reduce the no of fresh count
-                        q.add(new int[] {x,y}); //add this rotten orange in queue
-                    }
+
+        if(countFreshOranges == 0)
+            return 0;
+
+        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+
+            if(countFreshOranges == 0)
+                return numOfMinutes;
+
+            for(int i = 0 ; i < size ; i++) {
+                int[] point = queue.poll();
+
+                for(int dir[] : dirs) {
+                    int newX = point[0] + dir[0];
+                    int newY = point[1] + dir[1];
+
+                    if(newX < 0 || newY < 0 || newX >= grid.length || newY >= grid[0].length || grid[newX][newY] != 1)
+                        continue;
+
+                    grid[newX][newY] = 2;
+                    queue.offer(new int[]{newX , newY});
+                    countFreshOranges--;
                 }
             }
-            
-            if(!q.isEmpty()){  //if fresh oranges rot and added to queue means minutes++
-                minutes++;
-            }
+            numOfMinutes++;
         }
-        
-        if(freshCount==0){ //if no fresh orange remains return minutes
-            return minutes;
-        }
-        return -1; // else return -1 if some fresh oranges remain
+        return -1;
     }
 }
