45c7bc1f4afcab9b9a9b98f6a039f205bf08e494
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-04-22 09:31:56 +0530

    Time: 9 ms (10.70%), Space: 54.6 MB (5.44%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.java
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -1,39 +1,66 @@
 class Solution {
-    public boolean isBipartite(int[][] graph) {
-        int arr[]=new int[graph.length];
-        for(int i=0;i<arr.length;i++)
-        {arr[i]=-1;}
-        for(int i=0;i<graph.length;i++)
-        {
-            if(arr[i]==-1)
-            {
-                if(!helper(graph,i,arr))
-                {
-                    return false;
-                }
-            }
+    
+     /*
+   -1  ->unvis
+    1  ->blue
+    0  ->red
+    
+    */
+    
+    public class pair{
+        int vertex;
+        int color;
+        pair(int vertex,int color){
+            this.vertex = vertex;
+            this.color=color;
         }
-        return true;
     }
-    public boolean helper(int [][]graph,int k,int []arr)
-    {
-        Queue<Integer>q=new ArrayDeque<>();
-        q.add(k);
-        arr[k]=1;
-        while(!q.isEmpty())
-        {
-           int c=q.poll();
-            for(int i=0;i<graph[c].length;i++)
-            {
-                if(arr[graph[c][i]]==-1)
-                {
-                    arr[graph[c][i]]=1-arr[c];
-                    q.add(graph[c][i]);
-                }
-               if(arr[c]==arr[graph[c][i]])
-                    {
-                        return false;
+    
+    public boolean isBipartite(int[][] graph) {
+        int n = graph.length;
+        int [] vis = new int[n];
+        Arrays.fill(vis,-1); // initially bhrdi vis array -1 se mns unvisited hn sab
+        
+        
+        // graph components mn hoskts h toh sbhi components ko dkha pdega 
+        
+        for(int v=0;v<n;v++){
+            if(vis[v]==-1){
+                // unvisited hn toh 
+                // BFS mtlb Remove Mark/Work and Add
+                
+                Queue<pair> qu = new LinkedList<>();
+                // seeding 
+                qu.add(new pair(v,0));
+                
+                
+                while(qu.size()>0){
+                    int size = qu.size();
+                    while(size-- >0){
+                        // remove
+                        pair rem = qu.remove();
+                        
+                        if(vis[rem.vertex]!=-1){
+                            // visited
+                            if(vis[rem.vertex]== rem.color){
+                                // mtlb agr remove vale ka color and iska color same h                                    toh koi dikkt ni hogi
+                                continue;
+                            }else{
+                                return false;
+                            }
+                        }
+                        // agr vis ni h toh ab mark kro
+                        vis[rem.vertex] = rem.color;
+                        int src = rem.vertex;
+                        for(int e : graph[src]){
+                            if(vis[e]==-1){
+                                // add krao
+                                qu.add(new pair(e,1-rem.color));
+                                // 0 dega mtlb red 1 toh blue 
+                            }
+                        }
                     }
+                }
             }
         }
         return true;
