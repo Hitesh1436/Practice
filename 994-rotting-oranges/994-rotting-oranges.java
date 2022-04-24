9ee15eba33616cfc131d744a8432c5130b09fa33
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-04-24 15:58:55 +0530

    Time: 3 ms (66.02%), Space: 43.8 MB (10.53%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.java
+++ 994-rotting-oranges/994-rotting-oranges.java
@@ -1,50 +1,76 @@
 class Solution {
-    public int orangesRotting(int[][] grid) {
-
-        if(grid == null || grid.length == 0)
-            return 0;
-
-        int numOfMinutes = 0;
-        int countFreshOranges = 0;
-        Queue<int[]> queue = new LinkedList<>();
-
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if(grid[i][j] == 1)
-                    countFreshOranges++;
-                else if (grid[i][j] == 2)
-                    queue.offer(new int [] {i,j});
-            }
+    
+    public class pair{
+        int i;
+        int j;
+        pair(int x,int y){
+            i=x;
+            j=y;
         }
-
-        if(countFreshOranges == 0)
-            return 0;
-
-        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
-
-        while(!queue.isEmpty()) {
-            int size = queue.size();
-
-            if(countFreshOranges == 0)
-                return numOfMinutes;
-
-            for(int i = 0 ; i < size ; i++) {
-                int[] point = queue.poll();
-
-                for(int dir[] : dirs) {
-                    int newX = point[0] + dir[0];
-                    int newY = point[1] + dir[1];
-
-                    if(newX < 0 || newY < 0 || newX >= grid.length || newY >= grid[0].length || grid[newX][newY] != 1)
-                        continue;
-
-                    grid[newX][newY] = 2;
-                    queue.offer(new int[]{newX , newY});
-                    countFreshOranges--;
+    }
+    int freshOrange=0;
+    static int [][]dir = {{1,0},{0,1},{0,-1},{-1,0}};  // for all 4 directions
+    public int seed(Queue<pair>qu, int[][]grid){
+        int n = grid.length;
+        int m = grid[0].length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==2){
+                    // rotten orange h 
+                    qu.add(new pair(i,j));
+                }else if(grid[i][j]==1){
+                    // freshOrange h
+                    freshOrange++;
                 }
             }
-            numOfMinutes++;
         }
-        return -1;
+        return freshOrange;
     }
+    
+    public int orangesRotting(int[][] grid) {
+        Queue<pair> qu = new LinkedList<>();
+        freshOrange=seed(qu,grid);   // seeding krai h bas alag funt likh dia clean dikhe
+        
+        if(freshOrange==0){
+            return 0;
+        }
+        
+        int time =0;
+         int n = grid.length;
+         int m = grid[0].length;
+        while(qu.size()!=0){
+            int size = qu.size();
+            
+            
+            while(size-- > 0){
+                // remove mark work add...
+                pair rem = qu.remove();      
+                int i = rem.i;
+                int j= rem.j;
+                
+                if(grid[i][j]==1){
+                    grid[i][j]=2;
+                    freshOrange--;
+                }
+                if(freshOrange==0){
+                    return time;
+                }
+                
+                // add ke liye 
+                for(int []d : dir){
+                    int r = i+ d[0];
+                    int c = j+ d[1];
+                    
+                    if(r>=0 && r<n && c>=0 && c<m && grid[r][c]==1){
+                        qu.add(new pair(r,c));
+                    }
+                }
+                
+            }
+            time ++;
+     
+        }
+        return freshOrange !=0 ? -1:time;
+    }
+    
 }
