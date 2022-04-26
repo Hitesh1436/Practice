f57afd17d704eee90b2334eab8c416522458e881
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-04-26 20:42:01 +0530

    Time: 118 ms (69.53%), Space: 191.9 MB (52.41%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,51 +1,37 @@
 class Solution {
-    static public class Edge{
-        int src;
-        int nbr;
-        Edge(int src,int nbr){
-            this.src=src;
-            this.nbr=nbr;
-        }
-    }
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        ArrayList<Edge>[] graph=new ArrayList[n];
+        if(edges.length == 0)
+            return true;
+        
+        List<List<Integer>> adj = new ArrayList<>();
         
         for(int i=0;i<n;i++){
-            graph[i]=new ArrayList<Edge>();
+            adj.add(new ArrayList<Integer>());
         }
         
-        for(int j=0;j<edges.length;j++){
-            int v1=edges[j][0];
-            int v2=edges[j][1];
-            
-            Edge e1=new Edge(v1,v2);
-            Edge e2=new Edge(v2,v1);
-            
-            graph[v1].add(e1);
-            graph[v2].add(e2);
+        for(int[] edge : edges){
+            adj.get(edge[0]).add(edge[1]);
+            adj.get(edge[1]).add(edge[0]);
         }
         
-        boolean visited[]=new boolean[n];
-        boolean hasp=HasPath(graph,visited,source,destination);
-        return hasp;
-    }
-    
-    
-       public static boolean HasPath(ArrayList<Edge>[] graph,boolean v[],int src,int dest){
-       if(src==dest){
-          return true;
-       }
-
-       v[src]=true;
-
-       for(Edge e:graph[src]){
-          if(v[e.nbr]==false){
-             boolean hp=HasPath(graph,v,e.nbr,dest);
-             if(hp==true){
-                return true;
-             }
-          }
-       }
-       return false;
+        Queue<Integer> q = new LinkedList<>();
+        q.add(source);
+        
+        boolean[] visited = new boolean[n];
+        
+        while(!q.isEmpty()){
+            int node = q.remove();
+            visited[node] = true;
+            
+            for(Integer l : adj.get(node)){
+                if(l == destination)
+                    return true;
+                if(!visited[l])
+                    q.add(l);
+            }
+            
+        }
+        
+        return false;
     }
 }
