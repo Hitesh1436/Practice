5610dc024dcf640a4ec9f490aace7e2c89a6864b
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-04-29 10:35:03 +0530

    Time: 8 ms (34.09%), Space: 39.1 MB (81.65%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -2,9 +2,9 @@ class Solution {
     public int fib(int n) {
         if(n<=1){
             return n;
+        }else{
+            return fib(n-1) + fib(n-2);
         }
-        int recAns = fib(n-1) + fib(n-2);
-        int myAns = recAns;
-        return recAns;
+       
     }
 }
