43b0cdf149bcbc653817f608823327ef08a578b7
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-04-29 10:42:08 +0530

    Time: 0 ms (100.00%), Space: 39.1 MB (81.65%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,17 +1,17 @@
 class Solution {
-     public int fib(int N) {
-        
-        int first = 0;
-        int second = 1;
-        
-        if(N < 2) return N;
-        
-        for(int i = 2; i < N ; i++){
-            int temp = first;
-            first = second;
-            second = temp + second;
+    public int fib(int n) {
+        if(n<=1){
+            return n;
         }
-        
-        return first + second;
+        // dp se try kro
+        int []dp = new int[n+1];
+        dp[0]=0;
+        dp[1] =1;
+        for(int i=2;i<=n;i++){
+            dp[i]= dp[i-1]+ dp[i-2];
+            
+        }
+
+    return dp[n];
     }
 }
