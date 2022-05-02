8704027e094c581483395b444e4c6cf65e3e5545
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-05-02 17:22:45 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (75.58%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -14,30 +14,16 @@
  * }
  */
 class Solution {
-    
-	List<Integer> list = new ArrayList<Integer>();
-
-	public boolean isValidBST(TreeNode root) {
-		inOrder(root);
-		return isSortedArray();
-	}
-
-	private boolean isSortedArray() {
-		for (int i = 0; i < list.size() - 1; i++) {
-			if (list.get(i) >= list.get(i + 1)) {
-				return false;
-			}
-		}
+    public TreeNode prev = null;
+    public boolean isValidBST(TreeNode root) {
+        if(root == null) return true;
+        if(!isValidBST(root.left))return false;
         
-		return true;
-	}
-
-	private void inOrder(TreeNode root) {
-		if (root == null)
-			return;
-		inOrder(root.left);
-		list.add(root.val);
-		inOrder(root.right);
-	}
-
+        if(prev!=null && prev.val>=root.val)return false;
+        prev = root;
+        
+        if(!isValidBST(root.right))return false;
+        
+        return true;
+    }
 }
