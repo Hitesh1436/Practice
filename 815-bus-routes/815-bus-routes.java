a69b2f7df3fd2ab2bbdfe5519eba1f046c96ce19
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-05-05 11:51:03 +0530

    Time: 139 ms (24.00%), Space: 195.8 MB (5.07%) - LeetHub

--- 815-bus-routes/815-bus-routes.java
+++ 815-bus-routes/815-bus-routes.java
@@ -1,35 +1,34 @@
 class Solution {
-    // BFS start with bus first
-    public int numBusesToDestination(int[][] routes, int S, int T) {
-        if (routes == null || routes.length == 0) return -1;
-        if (S == T) return 0;
-        Map<Integer, List<Integer>> map = new HashMap<>(); // stop -> {list bus pass through this stops}
-        for (int i = 0; i < routes.length; i++) {
-            for (int stop : routes[i]) {
-                map.computeIfAbsent(stop, v -> new ArrayList<>()).add(i);
+    public int numBusesToDestination(int[][] routes, int source, int target) {
+        ArrayList<Integer>[] graph = new ArrayList[1000000];
+        for(int i = 0 ; i < routes.length ; i++){
+            int[] arr = routes[i];
+            for(int e : arr){
+                if(graph[e] == null) graph[e] = new ArrayList<>();
+                graph[e].add(i);
             }
         }
-        Queue<Integer> queue = new LinkedList<>();      // bus queue
-        for (int bus : map.get(S)) queue.offer(bus);
-        Set<Integer> visitedBus = new HashSet<>();
-        Set<Integer> visitedStop = new HashSet<>();
-        visitedStop.add(S);
-        int count = 0;
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                int bus = queue.poll();
-                if (!visitedBus.add(bus)) continue;
-                for (int stop : routes[bus]) {
-                    if (stop == T) return count+1;
-                    if (!visitedStop.add(stop)) continue;
-                    for (int nextBus : map.get(stop)) 
-                        if (!visitedBus.contains(nextBus)) {
-                            queue.offer(nextBus);
-                        }
+        
+        boolean[] vis = new boolean[1000000];
+        boolean[] bus_vis = new boolean[routes.length];
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{source, 0});
+        while(!q.isEmpty()){
+            int[] e = q.remove();
+            int s = e[0];
+            int lev = e[1];
+            if(s == target) return lev;
+            vis[s] = true;
+            for(int bus : graph[s]){
+                if(bus_vis[bus]) continue;
+                
+                for(int x : routes[bus]){
+                    if(!vis[x]){
+                        q.add(new int[]{x, lev + 1});
+                    }
                 }
+                bus_vis[bus] = true;
             }
-            count++;
         }
         return -1;
     }
