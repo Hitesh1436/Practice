49a0998523df7d15df579e2750a62e49de6e4b3c
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-05-07 11:39:39 +0530

    Time: 278 ms (5.12%), Space: 120.1 MB (5.12%) - LeetHub

---
+++ 2241-design-an-atm-machine/2241-design-an-atm-machine.java
@@ -0,0 +1,40 @@
+class ATM {
+  private final long[] money;
+  private final int[] coin;
+ 
+  public ATM() {
+    money = new long[5];
+    coin = new int[] {20, 50, 100, 200, 500};
+  }
+ 
+  public void deposit(int[] banknotesCount) {
+    for (int i = 0; i < 5; i++) {
+      money[i] += banknotesCount[i];
+    }
+  }
+ 
+  public int[] withdraw(int amount) {
+    int[] result = new int[5];
+    for (int i = 4; i >= 0; i--) {
+      // choose larger coin at best we can
+      int k = (int) Math.min(money[i], amount / coin[i]);
+      result[i] = k;
+      amount -= k * coin[i];
+    }
+    if (amount != 0) {
+      return new int[] {-1};
+    }
+    for (int i = 0; i < 5; i++) {
+      money[i] -= result[i];
+    }
+    return result;
+  }
+}
+ 
+
+/**
+ * Your ATM object will be instantiated and called as such:
+ * ATM obj = new ATM();
+ * obj.deposit(banknotesCount);
+ * int[] param_2 = obj.withdraw(amount);
+ */
