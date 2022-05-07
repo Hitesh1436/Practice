74f154578d094b02438e8ecdeaff2bc741c625e7
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-05-07 11:58:20 +0530

    Time: 125 ms (84.43%), Space: 80.5 MB (83.74%) - LeetHub

--- 2043-simple-bank-system/2043-simple-bank-system.java
+++ 2043-simple-bank-system/2043-simple-bank-system.java
@@ -1,47 +1,30 @@
-class Bank
-{
-    int n;
-    long[] bal;
-    public Bank(long[] balance)
-    {
-        bal = balance.clone();
-        n = balance.length;
-    }
-    public boolean transfer(int account1, int account2, long money)
-    {
-        if(account1 > 0 && account1 <= n && account2 > 0 && account2 <= n)
-        {
-            if(bal[account1-1] >= money)
-            {
-                bal[account1-1] -= money;
-                bal[account2-1] += money;
+class Bank {
+        final long[] accounts;
+ 
+        public Bank(long[] balance) {accounts = balance;}
+ 
+        public boolean transfer(int i1, int i2, long money) {
+            if (i2 <= accounts.length && withdraw(i1, money))
+                return deposit(i2, money);
+            else return false;
+        }
+ 
+        public boolean deposit(int i, long money) {
+            if (i <= accounts.length) {
+                accounts[i - 1] += money;
                 return true;
-            }
+            } else return false;
         }
-        return false;
-    }
-    public boolean deposit(int account, long money)
-    {
-        if(account > 0 && account <= n)
-        {
-            bal[account-1] += money;
-            return true;
-        }
-        return false;
-    }
-    public boolean withdraw(int account, long money)
-    {
-        if(account > 0 && account <= n)
-        {
-            if(bal[account-1] >= money)
-            {
-                bal[account-1] -= money;
+ 
+        public boolean withdraw(int i, long money) {
+            if (i <= accounts.length && accounts[i - 1] >= money) {
+                accounts[i - 1] -= money;
                 return true;
-            }
+            } else return false;
         }
-        return false;
     }
-}
+ 
+
 
 /**
  * Your Bank object will be instantiated and called as such:
