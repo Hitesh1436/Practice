47c24f72ede0185f53ede7454fb7728478b2de3d
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-05-11 09:25:45 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (24.19%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,44 +1,18 @@
 class Solution {
-    
-    public boolean inRange(int i, int j, int n, int m){
-        if(i < 0 || i >= n || j < 0 || j >= m)
-            return false;
-        return true;
-    }
-    
-    public int travelHelper(int sr, int sc, int dr, int dc, int [][] dir, int [][] dp){
-        if(sr == dr && sc == dc){
-            return dp[sr][sc] = 1;
-        }
-        
-        if(dp[sr][sc] != 0){
-            return dp[sr][sc];
-        }
-        
-        int count  = 0;
-        for(int d = 0; d < dir.length; d++){
-            int r = sr + dir[d][0];
-            int c = sc + dir[d][1];
-
-            if(inRange(r,c,dr + 1, dc + 1)){
-                count += travelHelper(r,c, dr, dc, dir, dp);
-            }
-        }
-        return dp[sr][sc] = count;
-
-    }
-    
-    public int travel(int n, int m){
-        
-       
-        int [][] dir = {{0,1}, {1,0}};
-        int [][] dp = new int[n][m];
-        int ans = travelHelper(0,0,n-1,m-1,dir,dp);
-        return ans;
-    }
-    
-   
     public int uniquePaths(int n, int m) {
-        return travel(n,m);
+        // int [][]ans = new int[n][m];
+        int [][]dp = new int[n][m];
+        
+        for(int x=0;x<n;x++){
+            for(int y=0;y<m;y++){
+                if(x==0 || y==0){
+                    dp[x][y]=1; 
+                }else{
+                    dp[x][y]= dp[x-1][y]+dp[x][y-1];   
+                }
+            }
+           
+        }
+         return dp[n-1][m-1];
     }
- }
+}
