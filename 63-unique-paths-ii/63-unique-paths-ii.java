d1f92d9ed04c9f96d41de690e0a69a8716e60911
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-05-11 09:47:01 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (96.20%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -1,17 +1,24 @@
 class Solution {
-    
-   public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-    int width = obstacleGrid[0].length;
-    int[] dp = new int[width];
-    dp[0] = 1;
-    for (int[] row : obstacleGrid) {
-        for (int j = 0; j < width; j++) {
-            if (row[j] == 1)
-                dp[j] = 0;
-            else if (j > 0)
-                dp[j] += dp[j - 1];
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+      int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+		// first row and first column can have only one possible path until there is an obstacle m then it will have zero path from there onwards
+        for(int i = 0; i < m;i++){
+            if(obstacleGrid[i][0] == 1) break;
+            dp[i][0] = 1;
         }
+        for(int i = 0;i < n;i++){
+            if(obstacleGrid[0][i] == 1) break;
+            dp[0][i] = 1;
+        }
+        
+        for(int i = 1;i < m;i++){
+            for(int j = 1;j < n;j++){
+                if(obstacleGrid[i][j] == 1) continue;  // handling for an obstacle , dont add any paths from here and you are good
+                dp[i][j] = dp[i][j-1] + dp[i-1][j];
+            }
+        }
+        return dp[m-1][n-1];
     }
-    return dp[width - 1];
-  }
 }
