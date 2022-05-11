59671b4c91058eaee422bbc80c3fcf330142e564
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-05-11 09:45:28 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (34.89%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -1,51 +1,17 @@
 class Solution {
     
-    public boolean inRange(int i,int j,int n,int m){
-        if(i<0 || j<0 || i>=n || j>=m){
-            return false;
+   public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+    int width = obstacleGrid[0].length;
+    int[] dp = new int[width];
+    dp[0] = 1;
+    for (int[] row : obstacleGrid) {
+        for (int j = 0; j < width; j++) {
+            if (row[j] == 1)
+                dp[j] = 0;
+            else if (j > 0)
+                dp[j] += dp[j - 1];
         }
-        return true;
     }
-    
-    public int travelHelper(int sr,int sc,int dr,int dc,int[][]dir,int [][]dp,int [][]grid){
-        if(sr == dr && sc == dc){
-            return dp[sr][sc]=1;
-        }
-        
-        if(dp[sr][sc]!=0){
-            return dp[sr][sc];
-        }
-        int count = 0;
-        for(int d=0;d<dir.length;d++){
-            int r = sr + dir[d][0];
-            int c = sc + dir[d][1];
-            
-            if(inRange(r,c,dr+1,dc+1) && grid[r][c]==0){
-                count += travelHelper(r,c,dr,dc,dir,dp,grid);
-            }
-        }
-        return dp[sr][sc] = count;
-    }
-    
-    public int travel(int n,int m,int [][]grid){
-        int [][]dir = {{0,1},{1,0}};
-        int [][]dp = new int[n][m];
-        
-        int ans = travelHelper(0,0,n-1,m-1,dir,dp,grid);
-        return ans;
-    }
-    
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int n = obstacleGrid.length;
-        int m = obstacleGrid[0].length;
-        
-        if(n==0 || m==0){
-            return 0;
-        }
-        if(obstacleGrid[0][0]==1  || obstacleGrid[n-1][m-1]==1){
-            return 0;
-        }
-        
-        return travel(n,m,obstacleGrid);
-    }
+    return dp[width - 1];
+  }
 }
