a8131defc407667f59185ed3846e18b8e9ee5f5d
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-05-11 09:16:14 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (15.12%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,4 +1,4 @@
- class Solution {
+class Solution {
     
     public boolean inRange(int i, int j, int n, int m){
         if(i < 0 || i >= n || j < 0 || j >= m)
@@ -6,7 +6,7 @@
         return true;
     }
     
-    public int mazePathHelper(int sr, int sc, int dr, int dc, int [][] dir, int [][] dp){
+    public int travelHelper(int sr, int sc, int dr, int dc, int [][] dir, int [][] dp){
         if(sr == dr && sc == dc){
             return dp[sr][sc] = 1;
         }
@@ -21,24 +21,24 @@
             int c = sc + dir[d][1];
 
             if(inRange(r,c,dr + 1, dc + 1)){
-                count += mazePathHelper(r,c, dr, dc, dir, dp);
+                count += travelHelper(r,c, dr, dc, dir, dp);
             }
         }
         return dp[sr][sc] = count;
 
     }
     
-    public int mazePath(int n, int m){
+    public int travel(int n, int m){
         
        
         int [][] dir = {{0,1}, {1,0}};
         int [][] dp = new int[n][m];
-        int ans = mazePathHelper(0,0,n-1,m-1,dir,dp);
+        int ans = travelHelper(0,0,n-1,m-1,dir,dp);
         return ans;
     }
     
    
     public int uniquePaths(int n, int m) {
-        return mazePath(n,m);
+        return travel(n,m);
     }
  }
