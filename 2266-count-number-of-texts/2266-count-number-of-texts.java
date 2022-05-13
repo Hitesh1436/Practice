847b317ef50201a7157743393aa22181a4f8a375
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-05-13 12:32:54 +0530

    Time: 40 ms (76.35%), Space: 53.4 MB (63.01%) - LeetHub

--- 2266-count-number-of-texts/2266-count-number-of-texts.java
+++ 2266-count-number-of-texts/2266-count-number-of-texts.java
@@ -1,30 +1,22 @@
 class Solution {
-     int mod = (1000000007);
-    
-    public int helper(int idx , String s , int[] key,int []dp){
-        // base case
-        if(idx == s.length()){
-            return 1;
-        }
-        if(dp[idx]!=-1){
-            return dp[idx];
-        }
-        int count = 0;
-        int num = s.charAt(idx) - '0';      // ascii ka use kia h string to number change krne mn
-        int press = key[num];
-        for(int i=0;i<press && idx+i<s.length() && s.charAt(idx)==s.charAt(idx+i);i++){
-            count += helper(idx+1+i,s,key,dp);
-           count %= mod;
-        }
-        return dp[idx]=count;
-    }
-    
+    int mod = (1000000007);
+
     public int countTexts(String pressedKeys) {
+        int[] key = new int[] { 0, 0, 3, 3, 3, 3, 3, 4, 3, 4 };
         int n = pressedKeys.length();
-        int [] key = new int []{0,0,3,3,3,3,3,4,3,4};
-        int []dp = new int[n];
-        Arrays.fill(dp,-1);
-        return helper(0,pressedKeys,key,dp);
-       
+        int[] dp = new int[n + 1];
+        dp[n] = 1;
+
+        for (int ind = n - 1; ind >= 0; ind--) {
+            int count = 0;
+            int num = pressedKeys.charAt(ind) - '0';
+            int rep = key[num];
+            for (int i = 0; i < rep && ind + i < pressedKeys.length() && pressedKeys.charAt(ind) == pressedKeys.charAt(ind + i); i++) {
+                count += dp[ind+i+1];
+                count %= mod;
+            }
+             dp[ind] = count;
+        }
+        return dp[0];
     }
 }
