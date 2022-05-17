38d46da5c7c2a433249338fdd193a8f9cc0fda47
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-05-17 10:19:43 +0530

    Time: 3 ms (10.78%), Space: 43.6 MB (40.76%) - LeetHub

--- 107-binary-tree-level-order-traversal-ii/107-binary-tree-level-order-traversal-ii.java
+++ 107-binary-tree-level-order-traversal-ii/107-binary-tree-level-order-traversal-ii.java
@@ -13,49 +13,44 @@
  *     }
  * }
  */
+
 class Solution {
-    public List<List<Integer>> levelOrderBottom(TreeNode node) {
-         Queue<TreeNode> queue = new LinkedList<>();
-        Stack<List<Integer>> st=new Stack<>();
-        List<Integer> l=new ArrayList<>();
-    queue.add(node);
-    queue.add(null);
-        List<List<Integer>> bl=new ArrayList<>();
-        if(node==null)
-            return bl;
-
-    while(queue.size() > 0){
-        TreeNode temp = queue.remove();
-
-        if(temp != null){
-            l.add(temp.val);
-
-            if(temp.left != null){
-                queue.add(temp.left);
-            }
-
-            if(temp.right != null){
-                queue.add(temp.right);
-            }
-        } else {
-            if(queue.size() > 0){
-                
-                queue.add(temp);
-                 
-                 
-            }
-            st.push(l);
-                 l=new ArrayList<>();
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        Queue<TreeNode> qu = new LinkedList<>();
+        qu.add(root);
+        qu.add(null);
+        List<Integer> al = new ArrayList<>();
+        Stack<List<Integer>> st = new Stack<>();
+        
+        if(root == null){
+            List<List<Integer>> base = new ArrayList<>();
+            return base;
+        }
+        while(qu.size()>0){
+            TreeNode rem = qu.remove();
             
+            if(rem != null){
+                al.add(rem.val);
+                
+                if(rem.left != null){
+                    qu.add(rem.left);
+                }
+                
+                if(rem.right != null){
+                    qu.add(rem.right);
+                }
+            }else{
+                if(qu.size()>0){
+                    qu.add(rem);
+                }
+                st.push(al);
+                al = new ArrayList<>();
+            }
         }
-       
-       
-    }
-        List<List<Integer>> rl=new ArrayList<>();
-        while(st.size()>0)
-        {
-            rl.add(st.pop());
-        }
-        return rl;
+        List<List<Integer>> ans = new ArrayList<>();
+            while(st.size()>0){
+                ans.add(st.pop());
+            }
+        return ans;
     }
 }
