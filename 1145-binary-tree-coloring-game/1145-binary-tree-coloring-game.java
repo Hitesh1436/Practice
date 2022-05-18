1930078ef7fb7e4c54d7a1861eeea1aeb523e0c5
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-05-18 22:26:29 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (35.80%) - LeetHub

--- 1145-binary-tree-coloring-game/1145-binary-tree-coloring-game.java
+++ 1145-binary-tree-coloring-game/1145-binary-tree-coloring-game.java
@@ -13,23 +13,31 @@
  *     }
  * }
  */
+// When you find the selected node, there are three different paths you can block: left ,right or parent 
+// if i color left(a) node blue, then red person can color all right and parent nodes red
+// if i color right (b) node blue, then red person can color all left and parent nodes red
+// if i color parent(c) node blue, then red person can color all right and left nodes red
+// so i can only win if i choose a node which contains elements greater than the count of elements in otther 2 nodes i.e a>b+c i color a node blue, or b>a+c i color b node blue , or c>a+b i color c node blue
+// if none of these 3 conditions hold true then red person wins bcoz he has more nodes to color
 class Solution {
-    public boolean btreeGameWinningMove(TreeNode root, int n, int x) {
-	if(root == null) return false;
+	public boolean btreeGameWinningMove(TreeNode root, int n, int x) {
+		if(root==null) return false;
+		if(root.val==x){
+			int a=countofnodes(root.left);
+			int b=countofnodes(root.right);
+			int c=n-a-b-1; // 1 for root
+			if(a>b+c || b>c+a || c>a+b) return true;
+			return false;
+		}
+		boolean left=btreeGameWinningMove(root.left,n,x);
+		if(left) return true;
+		boolean right=btreeGameWinningMove(root.right,n,x);
+		if(right) return true;
 
-	if(root.val == x){
-		int left = count(root.left);
-		int right = count(root.right);
-		int parent = n - (left+right+1);
-
-		return parent > (left + right) || left > (parent + right) || right > (left + parent);
+		return false;
 	}
-
-	return btreeGameWinningMove(root.left, n, x) || btreeGameWinningMove(root.right, n, x);
-}
-
-private int count(TreeNode node){
-	if(node == null) return 0;
-	return count(node.left) + count(node.right) + 1;
-}
+	public int countofnodes(TreeNode root){
+		if(root==null) return 0;
+		return countofnodes(root.left)+countofnodes(root.right)+1;
+	}
 }
