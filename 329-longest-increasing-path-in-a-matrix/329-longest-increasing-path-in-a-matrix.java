da9e66fdfcaa27e2b73e657ab0a2875c0f8384f8
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-05-19 08:04:03 +0530

    Time: 8 ms (92.94%), Space: 54.5 MB (23.90%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.java
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.java
@@ -1,49 +1,44 @@
 class Solution {
+   
     public int longestIncreasingPath(int[][] matrix) {
+        int n=matrix.length;
+        int m=matrix[0].length;
         
-		//Base condition
-        if(matrix.length==0)
-            return 0;
-        
-		
-        int dp[][] = new int[matrix.length][matrix[0].length];
-        int max = 0;
-        
-        for(int i =0;i<matrix.length;i++){
-            for(int j =0;j<matrix[0].length;j++){
-                //result is not stored
-                if(dp[i][j]==0){
-                   dfs(matrix,i,j,Integer.MIN_VALUE,dp);
-                   max = Math.max(dp[i][j],max); 
-                }
-            }
+		//We create a cache for saving the computed result
+        int cache[][]=new int[n][m];
+        int max=0;
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++)
+                max=Math.max(max, recurse(matrix,i,j,cache));
         }
-        
         return max;
     }
     
-    private int dfs(int[][] matrix,int i,int j,int pre,int[][] dp){
-        //Dont fo beyond boundaries and next element should be greater for increasing path
-        if(i<0 || j<0 ||i>=matrix.length || j>=matrix[0].length || pre>=matrix[i][j])
+    public int recurse(int [][]mat, int r, int c, int [][]cache){
+	//Check boundary condition
+        if(r<0||r>=mat.length||c<0||c>=mat[0].length)
             return 0;
+			//If result is pre-computed use the same result
+        if(cache[r][c]!=0)
+            return cache[r][c];
+        
+        int ans=0;
+
+        //In each direction up,bottom,left and right, check if it is possible to move or not
+        if(c-1>=0&&mat[r][c-1]>mat[r][c])
+            ans=Math.max(ans,recurse(mat,r,c-1,cache));
+        if(c+1<mat[0].length&&mat[r][c+1]>mat[r][c])
+            ans=Math.max(ans,recurse(mat,r,c+1,cache));
+        if(r-1>=0&&mat[r-1][c]>mat[r][c])
+            ans=Math.max(ans,recurse(mat,r-1,c,cache));
+        if(r+1<mat.length&&mat[r+1][c]>mat[r][c])
+            ans=Math.max(ans,recurse(mat,r+1,c,cache));        
 			
-		//already explored
-        else if(dp[i][j]!=0) return dp[i][j];
-        
-		//exploring all direction
-        int pathOne = dfs(matrix,i+1,j,matrix[i][j],dp);
-        int pathTwo = dfs(matrix,i-1,j,matrix[i][j],dp);
-        int pathThree = dfs(matrix,i,j-1,matrix[i][j],dp);
-        int pathFour = dfs(matrix,i,j+1,matrix[i][j],dp);
-        
-		//finding the max path
-        int maxOne = Math.max(pathOne,pathTwo);
-        int maxTwo = Math.max(pathThree,pathFour);
-        
-		//store maximum path on that index
-        dp[i][j] = 1+Math.max(maxOne,maxTwo);
-        
-        return dp[i][j];
+		//Finally, take the biggest possible answer and add 1 as this current element must be counted
+		//Store that result in the cache
+        cache[r][c]=1+ans;
+        return 1+ans;
         
     }
 }
