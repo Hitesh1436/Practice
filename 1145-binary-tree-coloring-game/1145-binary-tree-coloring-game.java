8d1ebae5e16f9023a2de10ab84aaa49ae60394db
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-05-19 08:18:20 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (22.47%) - LeetHub

--- 1145-binary-tree-coloring-game/1145-binary-tree-coloring-game.java
+++ 1145-binary-tree-coloring-game/1145-binary-tree-coloring-game.java
@@ -13,31 +13,36 @@
  *     }
  * }
  */
-// When you find the selected node, there are three different paths you can block: left ,right or parent 
-// if i color left(a) node blue, then red person can color all right and parent nodes red
-// if i color right (b) node blue, then red person can color all left and parent nodes red
-// if i color parent(c) node blue, then red person can color all right and left nodes red
-// so i can only win if i choose a node which contains elements greater than the count of elements in otther 2 nodes i.e a>b+c i color a node blue, or b>a+c i color b node blue , or c>a+b i color c node blue
-// if none of these 3 conditions hold true then red person wins bcoz he has more nodes to color
 class Solution {
-	public boolean btreeGameWinningMove(TreeNode root, int n, int x) {
-		if(root==null) return false;
-		if(root.val==x){
-			int a=countofnodes(root.left);
-			int b=countofnodes(root.right);
-			int c=n-a-b-1; // 1 for root
-			if(a>b+c || b>c+a || c>a+b) return true;
-			return false;
-		}
-		boolean left=btreeGameWinningMove(root.left,n,x);
-		if(left) return true;
-		boolean right=btreeGameWinningMove(root.right,n,x);
-		if(right) return true;
-
-		return false;
-	}
-	public int countofnodes(TreeNode root){
-		if(root==null) return 0;
-		return countofnodes(root.left)+countofnodes(root.right)+1;
-	}
+    
+    int xKaLeft,xKaRight;
+    
+    public int size(TreeNode node , int x){
+        if(node == null){
+            return 0;
+        }
+        
+        int leftSize  = size(node.left,x);
+        int rightSize = size(node.right,x);
+        
+        if(node.val == x){
+            xKaLeft = leftSize;
+            xKaRight = rightSize;
+        }
+        int totalSize = leftSize + rightSize + 1;
+        return totalSize;
+    }
+    public boolean btreeGameWinningMove(TreeNode root, int n, int x) {
+        size(root,x);
+         
+        int otherSide = n -(xKaLeft + xKaRight +1);
+        
+        int maxOfThree = Math.max(otherSide,Math.max(xKaLeft,xKaRight));
+        
+        int rest = n - maxOfThree;
+        if(maxOfThree > rest){
+            return true;
+        }
+        return false;
+    }
 }
