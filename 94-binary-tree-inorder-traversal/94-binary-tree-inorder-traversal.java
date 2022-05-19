06c0ed0b20f05220a778f42d1e9d70a79c6238dc
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-05-19 22:24:27 +0530

    Time: 1 ms (41.96%), Space: 42.8 MB (9.80%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
@@ -14,34 +14,31 @@
  * }
  */
 class Solution {
-    
-    public  TreeNode getRightMostNode(TreeNode leftNode,TreeNode curr){
-      while(leftNode.right!=null && leftNode.right!=curr){
-          leftNode = leftNode.right;
-      }
-      return leftNode;
-  }
     public List<Integer> inorderTraversal(TreeNode root) {
-     ArrayList<Integer> ans = new ArrayList<>();
-    TreeNode curr = root;
-    
-    while(curr!=null){
-        TreeNode leftNode = curr.left;
-        if(leftNode == null){
-            ans.add(curr.val);
-            curr = curr.right;
-        }else{
-            TreeNode rightMostNode = getRightMostNode(leftNode,curr);
-            if(rightMostNode.right == null){  // thread create
-                rightMostNode.right = curr;
-                curr = curr.left;
-            }else{
-                rightMostNode.right = null;
+        List<Integer> ans = new ArrayList<>();
+        
+        TreeNode curr = root;
+        
+        while(curr != null){
+            if(curr.left== null){
                 ans.add(curr.val);
                 curr = curr.right;
+            }else{
+                // iop -> inOrder predecessor i.e hmre left ka rightMost node
+                TreeNode iop = curr.left;
+                while(iop.right!= null  && iop.right != curr){
+                    iop = iop.right;
+                }
+                if(iop.right == null){   
+                    iop.right = curr;    // making of thread
+                    curr = curr.left;
+                }else{
+                    iop.right = null;    // breaking of thread
+                        ans.add(curr.val);
+                    curr = curr.right;
+                }
             }
         }
+        return ans;
     }
-    return ans;
- }
 }
