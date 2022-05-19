4b9e7972aef7e48088b4a4d9d525388600b3b592
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-05-19 16:07:59 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (28.96%) - LeetHub

--- 872-leaf-similar-trees/872-leaf-similar-trees.java
+++ 872-leaf-similar-trees/872-leaf-similar-trees.java
@@ -15,21 +15,22 @@
  */
 class Solution {
     public boolean leafSimilar(TreeNode root1, TreeNode root2) {
-        if (root1 == null || root2 == null) return false;
-        StringBuilder sb1 = new StringBuilder();
-        StringBuilder sb2 = new StringBuilder();
-        helper(sb1, root1);
-        helper(sb2, root2);
-        return sb1.toString().equals(sb2.toString());
+        if(root1==null && root2==null) return true;
+        if(root1==null || root2==null) return false;
+        
+        ArrayList<Integer> l1=getVal(root1, new ArrayList<>());
+        ArrayList<Integer> l2=getVal(root2, new ArrayList<>());
+        
+        return l1.equals(l2);
     }
     
-    private void helper(StringBuilder sb, TreeNode root) {
-        if (root == null) return;
-        if (root.left == null && root.right == null) {
-            sb.append(root.val);
-            sb.append('-');
-        } 
-        helper(sb, root.left);
-        helper(sb, root.right);
+    public ArrayList<Integer> getVal(TreeNode root, ArrayList<Integer> temp){
+        if(root==null) return null;
+        
+        getVal(root.left,temp);
+        if(root.left==null && root.right==null) temp.add(root.val);
+        getVal(root.right,temp);
+        
+        return temp;
     }
 }
