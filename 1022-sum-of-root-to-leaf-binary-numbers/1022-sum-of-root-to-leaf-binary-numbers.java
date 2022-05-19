7008a88fc5008ccf65526dc37eecef105c02ba41
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-05-19 09:37:47 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (60.71%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.java
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.java
@@ -13,19 +13,24 @@
  *     }
  * }
  */
-class Solution {
-    public int sumRootToLeaf(TreeNode root) {
-        return pathSumRootToLeaf(root, 0);
+class Solution 
+{//Revise left and right shift operator from GFG (recommended)
+    public int sumRootToLeaf(TreeNode root)
+    {
+        if(root == null)//base case for null graph 
+            return 0;
+        
+        return sum(root, 0);//sum is passed because it has a cyclic dependency  in Recursion for calculating the sum 
     }
-    
-    private int pathSumRootToLeaf(TreeNode root, int parentNodeSum){
-        if(root == null) return 0;
+    public int sum(TreeNode root, int sum)
+    {
+        if(root == null)//base case 1//we we reach an null node we return 0, no shifting operation is done 
+            return 0;
         
-        parentNodeSum = 2 * parentNodeSum + root.val;
-        if(root.left == null && root.right == null){
-            return parentNodeSum;
-        }
+        sum= (sum<<1) + root.val;//in binary addition 0 + 1= 1 && 0 + 0= 0, so only the last place will change by 0 or 1//doing left shift will append zero at the end 
         
-        return pathSumRootToLeaf(root.left, parentNodeSum) + pathSumRootToLeaf(root.right, parentNodeSum);
+        if(root.left == null && root.right == null)//base case 2
+            return sum;//when we got the desired result(Binary Number in integer) at the leaf node returning it to get the total sum and the traverse if any subtree is left 
+        return sum(root.left, sum) + sum(root.right, sum);//calculating the left and right sum recursively //bactracking helps a lot in leaf node sum 
     }
 }
