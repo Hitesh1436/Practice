dd98b426b1a69db4904372c8e12165b90c487777
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-05-21 09:02:54 +0530

    Added solution - LeetHub

--- Inorder Successor in BST - GFG/inorder-successor-in-bst.java
+++ Inorder Successor in BST - GFG/inorder-successor-in-bst.java
@@ -122,52 +122,27 @@ class Node{
 	}
 }
 */
-class Solution
-{
-    // returns the inorder successor of the Node x in BST (rooted at 'root')
-	public Node inorderSuccessor(Node root,Node x) {
-    Node curr = root;
-    Node succ = null;
-        
-    boolean found = false;
-    
-    while(curr != null){
-        if(curr.left == null){
-            // print
-            if(found == true){
-                succ =  curr;
-                break;
-            }
-            
-            if(curr == x){
-                found = true;
-            }
-            curr = curr.right;
-        } else {
-            Node iop = curr.left;
-            while(iop.right != null && iop.right != curr){
-                iop = iop.right;
-            }
-            
-            if(iop.right == null){
-                iop.right = curr;       // making the thread
-                curr = curr.left;
-            } else {
-                // print
-                    if(found == true){
-                        succ =  curr;
-                        break;
-                    }
-                    
-                    if(curr == x){
-                        found = true;
-                    }
-                     iop.right = null;
-                curr = curr.right;
-                }
-            }
-        }
-        return succ;
+class Solution{
+
+	public Node inorderSuccessor(Node root,Node x){
+	    Node succ = null;
+         if(x.right != null){
+             succ = x.right;
+             while(succ.left != null){
+                 succ = succ.left;
+             }
+             return succ;
+         }
+         while(root != null){
+             if(x.data > root.data){
+                 root = root.right;
+             }else if(x.data < root.data){
+                 succ = root;
+                 root = root.left;
+             }else{
+                 break;
+             }
+         }
+          return succ;
     }
-    
 }
