78ef5de3fa6ce11f009c4c5b7fb361f276cf8879
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-05-22 08:49:59 +0530

    Time: 8 ms (48.20%), Space: 42.3 MB (53.92%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.java
+++ 647-palindromic-substrings/647-palindromic-substrings.java
@@ -1,28 +1,28 @@
 class Solution {
     public int countSubstrings(String s) {
         int count = 0;
-        for(int i = 0; i < s.length(); i++) {
+        for(int i = 0; i < s.length(); i++){
+            //count odd length palindrome, 
+            //only one center character
+            count += countPalindromesAroundCenter(s, i, i);
             
-            // handle odd length palindrome (current character)
-            count += palindromeCount(s, i, i);
-            
-            // handle even length palindrome (current character and next character)
-            if (i < s.length() - 1 && s.charAt(i) == s.charAt(i + 1)) { 
-                count += palindromeCount(s, i, i + 1);
-            }        
-        }
-        return count;     
+            //count even length palindrome, 
+            //two center character
+            count += countPalindromesAroundCenter(s, i, i + 1);
+        } 
+        
+        return count;
     }
     
-    public int palindromeCount(String str, int leftPointer, int rightPointer) {
+    
+    private int  countPalindromesAroundCenter(String s, int l, int r){
         int count = 0;
-        while(leftPointer >= 0 && rightPointer < str.length() && 
-                  str.charAt(leftPointer) == str.charAt(rightPointer)) {
-                count++;
-                leftPointer--;
-                rightPointer++;
+        while(l >= 0 && r < s.length()){
+            //just return count, as we found first mismatch
+            if(s.charAt(l--) != s.charAt(r++))  return count;
+            
+            count++;
         }
-        
         return count;
     }
 }
