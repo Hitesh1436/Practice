0c0dd996ee082beecabe318e96a1dab25687a6f7
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-05-23 09:35:19 +0530

    Time: 21 ms (96.06%), Space: 43.2 MB (59.51%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.java
+++ 474-ones-and-zeroes/474-ones-and-zeroes.java
@@ -1,15 +1,38 @@
 class Solution {
-    public int findMaxForm(String[] S, int M, int N) {
-        int[][] dp = new int[M+1][N+1];
-        for (String str : S) {
-            int zeros = 0, ones = 0;
-            for (char c : str.toCharArray())
-                if (c == '0') zeros++;
-                else ones++;
-            for (int i = M; i >= zeros; i--)
-                for (int j = N; j >= ones; j--)
-                    dp[i][j] = Math.max(dp[i][j], dp[i-zeros][j-ones] + 1);
+    public int findMaxForm(String[] s, int m, int n) {
+        int [][]dp = new int[m+1][n+1];
+        for(String str : s){
+            int zeros = 0,ones = 0;
+            for(char ch : str.toCharArray()){
+                if(ch == '0'){
+                    zeros++;
+                }else{
+                    ones++;
+                }
+            }
+            for(int i=m;i>=zeros;i--){
+                    for(int j=n;j>=ones;j--){
+                        dp[i][j] = Math.max(dp[i][j],dp[i-zeros][j-ones]+1);
+                    }
+                }
+            
         }
-        return dp[M][N];
-    }
+        return dp[m][n];
+    } 
 }
+
+// class Solution {
+//     public int findMaxForm(String[] S, int M, int N) {
+//         int[][] dp = new int[M+1][N+1];
+//         for (String str : S) {
+//             int zeros = 0, ones = 0;
+//             for (char c : str.toCharArray())
+//                 if (c == '0') zeros++;
+//                 else ones++;
+//             for (int i = M; i >= zeros; i--)
+//                 for (int j = N; j >= ones; j--)
+//                     dp[i][j] = Math.max(dp[i][j], dp[i-zeros][j-ones] + 1);
+//         }
+//         return dp[M][N];
+//     }
+// }
