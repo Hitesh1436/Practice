47f536495d77affd2a9bdbdfdb500afcc259cbb4
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-05-24 10:43:25 +0530

    Time: 4 ms (7.42%), Space: 43 MB (68.91%) - LeetHub

--- 112-path-sum/112-path-sum.java
+++ 112-path-sum/112-path-sum.java
@@ -14,10 +14,28 @@
  * }
  */
 class Solution {
-    public boolean hasPathSum(TreeNode root, int targetSum) {
-	if (root == null) return false;
-	if (root.left == null && root.right==null && root.val == targetSum) return true;
-	targetSum -= root.val;
-	return hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum);
-   }
+    public boolean hasPathSum(TreeNode root, int sum) {
+	Stack<TreeNode> stack = new Stack<>();
+	while (!stack.isEmpty() || root != null) {
+		while (root != null) {
+			stack.push(root);
+			sum -= root.val;
+			root = root.left;
+		}
+		
+		if (sum == 0 && stack.peek().right == null && stack.peek().left == null) {
+			return true;
+		} 
+		
+		//root == null
+		//pop all nodes whose children pathes have been both checked.
+		while (!stack.isEmpty() && stack.peek().right == root) {
+		    root = stack.pop();
+		    sum += root.val;
+		}
+		root = stack.isEmpty() ? null : stack.peek().right;
+		//root is now the first node in in-order which has never been pushed in the stack.
+	}
+	return false;
+ }
 }
