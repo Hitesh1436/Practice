2ea1f44679e5f0d191ce39f07f38462ff1e5b1ae
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-05-24 14:48:18 +0530

    Time: 3 ms (95.23%), Space: 41.9 MB (98.57%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.java
@@ -15,16 +15,26 @@
  */
 class Solution {
     
-    private boolean helper(TreeNode root, TreeNode subRoot){
-        if(root == null && subRoot == null) return true;
-        if(root == null || subRoot == null) return false;
-        if(root.val !=  subRoot.val) return false;
-        return helper(root.left,subRoot.left) && helper(root.right,subRoot.right);
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+		// in case of bad input
+        if (root == null) return false;
+		
+        // if root vals are equal, then make sure rest of subtree is equal
+        if (root.val == subRoot.val && equals(root, subRoot)) return true;
+        
+		// there can still be duplicate vals in the tree, so we need to keep
+		// checking deeper for top root val then dive into the equals method
+		// if we find it
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
     }
     
-    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if(root == null) return false;
-        if(helper(root,subRoot)) return true;
-        return isSubtree(root.left,subRoot) || isSubtree(root.right,subRoot);
+    private boolean equals(TreeNode root, TreeNode subRoot) {
+		// base case, both nodes are null return true
+        if (root == null && subRoot == null) return true;
+		// now if one node is null, trees are not equal, also check for root val equivalence
+        if (root == null || subRoot == null || root.val != subRoot.val) return false;
+
+		// check entire depth of both subtrees
+        return equals(root.left, subRoot.left) && equals(root.right, subRoot.right);
     }
 }
