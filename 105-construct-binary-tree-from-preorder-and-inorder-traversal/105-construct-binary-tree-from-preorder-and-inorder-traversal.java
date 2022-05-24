9dbc16422e96d7fbd97faade61bd8c892a523a5e
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-05-24 08:12:31 +0530

    Time: 4 ms (54.25%), Space: 44 MB (61.04%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -17,26 +17,41 @@
 
 // psi = pre start index,pei= pre end index,isi = in statr index, iei = in end index
 class Solution {
-    public TreeNode helper(int[] preorder,int pstart,int pend, int[] inorder,int istart,int iend) {
-        if(istart>iend){
-            return null;
-        }
-        int idx = istart;
-        while(inorder[idx]!=preorder[pstart])
-            idx++;
-            // tnol-> tonal no. of elements
-            int tnol = idx - istart;   // left subtree mn itne honge
-            
-            TreeNode ans = new TreeNode(preorder[pstart]);
-            
-            ans.left = helper(preorder,pstart+1,pstart+tnol,inorder,istart,idx-1);
-            ans.right= helper(preorder,pstart+tnol+1,pend,inorder,idx+1,iend);
-        
-        return ans;
-        
-    }
+     private  HashMap<Integer,Integer> hm = new HashMap<>() ;
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        int n = preorder.length;
-        return helper(preorder,0,n-1,inorder,0,n-1);
+        
+        for(int i = 0 ; i < inorder.length ; i = i + 1)
+        {
+            hm.put(inorder[i],i) ;
+        }
+        return Construct(0,preorder.length-1,0,inorder.length-1,preorder ) ; 
     }
-}
+
+    private TreeNode Construct(int pLo,int pHi , int inL , int inHi ,int pre[]  )
+    {
+        if(pLo>pHi  ||  inL > inHi)
+        {
+            return null ;
+        }
+        TreeNode node = new TreeNode(pre[pLo]) ;
+
+        int pos = findInInorder(pre[pLo] ) ;
+
+        int diffL =  pos - inL ;
+         
+        node.left = Construct( pLo+1, pLo+diffL, inL,pos-1,pre ) ;
+        node.right = Construct( pLo+diffL+1, pHi, pos+1,inHi,pre ) ;
+
+        return node ;
+    }
+
+    private int findInInorder(int val  )
+    {
+         if(hm.containsKey(val))
+         {
+             return hm.get(val) ;
+         }
+
+        return - 1;
+    }
+} 
