e3af42505228d6e46af6286746b8cbafe943782f
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-05-26 15:48:33 +0530

    Time: 26 ms (9.22%), Space: 54.1 MB (16.51%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,10 +1,19 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Arrays.sort(nums);                          //Sort the array
-        for(int i=0 ; i<nums.length-1; i+=2){       
-            if(nums[i]!=nums[i+1])                  //Check pairwise values in array
-                return nums[i];                     //if any pairwise values are different, then it is answer
+        HashMap<Integer,Integer> map = new HashMap<>();
+            for(int i=0;i<nums.length;i++){
+                if(map.containsKey(nums[i])){
+                    map.put(nums[i],map.get(nums[i])+1);
+                }else{
+                    map.put(nums[i],1);
+                }
+            }
+        int ans = 0;
+        for(int i : map.keySet()){
+            if(map.get(i)==1){
+                ans = i;
+            }
         }
-        return nums[nums.length - 1];       //if answer is largest value, then it will not be detected in above loop hence return it here
-   }
+        return ans;
+    }
 }
