02a1f9ba2bf129495c6a80e02ca1ca32dd03e835
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-05-26 16:03:23 +0530

    Time: 208 ms (6.20%), Space: 51.1 MB (30.22%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,17 +1,21 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        if (nums.length == 1) {  // if the array has only one element it will return the first element itself
-            return nums[0];
+      Arrays.sort(nums);
+        int i= 0;
+        int j= nums.length-1;
+        int number=0;
+
+     while (i!=j){
+         if(nums[i]==nums[j]){
+          i =j+1;
+          j=nums.length-1;  
+         }
+         else
+             j--;
+     }
+        if(i==j){
+            number=nums[j];
         }
-        Arrays.sort(nums);
-        for (int i = 0 ; i < nums.length ; i = i+2) {
-            if (i == nums.length-1) {  // if i is at the last index return the last index
-                return nums[nums.length -1];
-            }
-            if(nums[i] != nums[i+1]) { 
-                return nums[i];
-            }
-        }
-        return 0;
+     return number;   
     }
 }
