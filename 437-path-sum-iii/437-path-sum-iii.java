46e269260cedc4b6ec73cba0e769eb095c38bc83
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-05-26 18:18:03 +0530

    Time: 5 ms (73.55%), Space: 44.8 MB (31.01%) - LeetHub

--- 437-path-sum-iii/437-path-sum-iii.java
+++ 437-path-sum-iii/437-path-sum-iii.java
@@ -14,29 +14,42 @@
  * }
  */
 class Solution {
-    public int pathSum(TreeNode root, int sum) {
-        HashMap<Integer, Integer> map = new HashMap();
-        map.put(0,1);
-        int ans = helper(root, 0, sum, map);
-        return ans;
-    }
-    
-    public int helper(TreeNode root, int psum, int target, HashMap<Integer, Integer> map) {
-        
-        if (root == null) {
+    int count = 0;
+    public int pathSum(TreeNode root, int targetSum) {
+        if(root == null){
             return 0;
         }
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(0,1);
+        // root ,jhn jarhe h vo val,targetSum,map pass krdia helper mn
+        helper(root,root.val,targetSum,map);
+        return count;
+    }
+    // psum = prefix sum, 
+    private void helper(TreeNode node,int psum,int tar,HashMap<Integer,Integer> map){
         
-        psum += root.val;
-        int res = map.getOrDefault(psum - target, 0);
-        map.put(psum, map.getOrDefault(psum, 0) + 1);
+        if(map.containsKey(psum - tar)== true){
+            count += map.get(psum-tar);
+        }
         
-        int left = helper(root.left, psum, target, map);
-        int right =  helper(root.right, psum, target, map);
+        if(map.containsKey(psum)== false){ // age vo key ni h toh daldo and uske against 1 dldo 
+            map.put(psum,1);
+        }else{
+            map.put(psum,map.get(psum)+1);// age vo key h toh daldo and usko +1 krdo
+        }
         
-        res += left + right;
+        if(node.left != null){
+            helper(node.left,psum + node.left.val,tar,map);
+        }
+        if(node.right != null){
+            helper(node.right,psum + node.right.val,tar,map);
+        }
         
-        map.put(psum, map.get(psum) - 1);
-        return res;
+        // calls
+        if(map.get(psum)==1){  // agr 1 h toh remove krdo
+            map.remove(psum);
+        }else{
+            map.put(psum,map.get(psum)-1);  // agr 1 se jyda h toh -1 krdo
+        }
     }
 }
