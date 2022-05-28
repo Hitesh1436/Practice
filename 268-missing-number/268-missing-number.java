77a3b312bdfb1420c0af9c409a5bc7e137b57916
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-05-28 09:01:19 +0530

    Time: 12 ms (7.76%), Space: 53.6 MB (7.31%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,17 +1,13 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        // sort krdii
-     Arrays.sort(nums);
-     int n = nums.length;
-            if(nums[n-1]==0){
-                return 1;
-            }
-        for(int i=0;i<n-1;i++){
-            if(nums[i]+1 != nums[i+1]){
-                return nums[i]+1;
-            }
-            if(nums[n-1]+1 == n){
-                return n;
+        int n = nums.length;
+        HashSet<Integer> set = new HashSet<>();
+        for(int i=0;i<n;i++){
+            set.add(nums[i]);
+        }
+        for(int i=0;i<=n;i++){
+            if(!set.contains(i)){
+                return i;
             }
         }
         return 0;
