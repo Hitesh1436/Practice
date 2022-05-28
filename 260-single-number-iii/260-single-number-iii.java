d10595c53e9f1a1eaa41a4b8f154f48b8c6ab724
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-05-28 09:58:40 +0530

    Time: 7 ms (23.18%), Space: 46.6 MB (10.92%) - LeetHub

--- 260-single-number-iii/260-single-number-iii.java
+++ 260-single-number-iii/260-single-number-iii.java
@@ -3,14 +3,16 @@ class Solution {
             HashSet<Integer> set= new HashSet<>();
         int[] ans=new int[2];
             for(int i=0; i<nums.length; i++){
-                    if(set.contains(nums[i]))
-                            set.remove(nums[i]);
-                    else
-                            set.add(nums[i]);
+                    if(set.contains(nums[i])){
+                         set.remove(nums[i]);
+                    }else{
+                        set.add(nums[i]);
+                    }               
             }
             int j=0;
-            for(int n:set)
-                    ans[j++]=n;
+            for(int n:set){
+                ans[j++]=n;
+            }        
             return ans;
     }
 }
