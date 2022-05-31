b160b4b40f0cbb28a0314a32f560fd86f8fc95b3
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-05-31 21:02:44 +0530

    Time: 0 ms (100.00%), Space: 44.4 MB (69.61%) - LeetHub

--- 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.java
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.java
@@ -22,21 +22,34 @@ class Node {
 */
 
 class Solution {
-    public Node connect(Node root) {
-        if(root ==null) return null;
-        Queue<Node> qu = new LinkedList<>();
-        qu.add(root);
-        while(qu.size()>0){
-            int size = qu.size();
-            for(int i=0;i<size;i++){
-                Node curr = qu.remove();
-                if(i<size-1){
-                    curr.next = qu.peek();
-                }
-                if(curr.left != null) qu.add(curr.left);
-                if(curr.right != null) qu.add(curr.right);
-            }
-        }
-        return root;
-    }
+	public Node connect(Node root) {
+		if(null == root) {
+			return null;
+		}
+
+		if (root.left != null && root.right != null) {
+			root.left.next = root.right;
+			root.right.next = findNext(root.next);
+		} else if (root.left != null) {
+			root.left.next = findNext(root.next);
+		} else if (root.right != null) {
+			root.right.next = findNext(root.next);
+		}
+
+		/**right first*/
+		connect(root.right);
+		connect(root.left);
+		return root;
+	}
+
+	private Node findNext(Node root) {
+		if (null == root) {
+			return null;
+		} else if (null != root.left) {
+			return root.left;
+		} else if (null != root.right) {
+			return root.right;
+		}
+		return findNext(root.next);
+	}
 }
