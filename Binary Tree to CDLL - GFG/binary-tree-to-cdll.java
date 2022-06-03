1724c962fb729314ad805a4a7b14e2b41044b808
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-06-03 11:19:39 +0530

    Added solution - LeetHub

--- Binary Tree to CDLL - GFG/binary-tree-to-cdll.java
+++ Binary Tree to CDLL - GFG/binary-tree-to-cdll.java
@@ -140,38 +140,28 @@ class Node{
 }
 */
 class Solution{
+    Node start;
+    Node prev;
+    private void helper(Node root){
+        if(root==null)return;
+        helper(root.left);
+        if(prev==null){
+            start=root;
+        }
+        else{
+            prev.right=root;
+            root.left=prev;
+        }
+        prev=root;
+        helper(root.right);
+    }
     //Function to convert binary tree into circular doubly linked list.
     Node bTreeToClist(Node root){
-        return helper(root);
-    }
-    Node helper(Node node){
-        if(node == null)return null;
-        
-        Node lhead = helper(node.left);
-        Node rhead = helper(node.right);
-        
-        Node one = node;
-        one.left = one.right = one;
-        Node s1 = concat(lhead,one);
-        Node s2 = concat(s1,rhead);
-        return s2;
-    }
-    Node concat(Node h1,Node h2){
-        if(h1 == null){
-            return h2;
-        }else if(h2 == null){
-            return h1;
-        }
-        Node t1 = h1.left;
-        Node t2 = h2.left;
-        
-        t1.right = h2;
-        h2.left = t1;
-        
-        t2.right = h1;
-        h1.left = t2;
-        
-        return h1;
+  
+        helper(root);
+        Node last=prev;
+        last.right=start;
+        start.left=last;
+        return start;
     }
 }
-    
