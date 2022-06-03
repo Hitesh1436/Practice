d1986a6d2ef23a942cb840132b6e55350b3b9be5
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-06-03 09:52:10 +0530

    Time: 1 ms (66.66%), Space: 42.6 MB (47.36%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
@@ -14,22 +14,58 @@
  * }
  */
 class Solution {
-    Queue<TreeNode> qu = new LinkedList<>();
-    public void flatten(TreeNode root) {
-        if(root == null) return;
-        preorder(root);
-        qu.remove();
-        TreeNode tmp = root;
-        while(!qu.isEmpty()){
-            tmp.right = qu.remove();
-            tmp.left = null;
-            tmp = tmp.right;
-        } 
+    class Pair{
+        TreeNode head;
+        TreeNode tail;
+        Pair(){
+            
+        }
+        Pair(TreeNode head,TreeNode tail){
+            this.head = head;
+            this.tail = tail;
+        }
     }
-    private void preorder(TreeNode node){
-        if(node == null)return;
-        qu.add(node);
-        preorder(node.left);
-        preorder(node.right);
+    public void flatten(TreeNode root) {
+        if(root != null){
+            helper(root);
+        }
+    }
+    private Pair helper(TreeNode node){
+        if(node.left != null && node.right != null){ 
+            Pair lp = helper(node.left);
+            Pair rp = helper(node.right);
+            Pair myP = new Pair();
+            
+            myP.head = node;
+            myP.head.left= null;
+            myP.head.right = lp.head;
+            lp.tail.right = rp.head;
+            myP.tail = rp.tail;
+            
+            return myP;
+        }else if(node.left != null){
+            Pair lp = helper(node.left);
+            Pair myP = new Pair();
+            
+            myP.head = node;
+            myP.head.left= null;
+            myP.head.right = lp.head;
+            myP.tail = lp.tail;
+            
+            return myP;
+        }else if(node.right != null){
+            Pair rp = helper(node.right);
+            Pair myP = new Pair();
+            
+            myP.head = node;
+            myP.head.left= null;
+            myP.head.right = rp.head;
+            myP.tail = rp.tail;
+            
+            return myP;
+        }else{
+          Pair myP = new Pair(node,node);
+            return myP;
+        }
     }
 }
