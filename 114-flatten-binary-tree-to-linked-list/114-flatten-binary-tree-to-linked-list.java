65135eb58249ac7dc16647cce3b1111e9a982483
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-06-03 10:13:39 +0530

    Time: 0 ms (100.00%), Space: 42.7 MB (47.36%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
@@ -14,58 +14,13 @@
  * }
  */
 class Solution {
-    class Pair{
-        TreeNode head;
-        TreeNode tail;
-        Pair(){
-            
-        }
-        Pair(TreeNode head,TreeNode tail){
-            this.head = head;
-            this.tail = tail;
-        }
-    }
+    TreeNode prev = null;
     public void flatten(TreeNode root) {
-        if(root != null){
-            helper(root);
-        }
-    }
-    private Pair helper(TreeNode node){
-        if(node.left != null && node.right != null){ 
-            Pair lp = helper(node.left);
-            Pair rp = helper(node.right);
-            Pair myP = new Pair();
-            
-            myP.head = node;
-            myP.head.left= null;
-            myP.head.right = lp.head;
-            lp.tail.right = rp.head;
-            myP.tail = rp.tail;
-            
-            return myP;
-        }else if(node.left != null){
-            Pair lp = helper(node.left);
-            Pair myP = new Pair();
-            
-            myP.head = node;
-            myP.head.left= null;
-            myP.head.right = lp.head;
-            myP.tail = lp.tail;
-            
-            return myP;
-        }else if(node.right != null){
-            Pair rp = helper(node.right);
-            Pair myP = new Pair();
-            
-            myP.head = node;
-            myP.head.left= null;
-            myP.head.right = rp.head;
-            myP.tail = rp.tail;
-            
-            return myP;
-        }else{
-          Pair myP = new Pair(node,node);
-            return myP;
-        }
+        if(root == null)return;
+        flatten(root.right);
+        flatten(root.left);
+        root.right = prev;
+        root.left = null;
+        prev = root;
     }
 }
