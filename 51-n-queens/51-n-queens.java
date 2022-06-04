33ae03a6f440ca543d368e860e0cc7410a01335d
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-06-04 16:42:03 +0530

    Time: 10 ms (29.33%), Space: 47.4 MB (9.44%) - LeetHub

--- 51-n-queens/51-n-queens.java
+++ 51-n-queens/51-n-queens.java
@@ -2,15 +2,13 @@ class Solution {
     public List<List<String>> solveNQueens(int n) {
         return queens(new boolean[n][n],0);
     }
- public   List<List<String>>  queens(boolean[][] board,int row){
+    private List<List<String>>  queens(boolean[][] board,int row){
         if (row == board.length) {
             List<List<String>> list = new ArrayList<>();
           list.add(display(board));
           return list;
         }
-
             List<List<String>> ans = new ArrayList<>();
-
             for (int col = 0; col < board.length; col++) {
                 if(ifSafe(board,row,col)){
                     board[row][col] = true;
@@ -20,16 +18,14 @@ class Solution {
             }
         return ans;
     }
-    public  boolean ifSafe(boolean[][] board,int row,int col){
+    private  boolean ifSafe(boolean[][] board,int row,int col){
         //chek for vertically
         for (int i = 0; i < row; i++) {
             if (board[i][col]){
                 return false;
             }
         }
-
         //check for diagonal left
-
         int maxleft = Math.min(row,col);
         for (int i = 1 ; i <=maxleft ; i++) {
             if (board[row-i][col-i]){
@@ -45,7 +41,7 @@ class Solution {
         }
         return true;
     }
-    public  List<String> display(boolean[][] board){
+    private  List<String> display(boolean[][] board){
         List<String> list = new ArrayList<>();
                 String p ="";
         for(boolean[] arr:board){
