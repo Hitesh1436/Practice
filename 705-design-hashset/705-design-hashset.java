74b508929ed3c595d0f95e0a350acb4fe627a110
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-06-06 15:25:34 +0530

    Time: 768 ms (9.07%), Space: 50 MB (89.28%) - LeetHub

--- 705-design-hashset/705-design-hashset.java
+++ 705-design-hashset/705-design-hashset.java
@@ -1,30 +1,24 @@
 class MyHashSet {
-    List<Integer> al;
+
+    /** Initialize your data structure here. */
+    List<Integer> l;
     public MyHashSet() {
-        al = new ArrayList<>();
+        l=new ArrayList();
     }
     
     public void add(int key) {
-        if(!al.contains(key))
-            al.add(key);
+        if(l.contains(key)==false)
+            l.add(key);
     }
     
     public void remove(int key) {
-        if(al.contains(key)){
-            al.remove(new Integer(key));
-        }
+        if(l.contains(key))
+            l.remove(l.indexOf(key));
     }
     
+    /** Returns true if this set contains the specified element */
     public boolean contains(int key) {
-        if(al.contains(key)) return true;
-        return false;
+        return l.contains(key);
+           
     }
 }
-
-/**
- * Your MyHashSet object will be instantiated and called as such:
- * MyHashSet obj = new MyHashSet();
- * obj.add(key);
- * obj.remove(key);
- * boolean param_3 = obj.contains(key);
- */
