f24d0252deb3f96a2e46983a6fc1536e83720c06
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-06-06 22:10:56 +0530

    Time: 23 ms (47.53%), Space: 50.9 MB (66.51%) - LeetHub

--- 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.java
+++ 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.java
@@ -7,30 +7,41 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+import java.util.StringTokenizer;
 public class Codec {
-     public String serialize(TreeNode root) 
-{
-    if(root == null) return "#";
-    
-    return "" + root.val + " " + serialize(root.left) + " " + serialize(root.right);
+public String serialize(TreeNode root) {
+    StringBuilder sb = new StringBuilder();
+    serialize(root, sb);
+    return sb.toString();
 }
 
+private void serialize(TreeNode root, StringBuilder sb) {
+    if (root == null)
+        sb.append("# ");
+    else {
+        sb.append(root.val + " ");
+        serialize(root.left, sb);
+        serialize(root.right, sb);
+    }
+}
 
+// ADD "import java.util.StringTokenizer;" AT TOP ==========================
 // Decodes your encoded data to tree.
-public TreeNode deserialize(String data){
-
-    return build(new Scanner(data));
+public TreeNode deserialize(String data) {
+    if (data == null || data.length() == 0) return null;
+    StringTokenizer st = new StringTokenizer(data, " ");
+    return deserialize(st);
 }
 
-private TreeNode build(Scanner sc)
-{
-    if(!sc.hasNext()) return null;
-    String tk = sc.next();
-    if(tk.equals("#")) return null;
-    
-    TreeNode root = new TreeNode(Integer.parseInt(tk));
-    root.left = build(sc);
-    root.right = build(sc);
+private TreeNode deserialize(StringTokenizer st) {
+    if (!st.hasMoreTokens())
+        return null;
+    String s = st.nextToken();
+    if (s.equals("#"))
+        return null;
+    TreeNode root = new TreeNode(Integer.valueOf(s));
+    root.left = deserialize(st);
+    root.right = deserialize(st);
     
     return root;
  }
