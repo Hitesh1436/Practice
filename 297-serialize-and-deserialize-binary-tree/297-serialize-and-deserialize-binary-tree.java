5f3d7fcf0d32260e7a5ef3a1d4bb755a019077d2
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-06-07 10:33:50 +0530

    Time: 17 ms (64.47%), Space: 52.2 MB (49.09%) - LeetHub

--- 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.java
+++ 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.java
@@ -8,81 +8,42 @@
  * }
  */
 public class Codec {
-    class Pair{
-        TreeNode node;
-        int state;
-        
-        public Pair(TreeNode node){
-            this.node=node;
-            this.state=0;
-        }
-    }
 
     // Encodes a tree to a single string.
-    StringBuilder sb = new StringBuilder("");
+    static StringBuilder sb;
     public String serialize(TreeNode root) {
-        helper(root);
-        return sb.toString();
+      sb = new StringBuilder();
+    serializeHelper(root);
+    return sb.toString();
+}
+public void serializeHelper(TreeNode root) {
+    if(root==null){
+        sb.append(". "); return;
     }
-    
-    public void helper(TreeNode node){
-        if(node==null){
-            sb.append(". ");
-            return;
-        }
-        
-        sb.append(node.val+" ");
-        helper(node.left);
-        helper(node.right);
-    }
-
-    // Decodes your encoded data to tree.
-    public TreeNode deserialize(String data) {
-        Stack<Pair> s=new Stack<>();
-        TreeNode root=new TreeNode();
-        
-        
-        String []str=data.split(" ");
-        
-        if(str[0].equals(".")){
-            return null;
-        }
-        root.val=Integer.parseInt(str[0]);
-        s.push(new Pair(root));
-        
-        for(int i=1;i<str.length;i++){
-            if(!s.isEmpty()){
-                Pair curr=s.peek();
-                TreeNode newNode=new TreeNode();
-            
-                if(str[i].equals(".")){
-                    newNode=null;
-                }else{
-                    newNode.val=Integer.parseInt(str[i]);
-                }
-                
-                if(curr.state==0){
-                    curr.node.left=newNode;
-                }else if(curr.state==1){
-                    curr.node.right=newNode;
-                }
-            
-                curr.state=curr.state+1;
-                
-                if(newNode!=null){
-                    s.push(new Pair(newNode));
-                }  
-            }
-            
-            while(!s.isEmpty() && s.peek().state==2){
-                s.pop();
-            }
-        }
-        
-        return root;
-    }
+    sb.append(root.val+" ");
+    serializeHelper(root.left);
+    serializeHelper(root.right);
 }
 
+    // Decodes your encoded data to tree.
+    static int idx;
+public TreeNode deserialize(String data) {
+    idx = 0;
+    return deserializeHelper(data.split(" "));
+}
+
+public TreeNode deserializeHelper(String[] data) {
+    if(idx>=data.length || data[idx].equals(".")){
+        return null;
+    }
+    TreeNode root = new TreeNode(Integer.parseInt(data[idx]));
+    idx++;
+    root.left = deserializeHelper(data);
+    idx++;
+    root.right = deserializeHelper(data);
+    return root;
+ }
+}
 
 // Your Codec object will be instantiated and called as such:
 // Codec ser = new Codec();
