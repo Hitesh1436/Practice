6b87af38378a88d58f20d39fb98d4e2869178692
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-06-08 08:39:37 +0530

    Time: 1 ms (18.06%), Space: 41.6 MB (61.81%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.java
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.java
@@ -3,21 +3,12 @@
 
 // But if none of above conditions satisfy, we can leverage the fact that there are only 2 character types in the given string always and hence, we can select the longest one or all the characters of one type, delete it since any length of one type of characaters is always a palindrome, then select the whole remaining string with the other type, delete it. Giving the result as 2 operations.
 
+
 class Solution {
-    public boolean isPalindrome(String s) {
-        int n = s.length();
-        for(int i=0;i<n/2;i++){
-            if(s.charAt(i) != s.charAt(n-1-i)){
-                return false;
-            }
-        }
-        return true;
-    }
-    public int removePalindromeSub(String s){
-        int n = s.length();
-        if(n == 0){
-            return 0;
-        }
-        return isPalindrome(s) ? 1: 2;
+    public int removePalindromeSub(String s) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(s);
+        sb.reverse();
+        return sb.toString().equals(s) ? 1 : 2;
     }
 }
