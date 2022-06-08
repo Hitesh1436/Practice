bc13cce9d865247aee7020e2fa456e12d216f03d
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-06-08 08:33:46 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (47.22%) - LeetHub

--- 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.java
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.java
@@ -1,3 +1,8 @@
+// We need to observe that if the string is already a palindrome, we can just select the whole of it and delete it in one go and hence give the result as 1.
+// If the string is already empty, that means our initial state is our final goal of the problem statement too. Hence, no deletions are required
+
+// But if none of above conditions satisfy, we can leverage the fact that there are only 2 character types in the given string always and hence, we can select the longest one or all the characters of one type, delete it since any length of one type of characaters is always a palindrome, then select the whole remaining string with the other type, delete it. Giving the result as 2 operations.
+
 class Solution {
     public boolean isPalindrome(String s) {
         int n = s.length();
