f1528f989d264eff1a13739bcb30e1b2f94b769c
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-06-10 08:38:51 +0530

    Time: 207 ms (11.06%), Space: 106.7 MB (8.62%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -1,20 +1,44 @@
+// Using Sliding Window Concept
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        if(s.length()==0) return 0;
-        char ch = s.charAt(0);
-        String ans = "";
-        ans += ch;
-        int max =1;
-        for(int i=1;i<s.length();i++){
-            ch = s.charAt(i);
-            int idx = ans.indexOf(ch);
-            if(idx == -1){
-                ans += ch;
-                max = Math.max(max,ans.length());
+        int left = 0;  // start of window
+        int right =0;  // end of window
+        int ans =0;
+        HashSet<Character> set = new HashSet<>();
+        while(right<s.length()){
+            if(!set.contains(s.charAt(right))){
+                set.add(s.charAt(right));
+                 ans = Math.max(ans,set.size());
+                right++;
             }else{
-                ans = ans.substring(idx+1)+ ch;
+                left++;
+                right = left;
+                set.clear();
             }
         }
-        return max;
+        return ans;
     }
 }
+
+//  Using Basic String Functions
+
+// class Solution {
+//     public int lengthOfLongestSubstring(String s) {
+//         if(s.length()==0) return 0;
+//         char ch = s.charAt(0);
+//         String ans = "";
+//         ans += ch;
+//         int max =1;
+//         for(int i=1;i<s.length();i++){
+//             ch = s.charAt(i);
+//             int idx = ans.indexOf(ch);
+//             if(idx == -1){
+//                 ans += ch;
+//                 max = Math.max(max,ans.length());
+//             }else{
+//                 ans = ans.substring(idx+1)+ ch;
+//             }
+//         }
+//         return max;
+//     }
+// }
