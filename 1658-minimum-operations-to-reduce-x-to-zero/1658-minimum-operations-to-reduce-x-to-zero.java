620cfa11394307627f6c69ad518e09058569bbbc
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-06-11 09:36:56 +0530

    Time: 4 ms (100.00%), Space: 97.1 MB (44.21%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
@@ -1,35 +1,27 @@
 class Solution {
     public int minOperations(int[] nums, int x) {
-	int sum = 0;
-	for (int i : nums) {
-		sum += i;
-	}
-	int target = sum - x;
-	int longSub = longestSub(nums, target);
-	return longSub == Integer.MIN_VALUE ? -1 : nums.length - longSub;
-}
-
-int longestSub(int[] nums, int target) {
-	Map<Integer, Integer> map = new HashMap<>();
-	if (target == 0) {
-		return 0;
-	}
-	int sum = 0;
-	int max = Integer.MIN_VALUE;
-	for (int i = 0; i < nums.length; i++) {
-
-		sum += nums[i];
-
-		if (sum == target) {
-			max = i + 1;
-		} else {
-			int comp = sum - target;
-			if (map.containsKey(comp)) {
-				max = Math.max(max, i - map.get(comp));
-			}
-		}
-		map.put(sum, i);
-	}
-	return max;
-}
+        int n = nums.length;   
+        int sum = 0;
+        for (int i = 0; i < n; i++) 
+            sum += nums[i];
+        int target = sum - x;
+        
+        if (target < 0)
+            return -1;
+        if (target == 0)
+            return n;
+        
+        int left = 0, right = 0;
+        int max = 0;
+        while (right < n) {
+            target -= nums[right++];
+            
+            while (target < 0)
+                target += nums[left++];
+            
+            if (target == 0)
+                max = Math.max(max, right - left);
+        }
+        return max == 0 ? -1 : nums.length - max;
+    }
 }
