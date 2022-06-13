5713ffe85229e16b4f4b12be61898d9732fd8c9a
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-06-13 08:14:37 +0530

    Time: 13 ms (9.95%), Space: 44.4 MB (29.81%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -26,34 +26,62 @@
 // Time complexity: O(N^2)
 // Space complexity: O(N^2)
 
+// class Solution {
+//     public int minimumTotal(List<List<Integer>> triangle) {
+//         // create dp table
+//         int[][] memo = new int[triangle.size()][triangle.get(triangle.size() - 1).size()];
+//         for (int[] row: memo)
+//             Arrays.fill(row, Integer.MAX_VALUE);
+        
+//         // start from the top of the triangle
+//         return recurse(triangle, 0, 0, memo);
+//     }
+    
+//     private int recurse(List<List<Integer>> triangle, int row, int col, int[][] memo) {
+//         // base case
+//         if (row == triangle.size())
+//             return 0;
+        
+//         // check dp table
+//         if (memo[row][col] != Integer.MAX_VALUE)
+//             return memo[row][col];
+        
+//         // either go to [row + 1, col] or [row + 1, col + 1]
+//         int remain = Math.min(recurse(triangle, row + 1, col, memo),
+//                               recurse(triangle, row + 1, col + 1, memo));
+        
+//         // combine answer with current value
+//         memo[row][col] = triangle.get(row).get(col) + remain;
+//         return memo[row][col];
+//     }
+// }
+
+
+// 3. Tabulation
+// Time complexity: O(N^2)
+// Space complexity: O(N^2)
+
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        // create dp table
-        int[][] memo = new int[triangle.size()][triangle.get(triangle.size() - 1).size()];
-        for (int[] row: memo)
-            Arrays.fill(row, Integer.MAX_VALUE);
+        // create dp table by copying the input triangle
+        List<List<Integer>> table = new ArrayList<>();
+        for (List<Integer> row: triangle) 
+            table.add(new ArrayList<>(row));
         
-        // start from the top of the triangle
-        return recurse(triangle, 0, 0, memo);
-    }
-    
-    private int recurse(List<List<Integer>> triangle, int row, int col, int[][] memo) {
-        // base case
-        if (row == triangle.size())
-            return 0;
+        // iterate over the entire triangle from bottom to top
+        for (int row = triangle.size() - 2; row >= 0; row--) {
+            for (int col = 0; col < triangle.get(row).size(); col++) {
+                // either go to [row + 1, col] or [row + 1, col + 1]
+                int remain = Math.min(table.get(row + 1).get(col),
+                                      table.get(row + 1).get(col + 1));
+                
+                // combine answer with current value
+                table.get(row).set(col, triangle.get(row).get(col) + remain);
+            }
+        }
         
-        // check dp table
-        if (memo[row][col] != Integer.MAX_VALUE)
-            return memo[row][col];
-        
-        // either go to [row + 1, col] or [row + 1, col + 1]
-        int remain = Math.min(recurse(triangle, row + 1, col, memo),
-                              recurse(triangle, row + 1, col + 1, memo));
-        
-        // combine answer with current value
-        memo[row][col] = triangle.get(row).get(col) + remain;
-        return memo[row][col];
+        // return answer for the top of the triangle
+        return table.get(0).get(0);
     }
 }
-
 
