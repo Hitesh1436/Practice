e856c99b8205fda23ee2b4f2f9f9ddcd38a58d68
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-06-13 08:15:36 +0530

    Time: 3 ms (75.26%), Space: 43.5 MB (68.73%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -99,8 +99,7 @@ class Solution {
         for (int row = triangle.size() - 1; row >= 0; row--) {
             for (int col = 0; col <= row; col++) {
                 // either go to [row + 1, col] or [row + 1, col + 1]
-                int remain = Math.min(table[col],
-                                      table[col + 1]);
+                int remain = Math.min(table[col],table[col + 1]);
                 
                 // combine answer with current value
                 table[col] = triangle.get(row).get(col) + remain;
