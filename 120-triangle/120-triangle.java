869673abc394d2c15f31e85f87e803302188268d
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-06-13 08:15:01 +0530

    Time: 3 ms (75.26%), Space: 43.8 MB (57.81%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -61,27 +61,53 @@
 // Time complexity: O(N^2)
 // Space complexity: O(N^2)
 
+// class Solution {
+//     public int minimumTotal(List<List<Integer>> triangle) {
+//         // create dp table by copying the input triangle
+//         List<List<Integer>> table = new ArrayList<>();
+//         for (List<Integer> row: triangle) 
+//             table.add(new ArrayList<>(row));
+        
+//         // iterate over the entire triangle from bottom to top
+//         for (int row = triangle.size() - 2; row >= 0; row--) {
+//             for (int col = 0; col < triangle.get(row).size(); col++) {
+//                 // either go to [row + 1, col] or [row + 1, col + 1]
+//                 int remain = Math.min(table.get(row + 1).get(col),
+//                                       table.get(row + 1).get(col + 1));
+                
+//                 // combine answer with current value
+//                 table.get(row).set(col, triangle.get(row).get(col) + remain);
+//             }
+//         }
+        
+//         // return answer for the top of the triangle
+//         return table.get(0).get(0);
+//     }
+// }
+
+// 4. Tabulation With Optimized Space
+// Time complexity: O(N^2)
+// Space complexity: O(N)
+
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        // create dp table by copying the input triangle
-        List<List<Integer>> table = new ArrayList<>();
-        for (List<Integer> row: triangle) 
-            table.add(new ArrayList<>(row));
-        
+        // create dp table with size of the largest column (which is number of rows)
+        int[] table = new int[triangle.size() + 1];
+        Arrays.fill(table, 0);
+                
         // iterate over the entire triangle from bottom to top
-        for (int row = triangle.size() - 2; row >= 0; row--) {
-            for (int col = 0; col < triangle.get(row).size(); col++) {
+        for (int row = triangle.size() - 1; row >= 0; row--) {
+            for (int col = 0; col <= row; col++) {
                 // either go to [row + 1, col] or [row + 1, col + 1]
-                int remain = Math.min(table.get(row + 1).get(col),
-                                      table.get(row + 1).get(col + 1));
+                int remain = Math.min(table[col],
+                                      table[col + 1]);
                 
                 // combine answer with current value
-                table.get(row).set(col, triangle.get(row).get(col) + remain);
+                table[col] = triangle.get(row).get(col) + remain;
             }
         }
         
-        // return answer for the top of the triangle
-        return table.get(0).get(0);
+        return table[0];
     }
 }
 
