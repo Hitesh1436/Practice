f4d929bbe43ccd8bd24f7d4842ff9a8e1b972d96
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-06-14 10:08:31 +0530

    Time: 10 ms (32.89%), Space: 65.7 MB (40.49%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,33 +1,30 @@
 class Solution {
-    public int numEnclaves(int[][] arr) {
-// first of all we will remove zeros from the corners of the matrix
-        for(int i=0;i<arr.length;i++){
-            for(int j =0;j<arr[0].length;j++){
-                if(i==0 ||j==0 ||i==arr.length-1 || j==arr[0].length-1){
-                    dfsTraversal(arr,i,j);
+    public int numEnclaves(int[][] grid) {
+        int count =0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(i ==0 || j==0 || i==grid.length-1 || j== grid[0].length-1){
+                    dfs(grid,i,j);
+                }
+            }
+        }  
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]==1){
+                    count ++;
                 }
             }
         }
-        
-        int count=0;
-    for(int i=0;i<arr.length;i++){
-        for(int j=0;j<arr[i].length;j++){
-            if(arr[i][j]==1){
-                count++;
-            }
-        }
+        return count;
     }
-    return count;
-}
-    public void dfsTraversal(int [][] arr,int i ,int j ){
-        if(i<0 || j<0 ||i>=arr.length || j>=arr[0].length || arr[i][j]==0){
-            return ;
+    private void dfs(int [][]grid,int i,int j){
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]==0){
+            return;
         }
-        
-        arr[i][j]=0;
-      dfsTraversal(arr,i-1,j);//up
-      dfsTraversal(arr,i,j+1);//right
-      dfsTraversal(arr,i+1,j);//down
-      dfsTraversal(arr,i,j-1);//left
+        grid[i][j] = 0;  // mark krdia 
+        dfs(grid,i-1,j);
+        dfs(grid,i+1,j);
+        dfs(grid,i,j-1);
+        dfs(grid,i,j+1);
     }
 }
