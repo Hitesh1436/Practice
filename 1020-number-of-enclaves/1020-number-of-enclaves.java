ccd76ba8288fe02a5ae25c2b5efd8b6b20750b86
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-06-14 10:16:08 +0530

    Time: 11 ms (27.90%), Space: 65.9 MB (32.68%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,30 +1,39 @@
 class Solution {
+    boolean flag = true;
+    int len = 0;
     public int numEnclaves(int[][] grid) {
-        int count =0;
+        boolean [][]vis = new boolean[grid.length][grid[0].length];
+        int count=0;
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
-                if(i ==0 || j==0 || i==grid.length-1 || j== grid[0].length-1){
-                    dfs(grid,i,j);
-                }
-            }
-        }  
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j]==1){
-                    count ++;
+                if(vis[i][j] == false && grid[i][j]==1){
+                    flag = true;
+                    len =0;
+                    traverse(grid,vis,i,j);
+                    if(flag == true){
+                        count += len;
+                    }
                 }
             }
         }
         return count;
     }
-    private void dfs(int [][]grid,int i,int j){
-        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]==0){
+    private void traverse(int[][] grid, boolean[][]vis,int i,int j){
+         if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length){
+            flag = false;
+            return;
+        } else if(grid[i][j] == 0){
+            return;
+        } else if(vis[i][j] == true){
             return;
         }
-        grid[i][j] = 0;  // mark krdia 
-        dfs(grid,i-1,j);
-        dfs(grid,i+1,j);
-        dfs(grid,i,j-1);
-        dfs(grid,i,j+1);
+        
+        vis[i][j] = true;
+        len++;
+        
+        traverse(grid, vis, i - 1, j);
+        traverse(grid, vis, i + 1, j);
+        traverse(grid, vis, i, j - 1);
+        traverse(grid, vis, i, j + 1);
     }
 }
