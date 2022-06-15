95b42f6a7bda6b5c865e1be26ce4547519896eff
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-06-15 22:30:59 +0530

    Time: 24 ms (59.43%), Space: 60.7 MB (59.65%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.java
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.java
@@ -1,31 +1,51 @@
 class Solution {
-    public int maxDistance(int[][] matrix) {
-      Queue<int[]> queue = new LinkedList<>();
-      int[][] directions = new int[][] {{1,0}, {-1,0}, {0,1}, {0,-1}};
-      int m = matrix.length;
-      int n = matrix[0].length;
-      int max = Integer.MIN_VALUE; //if it is not possible return -1..
-      for(int i = 0; i < m; i++)
-          for(int j = 0; j < n; j++)
-              if(matrix[i][j] == 1){ //land
-                  matrix[i][j] = 0; //make the land 0 because we will sum it with child!
-                  queue.add(new int[]{i,j});
-              }
-              else
-                  matrix[i][j] = Integer.MIN_VALUE; //waters
-
-      while(!queue.isEmpty()){
-          int[] pos = queue.remove();
-          for(int[] dir: directions){
-              int x = pos[0] + dir[0];
-              int y = pos[1] + dir[1];
-              if( x >= 0 && y >= 0 && x < m && y < n && matrix[x][y] < 0){ //point must be valid and water (Integer.MIN_VALUE)
-                  queue.add(new int[]{x,y});
-                  matrix[x][y] = matrix[pos[0]][pos[1]] + 1;
-                  max = Math.max(max , matrix[x][y]);
-              }
-          }
-      }
-      return max == Integer.MIN_VALUE ? -1 : max;
+    public class Pair{
+        int x;
+        int y;
+        
+        Pair(int x ,int y ){
+            this.x=x;
+            this.y=y;
+        }
+    }
+    public int maxDistance(int[][] grid) {
+        ArrayDeque <Pair> q=new ArrayDeque<>();
+        
+        for(int i=0;i<grid.length;i++ ){
+            for(int j =0;j<grid[i].length;j++){
+                if(grid[i][j]==1){
+                    q.add(new Pair(i,j));
+                    
+                }
+                else{
+                    
+                }
+            }
+        }
+        
+        if(q.size()==0 || q.size()==grid.length*grid[0].length){
+            return -1;
+        }
+        int [][] dirs={{-1,0},{0,1},{1,0},{0,-1}};
+        
+        int level=-1;
+        while(q.size()!=0){
+            int size=q.size();
+            level++;
+        while(size-->0){
+            Pair rem=q.removeFirst();
+            
+            for(int i =0;i<dirs.length;i++){
+            int newrow=rem.x+dirs[i][0];
+            int newcol=rem.y+dirs[i][1];
+                
+            if(newrow>=0 && newcol>=0 && newrow<grid.length && newcol<grid[0].length && grid[newrow][newcol]==0){
+                grid[newrow][newcol]=1;//marking it as visited
+                q.add(new Pair(newrow,newcol));
+            }
+         }
+        }    
+  }
+   return level;
     }
 }
