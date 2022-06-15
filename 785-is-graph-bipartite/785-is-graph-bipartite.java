8eff0d9c82f9cc88fb62d0f2469684b243460509
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-06-15 12:46:39 +0530

    Time: 0 ms (100.00%), Space: 54 MB (34.98%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.java
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -1,10 +1,9 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
-         int [] vis = new int [graph.length];
-        
+        int [] vis = new int[graph.length];
         for(int v=0;v<graph.length;v++){
             if(vis[v] == 0){
-                boolean isBip =  traverseBFS(graph,vis,v);
+                boolean isBip = traverseDFS(graph,vis,v,1);
                 if(isBip == false){
                     return false;
                 }
@@ -12,37 +11,19 @@ class Solution {
         }
         return true;
     }
-    class Pair{
-        int v;
-        int clr;
-        
-        Pair(int v,int clr){
-            this.v = v;
-            this.clr = clr;
-        }
-    }
-    private boolean traverseBFS(int[][]graph,int []vis,int v){
-        ArrayDeque<Pair> qu = new ArrayDeque<>();
-        
-        qu.add(new Pair(v,1));  // 1 mns red and -1 mns green 
-        while(qu.size()>0){
-            // r
-            Pair rem = qu.remove();
-            // m*
-            if(vis[rem.v] != 0){
-                int oldClr = vis[rem.v];
-                int newClr = rem.clr;
-                if(oldClr == newClr){
-                    continue;
-                }else{
+    private boolean traverseDFS(int[][] graph,int []vis,int v,int color){
+        vis[v] = color;
+        for(int nbr : graph[v]){
+            if(vis[nbr] == 0){
+                boolean isBip = traverseDFS(graph,vis,nbr,color*-1);
+                if(isBip == false){
                     return false;
                 }
-            }
-            vis[rem.v] = rem.clr;
-            
-            for(int nbr : graph[rem.v]){
-                if(vis[nbr] == 0){
-                    qu.add(new Pair(nbr,rem.clr * -1));
+            }else{
+                int oldColor = vis[nbr];
+                int newColor = color * -1;
+                if(oldColor != newColor){
+                    return false;
                 }
             }
         }
