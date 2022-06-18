6e38f255adba5fc764988263f1b472266456cc04
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-06-18 10:21:37 +0530

    Time: 7 ms (80.41%), Space: 58.7 MB (27.04%) - LeetHub

--- 690-employee-importance/690-employee-importance.java
+++ 690-employee-importance/690-employee-importance.java
@@ -8,18 +8,26 @@ class Employee {
 */
 
 class Solution {
-    public int getImportance(List<Employee> employees, int id) {
-        HashMap<Integer,Employee> map = new HashMap<>();
-        for(Employee e : employees){
-            map.put(e.id,e);
-        }
-        return helper(map,id);
-    }
-    private int helper( HashMap<Integer,Employee> map,int id){
-        int imp = map.get(id).importance;   
-        for(int subId : map.get(id).subordinates){
-            imp += helper(map,subId);
-        }
-        return imp;
-    }
+	public int getImportance(List<Employee> employees, int id) {
+		// Do some preprocessing to avoid the search time complexity for each ID, so make a HasMap of [ID -> Employee]
+		HashMap<Integer,Employee> map=new HashMap<>();
+		for(int i=0;i<employees.size();i++){
+			map.put(employees.get(i).id,employees.get(i)); // ID->Emp
+		}
+		return getans(map,id);
+	}
+	public int getans(HashMap<Integer, Employee> map,int id){
+		// Recursively get the ans for each id
+		Employee rp=map.get(id); 
+		if(rp.subordinates.size()==0){ // base case !
+			return rp.importance;
+		}
+		int ans=0;
+		List<Integer> child=rp.subordinates;
+		for(int i=0;i<child.size();i++){
+			ans+=getans(map,child.get(i)); // get ans for child IDs
+		}
+
+		return rp.importance+ans; // Add my importance to child IDs
+	}
 }
