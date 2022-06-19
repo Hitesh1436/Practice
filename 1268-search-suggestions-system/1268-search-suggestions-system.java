741e7ac7111027536e44f96dc47f7d3da979ef49
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-06-19 09:55:17 +0530

    Time: 102 ms (33.98%), Space: 50.4 MB (83.87%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.java
+++ 1268-search-suggestions-system/1268-search-suggestions-system.java
@@ -1,30 +1,33 @@
 class Solution {
-    public List<List<String>> suggestedProducts(String[] pro, String word) {
-        List<List<String>> ans = new ArrayList<>();
-        if(pro == null ||  pro.length == 0 || word == null || word.length()==0){
-            return ans;
-        }
-        Arrays.sort(pro);
-        List<String> possibleAns = new ArrayList<>();
-        for(String p : pro){
-            possibleAns.add(p);
-        }
-        for(int i=0;i<word.length();i++){
-            char ch = word.charAt(i);
-            List<String> filtered = new ArrayList<>();
-            for(String pd : possibleAns){
-                if(i < pd.length() && ch == pd.charAt(i)){
-                    filtered.add(pd);
+    public List<List<String>> suggestedProducts(String[] products, String searchWord) {
+        Map<String, List<String>> map = new HashMap<>();
+        List<List<String>> result = new ArrayList<>();
+        
+        for(String product : products){
+            String key = "";
+            for(int i=0;i<product.length();i++){
+                key += product.charAt(i);
+                if(map.get(key) == null){
+                    map.put(key, new ArrayList<>());
                 }
+                map.get(key).add(product);
             }
-            List<String> atMostThree = new ArrayList<>();
-            for(int j=0;j<3 && j<filtered.size();j++){
-                atMostThree.add(filtered.get(j));
-            }
-            
-            ans.add(atMostThree);
-            possibleAns = filtered;
         }
-        return ans;
+        String key = "";
+        for(int i=0;i<searchWord.length();i++){
+            key += searchWord.charAt(i);
+            List<String> wordsWithPrefix = map.get(key);
+            if(wordsWithPrefix == null){
+                result.add(new ArrayList<>());
+                continue;
+            }
+            Collections.sort(wordsWithPrefix);
+            List<String> curr = new ArrayList<>();
+            for(int j=0;j<3 && j<wordsWithPrefix.size();j++){
+                curr.add(wordsWithPrefix.get(j));  
+            }
+            result.add(curr);    
+        }
+        return result;        
     }
 }
