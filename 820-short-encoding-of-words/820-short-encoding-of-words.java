9bfb39659159d1489153e8637b4c81ff9e7a9e58
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-06-20 08:55:42 +0530

    Time: 700 ms (6.13%), Space: 117.7 MB (5.52%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.java
+++ 820-short-encoding-of-words/820-short-encoding-of-words.java
@@ -2,15 +2,14 @@ class Solution {
     public int minimumLengthEncoding(String[] words) {
         
         String ans = "";
-        Arrays.sort(words, (a,b)->b.length() - a.length());
+        // Lambda Expression hai yeh same kaam krta h jse comparable krta h
+        Arrays.sort(words, (a,b)->
+                    b.length() - a.length());
         
         for(int i=0;i<words.length;i++){
-            
             String temp = words[i] + "#";
             if(!ans.contains(temp)) ans = ans + temp;
         }
-        
-        return ans.length();
-        
+        return ans.length(); 
     }
 }
