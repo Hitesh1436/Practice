567db8666897f367c016303e6ac65cad88443d35
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-06-20 11:01:13 +0530

    Time: 3 ms (8.41%), Space: 42 MB (41.84%) - LeetHub

--- 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.java
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.java
@@ -1,15 +1,19 @@
 class Solution {
-    public int balancedStringSplit(String s) {
-        int ans =0;
-        int bal = 0;
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i) == 'L'){
-                bal++;
-            }else{
-                bal--;
-            }
-            if(bal == 0) ans++;
+     public int balancedStringSplit(String s) {
+        Stack<Character> stack = new Stack();
+        int res = 0;
+        //going over string chars
+        for (char ch : s.toCharArray()) {
+            //check if char doesn't balance the previous one, or the stack is empty
+            if (stack.isEmpty() || stack.peek() == ch)
+                stack.push(ch);
+            //if chars are balanced - remove the pair
+            else
+                stack.pop();
+            //if stack is empty - all pairs are balanced and we have a balanced substring
+            if (stack.isEmpty()) 
+                res++;
         }
-        return ans;
+        return res;
     }
 }
