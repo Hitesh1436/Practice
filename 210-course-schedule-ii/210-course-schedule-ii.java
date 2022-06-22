a30acd8c45ffe92b33c0ba86b94f5ddb085d0ee7
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-06-22 17:24:11 +0530

    Time: 9 ms (57.99%), Space: 48.3 MB (75.76%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.java
+++ 210-course-schedule-ii/210-course-schedule-ii.java
@@ -1,35 +1,43 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        int[] ans = new int[numCourses];
-        
-        int[] in = new int[numCourses];
-        for(int[] edge: prerequisites){
-            in[edge[1]]++;
+       ArrayList<Integer>[] graph = new ArrayList[numCourses];
+        for(int i = 0; i < graph.length; i++){
+            graph[i] = new ArrayList<>();
         }
+        for(int[] req: prerequisites){
+            graph[req[0]].add(req[1]);
+        }
+        int[] in = new int[numCourses];
+        for(int v = 0; v < graph.length; v++){
+            for(int n: graph[v]){
+                in[n]++;
+            }
+        } 
         ArrayDeque<Integer> queue = new ArrayDeque<>();
-        for(int course = 0; course < numCourses; course++){
-            if(in[course] == 0){
-                queue.add(course);
+        for(int v = 0; v < graph.length; v++){
+            if(in[v] == 0){
+                queue.add(v);
             }
         }
+        int[] ans = new int[graph.length];
         int idx = ans.length - 1;
         while(queue.size() > 0){
-            int course = queue.remove();
-            ans[idx--] = course;
+            int rem = queue.remove();
+            ans[idx] = rem;
+            idx--;
             
-            for(int[] edge: prerequisites){
-                if(edge[0] == course){
-                    in[edge[1]]--;
-                    
-                    if(in[edge[1]] == 0){
-                        queue.add(edge[1]);
-                    }
+            for(int n: graph[rem]){
+                in[n]--;
+                
+                if(in[n] == 0){
+                    queue.add(n);
                 }
             }
         }
-        if(idx != -1){
-            return new int[]{};
+        if(idx == -1){
+            return ans;
+        } else {
+            return new int[] {};
         }
-        return ans;
     }
 }
