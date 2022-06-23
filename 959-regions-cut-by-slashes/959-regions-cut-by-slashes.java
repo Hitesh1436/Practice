b7a0475b4bed14b535d6b10b145dbe6b28d12051
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-06-23 22:35:31 +0530

    Time: 4 ms (85.26%), Space: 41.6 MB (84.58%) - LeetHub

--- 959-regions-cut-by-slashes/959-regions-cut-by-slashes.java
+++ 959-regions-cut-by-slashes/959-regions-cut-by-slashes.java
@@ -1,40 +1,59 @@
 class Solution {
-
-  public int regionsBySlashes(String[] grid) {
-    int[][] arr = new int[grid.length * 3][grid.length * 3];
-
-    for (int i = 0; i < grid.length; i++) {
-      for (int j = 0; j < grid[i].toCharArray().length; j++) {
-        if (grid[i].charAt(j) == '/') {
-          arr[i * 3][j * 3 + 2] = 1;
-          arr[i * 3 + 1][j * 3 + 1] = 1;
-          arr[i * 3 + 2][j * 3] = 1;
-        } else if (grid[i].charAt(j) == '\\') {
-          arr[i * 3][j * 3] = 1;
-          arr[i * 3 + 1][j * 3 + 1] = 1;
-          arr[i * 3 + 2][j * 3 + 2] = 1;
+    //Companies : Amazon, Uber, Microsoft
+    public int regionsBySlashes(String[] arr) {
+        int n = arr.length;
+        if( n == 0 ) return 0; // 0 regions
+		
+		// Mapping co-ordinates , not cells
+        int[] par = new int[(n+1)*(n+1)];  // ex: 1*1 matrix  has 1 column but 2 co-ordinates (0,0) & (0,1)
+        
+        for( int i = 0 ; i < par.length ; i++ ){
+            int r = i / (n+1);
+            int c = i % (n+1);  
+            if( r == 0 || r == n || c == 0 || c == n ){ // boundary
+                // set their common global parent
+                par[i] = 1; // keep this in array range
+            }else{
+                par[i] = i;
+            }
         }
-      }
-    }
-
-    int regions = 0;
-    for (int i = 0; i < arr.length; i++) {
-      for (int j = 0; j < arr[i].length; j++) {
-        if (arr[i][j] == 0) {
-          dfs(i, j, arr);
-          regions++;
+        int regions = 1;
+        for( int i = 0 ; i < arr.length ; i++ ){
+            String str = arr[i];
+            for( int j = 0 ; j < str.length() ; j++ ){
+                char ch = str.charAt(j);
+    
+                if( ch == '/' ){
+                    int x1 = i;
+                    int y1 = j+1;
+                    int x2 = i+1;
+                    int y2 = j;
+                    int p1 = findPar( x1*(n+1) + y1 ,par );
+                    int p2 = findPar( x2*(n+1) + y2 ,par );
+                    if(p1 != p2){
+                        par[p2] = p1;
+                    }else{
+                        regions++;
+                    }
+                }else if( ch == '\\' ){
+                    int x1 = i;
+                    int y1 = j;
+                    int x2 = i+1;
+                    int y2 = j+1;
+                    int p1 = findPar( x1*(n+1) + y1 ,par );
+                    int p2 = findPar( x2*(n+1) + y2 ,par );
+                    if(p1 != p2){
+                        par[p2] = p1;
+                    }else{
+                        regions++;
+                    }
+                }else{ // empty space    
+                }
+            }
         }
-      }
+        return regions;
     }
-    return regions;
-  }
-
-  private void dfs(int i, int j, int[][] arr) {
-    if (i < 0 || i >= arr.length || j < 0 || j >= arr.length || arr[i][j] == 1) return;
-    arr[i][j] = 1;
-    dfs(i - 1, j, arr);
-    dfs(i + 1, j, arr);
-    dfs(i, j - 1, arr);
-    dfs(i, j + 1, arr);
-  }
+    public int findPar(int u , int[] par){
+        return par[u] == u ? u : ( par[u] = findPar( par[u],par ) );
+    }
 }
