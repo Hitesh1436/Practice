d8649fb6694f802a2c50df85f9eedd6516d176d6
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-06-28 11:10:15 +0530

    Time: 2 ms (65.21%), Space: 43 MB (22.96%) - LeetHub

--- 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.java
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.java
@@ -1,28 +1,62 @@
 class Solution {
-    public int find(int u){
-        return par[u] == u ? u : (par[u] = find(par[u]));
-    }
-    int[] par;
+    int []parent;
+    int []rank;
     public boolean equationsPossible(String[] equations) {
-        par = new int[26];
-        
-        for(int i = 0 ; i < 26 ; i++){
-            par[i] = i;
+        parent = new int[26];
+        rank   = new int[26];
+        for(int i=0;i<26;i++){
+            parent[i] = i;
+            rank[i] =0;
         }
-        for(String str : equations){
-            int p1 = find(str.charAt(0) - 'a');
-            int p2 = find(str.charAt(3) - 'a');
-            if(p1 != p2 && str.charAt(1) == '='){
-                par[p1] = p2;
-            }   
+        for(String eqn : equations){
+            if(eqn.charAt(1) == '='){
+                int op1 = eqn.charAt(0)-'a';
+                int op2 = eqn.charAt(3)-'a';
+                
+                int l1 = find(op1);
+                int l2 = find(op2);
+                if(l1 != l2){
+                    union(l1,l2);
+                }
+            }
         }
-        for(String str : equations){
-            int p1 = find(str.charAt(0) - 'a');
-            int p2 = find(str.charAt(3) - 'a');
-            if(p1 == p2 && str.charAt(1) == '!'){
-                return false;
-            }   
+        for(String eqn : equations){
+            if(eqn.charAt(1) == '!'){
+                int op1 = eqn.charAt(0)-'a';
+                int op2 = eqn.charAt(3)-'a';
+                
+                int l1 = find(op1);
+                int l2 = find(op2);
+                if(l1 == l2){
+                    return false;
+                }
+            }
         }
         return true;
-    }  
+    }
+    void unionHelper(int x,int y){
+        int xx = find(x);
+        int yy = find(y);
+        if(xx != yy){
+                union(x,y);
+        }
+    }
+   void union(int X, int Y){
+        if(rank[X] < rank[Y]){
+            parent[X] = Y;
+        } else if(rank[Y] < rank[X]){
+            parent[Y] = X;
+        } else {
+            parent[X] = Y;
+            rank[Y]++;
+        }
+    }
+    
+    int find(int x){
+        if(parent[x] != x){
+            parent[x] = find(parent[x]);
+        }
+        
+        return parent[x];
+    }
 }
