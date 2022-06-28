a11c91a2cd4e50e4e89eabab4f5de8318555559f
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-06-28 22:16:49 +0530

    Time: 1 ms (90.72%), Space: 42.3 MB (9.54%) - LeetHub

---
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<Integer> getRow(int n) {
+        Integer []arr = new Integer[n+1];
+        Arrays.fill(arr,0);
+        arr[0] = 1;
+        for(int i=1;i<=n;i++){
+            for(int j=i;j>0;j--){
+                arr[j] = arr[j] + arr[j-1];
+            }
+        }
+        return Arrays.asList(arr);
+    }
+}
