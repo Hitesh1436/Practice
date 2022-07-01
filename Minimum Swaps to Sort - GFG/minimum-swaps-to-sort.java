5eac2e49ebb598ce5a6fbfc7e394ba3f7c24bb04
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-07-01 21:27:55 +0530

    Added solution - LeetHub

--- Minimum Swaps to Sort - GFG/minimum-swaps-to-sort.java
+++ Minimum Swaps to Sort - GFG/minimum-swaps-to-sort.java
@@ -29,23 +29,40 @@ class GFG
 class Solution
 {
     //Function to find the minimum number of swaps required to sort the array.
-    public int minSwaps(int nums[])
-    {
-        int n = nums.length;
-        int []arr = nums.clone();
-        Arrays.sort(arr);
+    public int minSwaps(int nums[]){
+      int len = nums.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<len;i++)
+            map.put(nums[i], i);
+            
+        Arrays.sort(nums);   
         
-        int ans =0;
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i=0;i<n;i++){
-            map.put(arr[i],i);
-        }
-        for(int i=0;i<n;i++){
-            while(map.get(nums[i]) != i){
-                ans++;
-                int temp =nums[i];
-                nums[i] = nums[map.get(temp)];
-                nums[map.get(temp)] = temp;
+          // To keep track of visited elements. Initialize
+        // all elements as not visited or false.
+        boolean[] visited = new boolean[len];
+        Arrays.fill(visited, false);
+        
+          // Initialize result
+        int ans = 0;
+        for(int i=0;i<len;i++) {
+          
+              // already swapped and corrected or
+            // already present at correct pos
+            if(visited[i] || map.get(nums[i]) == i)
+                continue;
+                
+            int j = i, cycle_size = 0;
+            while(!visited[j]) {
+                visited[j] = true;
+                
+                  // move to next node
+                j = map.get(nums[j]);
+                cycle_size++;
+            }
+            
+              // Update answer by adding current cycle.
+            if(cycle_size > 0) {
+                ans += (cycle_size - 1);
             }
         }
         return ans;
