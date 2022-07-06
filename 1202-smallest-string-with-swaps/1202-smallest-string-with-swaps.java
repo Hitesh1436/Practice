4a0a411eb4be5330dbf9ee5ef4aaa9f18e46a7ff
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-07-06 15:44:06 +0530

    Time: 122 ms (27.45%), Space: 107.9 MB (15.28%) - LeetHub

--- 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.java
+++ 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.java
@@ -1,50 +1,54 @@
 class Solution {
+    // graph
+    ArrayList<Integer> gp[];
     public String smallestStringWithSwaps(String s, List<List<Integer>> pairs) {
-        char[] ch = s.toCharArray();
-        UnionFind uf = new UnionFind(ch.length);
-        HashMap<Integer,PriorityQueue<Character>>map = new HashMap<>();
+        // prepare graph;
+        gp = new ArrayList[s.length()];
+        for(int i = 0; i < s.length(); i++) {
+            gp[i] = new ArrayList<>();
+        }
+        for(int i = 0; i <pairs.size(); i++) {
+            int u = pairs.get(i).get(0);
+            int v = pairs.get(i).get(1);
+            gp[u].add(v);
+            gp[v].add(u);
+        }
         
-        // set union find
-        for(List<Integer>p : pairs){
-            uf.union(p.get(0),p.get(1));
+        // dfs and fill
+        boolean vis[] = new boolean[s.length()];
+        char ans[] = new char[s.length()];
+        for(int i = 0; i < s.length(); i++) {
+            if(!vis[i]) {
+                ArrayList<Integer> idx = new ArrayList<>();
+                dfs(i, vis, idx);
+                fill(s, ans, idx);
+            }
         }
-        // set map and pq
-        for(int i=0;i<s.length();i++){
-            int parent  = uf.find(i);
-            PriorityQueue<Character> pq = map.getOrDefault(parent,new PriorityQueue<>());
-            pq.add(ch[i]);
-            map.put(parent,pq);
+        return String.copyValueOf(ans);
+    }
+    public void dfs(int i, boolean[] vis, ArrayList<Integer> idxs) {
+        vis[i] = true;
+        idxs.add(i);
+        for(int nei = 0; nei < gp[i].size(); nei++) {
+            if(!vis[gp[i].get(nei)]) {
+                dfs(gp[i].get(nei), vis, idxs);
+            }
         }
-        // reorder
-         for (int i = 0;i<s.length();i++){
-            int parent = uf.find(i);
-            ch[i] = map.get(parent).remove();
+    }
+    public void fill(String s, char[] ans, ArrayList<Integer> idxs) {
+        int ch[] = new int[26];
+        Collections.sort(idxs);
+        for(int i = 0; i < idxs.size(); i++) {
+            ch[s.charAt(idxs.get(i)) - 'a']++;
         }
-        return String.valueOf(ch);
+        for(int i = 0; i < idxs.size(); i++) {
+            for(int c = 0; c < 26; c++) {
+                if(ch[c] > 0) {
+                    ans[idxs.get(i)] = (char)(c + 'a');
+                    ch[c]--;
+                    break;
+                }
+            }
+        }
     }
 }
-    public class UnionFind {
-        int[] parent;
-        public UnionFind(int n){
-            parent = new int[n];
-            for (int i = 0; i < n; i++){
-                parent[i] = i;
-            }
-        }
-        
-        public int find(int i ){
-            if (parent[i] == i){
-                return i;
-            }
-            parent[i] = find(parent[i]);
-            return parent[i];
-        }
-        
-        public void union(int i,int j ){
-            int a = find(i);
-            int b = find(j);
-            if (a != b){
-                parent[b] = a;
-            }
-        }
-    }
