00ccfcd1272a7d834f59f3a1cd53e35619fc6028
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-07-06 17:39:20 +0530

    Time: 1 ms (62.56%), Space: 42 MB (22.33%) - LeetHub

--- 765-couples-holding-hands/765-couples-holding-hands.java
+++ 765-couples-holding-hands/765-couples-holding-hands.java
@@ -1,21 +1,33 @@
 class Solution {
     public int minSwapsCouples(int[] row) {
-        int swaps = 0;
-        
-        for(int i=0;i<row.length-1;i++) {
-            int pair = (row[i]%2 == 0)? row[i]+1: row[i]-1; // get the number's pair, if n is even, its partner is n+1, else its partner is n-1
-            
-            if(row[i+1] == pair)  i++;// continue if the number is already with its pair 
-            
-            else for(int j=i+1;j<row.length;j++) // else find its pair and swap it
-                if(row[j] == pair) {
-                    int temp = row[i+1];
-                    row[i+1] = row[j];
-                    row[j] = temp;
-                    swaps++; // record the number of swaps
-                    break;
-                    }
+        int n = row.length;
+        parent = new int[n];
+        for(int i = 0 ; i < n ; i++){
+            parent[i] = i;
         }
-        return swaps; // and finally return the number of swaps :)
+        
+        for(int i = 0 ; i < n ; i+=2){
+            parent[row[i]] = row[i];
+            parent[row[i + 1]] = row[i];
+        }
+        int count = 0;
+        for(int i = 0 ; i < n ; i+=2){
+            int p1 = find(i);
+            int p2 = find(i + 1);
+            if(p1 != p2){
+                parent[p2] = p1;
+                count++;
+            }
+        }
+        return count;
+    }
+    int[] parent;
+    public int find(int u){
+        if(parent[u] ==u){
+            return u;
+        }else{
+            parent[u] = find(parent[u]);
+            return parent[u];
+        }
     }
 }
