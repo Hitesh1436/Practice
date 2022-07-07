46d280f2ed80c468fcf652c9a5ecb46ab04e2078
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-07-07 09:23:49 +0530

    Time: 169 ms (23.52%), Space: 93.3 MB (10.85%) - LeetHub

--- 854-k-similar-strings/854-k-similar-strings.java
+++ 854-k-similar-strings/854-k-similar-strings.java
@@ -1,29 +1,53 @@
 class Solution {
-    public int kSimilarity(String s1, String s2){
-        Queue<String> q = new ArrayDeque<>();
-        q.add(s1);
-        int lvl = 0;
-        while(q.size() > 0){
-            int size = q.size();
-            while(size-- > 0){
-                String rem  =   q.remove();
-                if(rem.equals(s2))return lvl;
-                int i = 0;
-                while(rem.charAt(i) == s2.charAt(i))i++;
-                int j = i;
-                while(j < rem.length()){
-                    if(rem.charAt(j) == s2.charAt(i) && s2.charAt(j) != rem.charAt(j)){
-                        StringBuilder sb = new StringBuilder(rem);
-                        sb.setCharAt(i, rem.charAt(j));
-                        sb.setCharAt(j, rem.charAt(i));
-                        if(sb.toString().equals(s2))return lvl+1;
-                        q.add(sb.toString());
+    int c = 0;
+    public int kSimilarity(String s1, String s2) {
+        HashSet<String> vis = new HashSet<>();
+        Queue<String> que = new ArrayDeque<>();
+        int level = 0;
+        que.add(s1);
+        while(que.size()>0){
+            int size = que.size();
+            while(size-->0){
+                String config = que.remove();
+                if(vis.contains(config)){
+                    continue;
+                }
+                vis.add(config);
+                if(config.equals(s2)){
+                    return level;
+                }
+                for(String s: getNeighbors(config,s2)){
+                    if(!vis.contains(s)){
+                        que.add(s);
                     }
-                    j++;
                 }
             }
-            lvl++;
+            level++;
         }
-        return lvl;
+        return -1;
+    }
+    private ArrayList<String> getNeighbors(String s1, String s2){
+        ArrayList<String> list = new ArrayList<String>();
+        int idx = 0;
+        for(int i=0;i<s1.length();i++){
+            if(s1.charAt(i)!=s2.charAt(i)){
+                idx = i; break;
+            }
+        }
+        for(int i=idx+1;i<s2.length();i++){
+            if(s1.charAt(i)==s2.charAt(idx)){
+                String s = swap(s1,i,idx);
+                list.add(s);
+            }
+        }
+        return list;
+    }
+    
+    private String swap(String str, int i, int j){
+        StringBuilder sb = new StringBuilder(str);
+        char c = sb.charAt(i);
+        sb.setCharAt(i,sb.charAt(j));
+        sb.setCharAt(j,c);
+        return sb.toString();
     }
 }
