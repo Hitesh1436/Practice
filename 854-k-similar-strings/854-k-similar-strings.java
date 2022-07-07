d4737caabf935b0c7bdb54ac99f75311fb2a7da6
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-07-07 15:19:51 +0530

    Time: 110 ms (49.67%), Space: 50 MB (84.54%) - LeetHub

--- 854-k-similar-strings/854-k-similar-strings.java
+++ 854-k-similar-strings/854-k-similar-strings.java
@@ -1,32 +1,33 @@
 class Solution {
-    int c = 0;
     public int kSimilarity(String s1, String s2) {
         HashSet<String> vis = new HashSet<>();
-        Queue<String> que = new ArrayDeque<>();
-        int level = 0;
-        que.add(s1);
-        while(que.size()>0){
-            int size = que.size();
-            while(size-->0){
-                String config = que.remove();
-                if(vis.contains(config)){
+        Queue<String> qu = new ArrayDeque<>();
+        int level =0;
+        qu.add(s1);
+        
+        while(qu.size()>0){
+            int size = qu.size();
+            while(size-- >0){
+                String rem = qu.remove();
+                if(vis.contains(rem)){
                     continue;
                 }
-                vis.add(config);
-                if(config.equals(s2)){
+                vis.add(rem);
+                
+                if(rem.equals(s2)){
                     return level;
                 }
-                for(String s: getNeighbors(config,s2)){
+                for(String s :getNeighbours(rem,s2)){
                     if(!vis.contains(s)){
-                        que.add(s);
+                        qu.add(s);
                     }
                 }
             }
             level++;
         }
-        return -1;
+        return -1;   
     }
-    private ArrayList<String> getNeighbors(String s1, String s2){
+    private ArrayList<String> getNeighbours(String s1, String s2){
         ArrayList<String> list = new ArrayList<String>();
         int idx = 0;
         for(int i=0;i<s1.length();i++){
@@ -42,12 +43,11 @@ class Solution {
         }
         return list;
     }
-    
     private String swap(String str, int i, int j){
-        StringBuilder sb = new StringBuilder(str);
-        char c = sb.charAt(i);
-        sb.setCharAt(i,sb.charAt(j));
-        sb.setCharAt(j,c);
-        return sb.toString();
+            StringBuilder sb = new StringBuilder(str);
+            char c = sb.charAt(i);
+            sb.setCharAt(i,sb.charAt(j));
+            sb.setCharAt(j,c);
+            return sb.toString();
+        }
     }
-}
