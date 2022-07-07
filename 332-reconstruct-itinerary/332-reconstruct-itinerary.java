0cfcaa7fb1f4fbe025924409872fe51aaef6e86a
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-07-07 16:15:23 +0530

    Time: 9 ms (70.17%), Space: 49.7 MB (75.12%) - LeetHub

--- 332-reconstruct-itinerary/332-reconstruct-itinerary.java
+++ 332-reconstruct-itinerary/332-reconstruct-itinerary.java
@@ -1,36 +1,31 @@
 class Solution {
-    HashMap<String, PriorityQueue<String>> adj;
-    
+    HashMap<String,PriorityQueue<String>>adj;
     public List<String> findItinerary(List<List<String>> tickets) {
         adj = new HashMap<>();
-        for(List<String> ticket: tickets){
-            String src = ticket.get(0);
-            String dest = ticket.get(1);
+        for(List<String> tic : tickets){
+            String src = tic.get(0);
+            String dest = tic.get(1);
             
             if(adj.containsKey(src)){
-                adj.get(src).add(dest);                
-            } else {
-                adj.put(src, new PriorityQueue<>());
+                adj.get(src).add(dest);
+            }else{
+                adj.put(src,new PriorityQueue<>());
                 adj.get(src).add(dest);
             }
         }
-        
-        LinkedList<String> res = new LinkedList<>();
-        dfs("JFK", res);
-        return res;
+        LinkedList<String> ans = new LinkedList<>();
+        dfs("JFK",ans);
+        return ans;
     }
-    
-    public void dfs(String v, LinkedList<String> res){
-        if(adj.containsKey(v) == false || adj.get(v).size() == 0){
-            res.addFirst(v);
+    public void dfs(String v,LinkedList<String> ans){
+        if(adj.containsKey(v) ==false || adj.get(v).size()==0){
+            ans.addFirst(v);
             return;
         }
-        
-        while(adj.get(v).size() > 0){
-            String n = adj.get(v).remove();
-            dfs(n, res);
+        while(adj.get(v).size()>0){
+            String nbr = adj.get(v).remove();
+            dfs(nbr,ans);
         }
-        
-        res.addFirst(v);
+        ans.addFirst(v);
     }
 }
