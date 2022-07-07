bfaa015693ceb3617bc4f832033021f3650c50d1
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-07-07 18:04:07 +0530

    Time: 1 ms (99.72%), Space: 40.6 MB (90.75%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.java
+++ 97-interleaving-string/97-interleaving-string.java
@@ -1,18 +1,37 @@
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
-    char[] c1 = s1.toCharArray(), c2 = s2.toCharArray(), c3 = s3.toCharArray();
-	int m = s1.length(), n = s2.length();
-	if(m + n != s3.length()) return false;
-	return dfs(c1, c2, c3, 0, 0, 0, new boolean[m + 1][n + 1]);
+     if(s1.length()+s2.length()!=s3.length())
+       return false;
+       Boolean dp[][]= new Boolean[s1.length()+1][s2.length()+1];
+       return  solve(s1,s2,s3,0,0,dp);
+    }
+    public boolean solve(String s1,String s2,String s3,int i,int j, Boolean[][]dp){
+           if( i == s1.length() && j == s2.length() )
+            return  true;
+         
+         if(dp[i][j]!=null){
+         
+             return dp[i][j];
+         }
+         
+        if(i< s1.length() && s1.charAt(i)==s3.charAt(i+j)){
+           boolean f1= solve(s1,s2,s3,i+1,j,dp);
+            dp[i][j] =  f1;
+            if(f1==true){
+                return true;
+            }
+        }
+       if(j< s2.length() && s2.charAt(j)==s3.charAt(i+j)){ 
+           boolean f2= solve(s1,s2,s3,i,j+1,dp);
+            dp[i][j] = f2;
+            if(f2==true){
+                return true;
+            }    
+        }   
+       dp[i][j]=false;  
+       return dp[i][j];  
+     }
 }
 
-    public boolean dfs(char[] c1, char[] c2, char[] c3, int i, int j, int k, boolean[][] invalid) {
-        if(invalid[i][j]) return false;
-        if(k == c3.length) return true;
-        boolean valid = 
-            i < c1.length && c1[i] == c3[k] && dfs(c1, c2, c3, i + 1, j, k + 1, invalid) || 
-            j < c2.length && c2[j] == c3[k] && dfs(c1, c2, c3, i, j + 1, k + 1, invalid);
-        if(!valid) invalid[i][j] = true;
-        return valid;
-    }
-}
+    
+   
