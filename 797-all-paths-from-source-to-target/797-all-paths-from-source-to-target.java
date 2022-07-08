a6f7270e520d454e7077aa409d313b5c0351eb96
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-07-08 18:57:57 +0530

    Time: 17 ms (7.64%), Space: 54.8 MB (68.80%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
@@ -1,26 +1,23 @@
 class Solution {
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        List<List<Integer>> res = new ArrayList<>();
-        int n = graph.length;
-        boolean []vis=new boolean[n];
-        List<Integer> path = new ArrayList<>();
-            path.add(0);  // src add krdia
-        dfs(graph,0,n-1,vis,res,path);
-        return res;
-    }
-    private void dfs(int [][]graph,int src,int dest,boolean []vis,List<List<Integer>> res,List<Integer>path){
-        if(src == dest){
-            res.add(new ArrayList<>(path));
-            return;
-        }
-        vis[src]= true;
-        for(int nbr : graph[src]){
-            if(vis[nbr]== false){
-                path.add(nbr);
-                dfs(graph,nbr,dest,vis,res,path);
-                path.remove(path.size()-1);  // backtracking kia h 
+        List<List<Integer>> ans = new ArrayList<>();
+        Queue<List<Integer>> q = new LinkedList<>();
+        List<Integer> currList = new ArrayList<>();
+        currList.add(0);
+        q.add(currList);
+        while(!q.isEmpty()) {
+            currList = q.poll();
+            int v = currList.get(currList.size() - 1);
+            if(v == graph.length - 1) {
+                ans.add(currList);
+                continue;
+            }
+            for(int i : graph[v]) {
+                List<Integer> temp = new ArrayList<>(currList);
+                temp.add(i);
+                q.add(temp);
             }
         }
-        vis[src]=false;
-    }
+        return ans;
+   }    
 }
