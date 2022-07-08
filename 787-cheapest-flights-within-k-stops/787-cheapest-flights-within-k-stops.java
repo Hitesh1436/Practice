45d058827c6ae46399142f62acdbc2d1adbcb1d4
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-07-08 23:45:11 +0530

    Time: 6 ms (80.53%), Space: 46.6 MB (68.79%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.java
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.java
@@ -1,49 +1,25 @@
-class Solution {
-    class Pair{
-        int v;
-        int wt;
-        int dist;
-        
-        Pair(int v, int wt, int dist){
-            this.v = v;
-            this.wt = wt;
-            this.dist = dist;
-        }
-    }
+class Solution{
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        ArrayList<ArrayList<Pair>> graph = new ArrayList<>();
+        int[] prev = new int[n];
+        Arrays.fill(prev, Integer.MAX_VALUE);
+        prev[src] = 0;
         
-        for(int i = 0; i < n; i++){
-            graph.add(new ArrayList<>());
-        }
-        
-        for(int[] edge : flights){
-            int u = edge[0];
-            int v = edge[1];
-            int wt = edge[2];
+        for(int i = 0 ; i <= k; i++){
+            int[] curr = new int[n];
+            for(int j = 0; j < n; j++)curr[j] = prev[j];
             
-            graph.get(u).add(new Pair(v, wt, 0));
-            
-        }
-        
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> a.wt - b.wt);
-        pq.add(new Pair(src, 0, -1));
-        int[] stop = new int[n];
-        Arrays.fill(stop, Integer.MAX_VALUE);
-        
-        while(pq.size() > 0){
-            Pair p = pq.remove();
-            if(p.dist > k || stop[p.v] < p.dist)continue;
-            
-            stop[p.v] = p.dist;
-            if(p.v == dst)return p.wt;
-
-            for(Pair nbr : graph.get(p.v)){
-                pq.add(new Pair(nbr.v, p.wt + nbr.wt, p.dist + 1));
+            for(int[] edge : flights){
+                int u = edge[0];
+                int v = edge[1];
+                int wt = edge[2];
                 
+                if(prev[u] != Integer.MAX_VALUE && prev[u] + wt < curr[v]){
+                    curr[v] = prev[u] + wt;
+                }
             }
-            
+            for(int j = 0; j < n; j++)prev[j] = curr[j];
         }
-        return -1;
+        
+        return prev[dst] == Integer.MAX_VALUE ? -1 : prev[dst];
     }
 }
