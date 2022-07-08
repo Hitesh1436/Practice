6b99f84cdc9b29acc3858f73e80154af7329ba89
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-07-08 10:02:02 +0530

    Time: 71 ms (30.94%), Space: 49 MB (48.44%) - LeetHub

--- 1473-paint-house-iii/1473-paint-house-iii.java
+++ 1473-paint-house-iii/1473-paint-house-iii.java
@@ -1,49 +1,44 @@
-// Top-down approach with memoization:
-class Solution {
-    
-    private int houses[], cost[][], m, n, target, memo[][][];
-    private static final int MAX = Integer.MAX_VALUE;
-    
+// Bottom-up approach:
+class Solution {    
     public int minCost(int[] houses, int[][] cost, int m, int n, int target) {
-        this.houses = houses;
-        this.cost = cost;
-        this.m = m;
-        this.n = n;
-        this.target = target;
-        this.memo = new int[m][n + 1][target + 1];
-        return dp(0, 0, 0);
-    }
-    
-    private int dp(int i, int color, int neighborhood) {
-        if (i == m)
-            return neighborhood != target ? -1 : 0;
+        int MAX = Integer.MAX_VALUE;
+        int[][][] dp = new int[m + 1][n + 1][target + 2];
         
-        if (neighborhood > target)
-            return -1;
-        
-        if (memo[i][color][neighborhood] != 0)
-            return memo[i][color][neighborhood];
-        
-        int min = MAX;
-        if (houses[i] == 0) {
-            for (int clr = 1; clr <= n; clr++) {
-                int price = 0;
-                if (clr == color)
-                    price = dp(i + 1, clr, neighborhood);
-                else
-                    price = dp(i + 1, clr, neighborhood + 1);
-                
-                if (price >= 0)
-                    min = Math.min(min, cost[i][clr - 1] + price);
-            }    
-        } else {
-            if (houses[i] == color)
-                min = dp(i + 1, houses[i], neighborhood);
-            else
-                min = dp(i + 1, houses[i], neighborhood + 1);
+        for (int i = m; i >= 0; i--) {
+            for (int color = n; color >= 0; color --) {
+                for (int neighborhood = target + 1; neighborhood >= 0; neighborhood--) {
+                    if (i == m) {
+                        dp[i][color][neighborhood] = neighborhood != target ? -1 : 0;
+                        continue;
+                    }
+                    
+                    if (neighborhood > target) {
+                        dp[i][color][neighborhood] = -1;
+                        continue;
+                    }
+                    
+                    int min = MAX;
+                    if (houses[i] == 0) {
+                        for (int clr = 1; clr <= n; clr++) {
+                            int price = 0;
+                            if (clr == color)
+                                price = dp[i + 1][clr][neighborhood];
+                            else
+                                price = dp[i + 1][clr][neighborhood + 1];
+                            
+                            if (price >= 0)
+                                min = Math.min(min, cost[i][clr - 1] + price);
+                        }
+                    } else {
+                        if (houses[i] == color)
+                            min = dp[i + 1][houses[i]][neighborhood];
+                        else
+                            min = dp[i + 1][houses[i]][neighborhood + 1];
+                    }
+                    dp[i][color][neighborhood] = min == MAX ? -1 : min;
+                }
+            }
         }
-        
-        memo[i][color][neighborhood] = min == MAX ? -1 : min;
-        return memo[i][color][neighborhood];
+        return dp[0][0][0];
     }
 }
