f955b307babd929ad1307e609dbd1b9d3b1d698a
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-07-09 09:39:59 +0530

    Time: 37 ms (26.84%), Space: 46.1 MB (49.41%) - LeetHub

--- 753-cracking-the-safe/753-cracking-the-safe.java
+++ 753-cracking-the-safe/753-cracking-the-safe.java
@@ -2,10 +2,10 @@ class Solution {
     public String crackSafe(int n, int k) {
         StringBuilder sb = new StringBuilder();
         for(int i=0;i<n;i++){
-            sb.append(0);
+            sb.append(0);  // sare 0 vala pswrd bnlia start mn 
         }
         HashSet<String> vis = new HashSet<>();
-        vis.add(sb.toString());
+        vis.add(sb.toString());  // vis mn vo 0 vala dldia
         
         int limit = (int)Math.pow(k,n);
         dfs(sb,vis,limit,n,k);
@@ -16,16 +16,17 @@ class Solution {
         if(vis.size()== lim){
             return true;  // mtlb answer milgya 
         }
-        String lnm1 = sb.substring(sb.length()-(n-1));  // lnm1-> length n minus one mtlb last ke n-1 change krna h new pwsrd bnne ke liye 
+        String lnm1 = sb.substring(sb.length()-(n-1));  // lnm1-> length n minus one mtlb last ke n-1 change krna h like kbhi 0 ya kbhi 1 add krke new pwsrd bnne ke liye 
         for(int i=0;i<k;i++){
             String npwd = lnm1 + i; // npwd - > new password
-            if(!vis.contains(npwd)){
+            if(!vis.contains(npwd)){  // agr new pswrd ni pda toh dalta hun usse
                 vis.add(npwd);
                 sb.append(i);
                 boolean flag = dfs(sb,vis,lim,n,k);
                 if(flag == true){
                     return true;
                 }else{
+                    // mtlb vis ke andr jo pswrd dala vo glt tha vo htaya vis mn se and sb se bhi delete kia
                     vis.remove(npwd);
                     sb.deleteCharAt(sb.length()-1);
                 }
