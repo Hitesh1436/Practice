c45ce942e942fdd023d70251d675f205d6615497
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-07-09 10:48:48 +0530

    Time: 10 ms (46.20%), Space: 46.8 MB (64.59%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.java
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.java
@@ -1,27 +1,27 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        int[] dist = new int[n];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[src] = 0;
+        int []dis = new int[n];  // dis -> distance array bnya h
+        Arrays.fill(dis,Integer.MAX_VALUE);  // sbpr infinity dldia
+        dis[src] =0;  // src vale pr  dldia bcz src to src dis 0 hoga
         
-        for(int i = 1; i <= k + 1; i++){
-            int[] dp1 = dist.clone();
-            for(int[] flight: flights){
-                int u = flight[0];
-                int v= flight[1];
-                int uv = flight[2];
+        for(int i=1;i<=k+1;i++){
+            // k+1 tk chlega tbhi k stops milenge hume src and dest ko htakr
+            int []dp1 = dis.clone(); // 2nd array bnai dis jsi h blkul abi tk to update dis 
+            for(int []fl : flights){
+                int u = fl[0];
+                int v = fl[1];
+                int uv = fl[2];
                 
-                if(dist[u] != Integer.MAX_VALUE){
-                    dp1[v] = Math.min(dp1[v], dist[u] + uv);
+                if(dis[u] != Integer.MAX_VALUE){
+                    dp1[v] = Math.min(dp1[v],dis[u]+uv);  // agr MAX se km h toh update krdia dis nikalkr
                 }
             }
-            dist = dp1;
+            dis = dp1;  
         }
-        
-        if(dist[dst] == Integer.MAX_VALUE){
-            return -1;
-        } else {
-            return dist[dst];
+        if(dis[dst] == Integer.MAX_VALUE){
+                return -1;
+            }else{
+                return dis[dst];
         }
     }
 }
