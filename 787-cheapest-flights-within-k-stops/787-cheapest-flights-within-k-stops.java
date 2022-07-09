efb208f487dd16d1207264e82cf8f8df267f54aa
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-07-09 09:44:16 +0530

    Time: 16 ms (26.37%), Space: 46.1 MB (80.45%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.java
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.java
@@ -1,25 +1,27 @@
-class Solution{
+class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        int[] prev = new int[n];
-        Arrays.fill(prev, Integer.MAX_VALUE);
-        prev[src] = 0;
+        int[] dist = new int[n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[src] = 0;
         
-        for(int i = 0 ; i <= k; i++){
-            int[] curr = new int[n];
-            for(int j = 0; j < n; j++)curr[j] = prev[j];
-            
-            for(int[] edge : flights){
-                int u = edge[0];
-                int v = edge[1];
-                int wt = edge[2];
+        for(int i = 1; i <= k + 1; i++){
+            int[] dp1 = dist.clone();
+            for(int[] flight: flights){
+                int u = flight[0];
+                int v= flight[1];
+                int uv = flight[2];
                 
-                if(prev[u] != Integer.MAX_VALUE && prev[u] + wt < curr[v]){
-                    curr[v] = prev[u] + wt;
+                if(dist[u] != Integer.MAX_VALUE){
+                    dp1[v] = Math.min(dp1[v], dist[u] + uv);
                 }
             }
-            for(int j = 0; j < n; j++)prev[j] = curr[j];
+            dist = dp1;
         }
         
-        return prev[dst] == Integer.MAX_VALUE ? -1 : prev[dst];
+        if(dist[dst] == Integer.MAX_VALUE){
+            return -1;
+        } else {
+            return dist[dst];
+        }
     }
 }
