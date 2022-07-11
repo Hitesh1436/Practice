b795d2d5950f1cfcc785e808bc5c50bef513f74d
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-07-11 17:02:07 +0530

    Time: 42 ms (49.50%), Space: 53.7 MB (78.02%) - LeetHub

--- 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
+++ 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
@@ -1,42 +1,36 @@
 class Solution {
-    class Tuple {
-        int row, col, cost;
-        public Tuple(int r, int c, int co) {
-            row = r;
-            col = c;
-            cost = co;
-        }
-    }
+    // 1: right  2: left  3: down  4: up
+    private static final int[][] DIRECTIONS = {{0,1}, {0,-1}, {1,0}, {-1,0}};
     public int minCost(int[][] grid) {
-        int rows = grid.length, columns = grid[0].length;
-        int targetRow = grid.length-1, targetColumn = grid[0].length-1;
+        int m = grid.length, n = grid[0].length;
         
-        boolean[][] traversed = new boolean[rows][columns];
-        PriorityQueue<Tuple> pq = new PriorityQueue<>((a, b)->(a.cost - b.cost));
+        int[][] costs = new int[m][n];
+        for (int[] c : costs) Arrays.fill(c, Integer.MAX_VALUE);
+        costs[0][0] = 0;
         
-        pq.add(new Tuple(0, 0, 0));
+        // min-heap storing triplets 0f {x, y, cost to reach (x,y) from (0,0)}
+        PriorityQueue<int[]> heap = new PriorityQueue<>((a,b) -> a[2] - b[2]);
+        heap.offer(new int[]{0, 0, 0});
         
-        while(!pq.isEmpty()) {
-            Tuple top = pq.poll();
-            int row = top.row, col = top.col;
-            traversed[row][col] = true;
-            if(row == targetRow && col == targetColumn) {
-                return top.cost;
+        while (!heap.isEmpty()) {
+            int[] curr = heap.poll();
+            int x = curr[0], y = curr[1], cost = curr[2];
+            
+            if (x == m-1 && y == n-1) return cost;
+            
+            
+            for (int i = 0; i < 4; i++) {
+                int[] dir = DIRECTIONS[i];
+                int newX = x + dir[0], newY = y + dir[1];
+                if (newX < 0 || newY < 0 || newX >= m || newY >= n) continue;
+                
+                int newCost = cost + (i + 1 == grid[x][y] ? 0 : 1); 
+                if (costs[newX][newY] > newCost) {
+                    costs[newX][newY] = newCost;
+                    heap.offer(new int[]{newX, newY, newCost});
+                }
             }
-			// traverse neighbors
-            if(col + 1 != columns && !traversed[row][col+1]) 
-                pq.add(new Tuple(row, col+1, (grid[row][col] == 1 ? 0 : 1) + top.cost ));
-            
-            if(col - 1 != -1 && !traversed[row][col-1]) 
-                pq.add(new Tuple(row, col-1, (grid[row][col] == 2 ? 0 : 1) + top.cost ));
-            
-            if(row + 1 != rows && !traversed[row+1][col]) 
-                pq.add(new Tuple(row+1, col, (grid[row][col] == 3 ? 0 : 1) + top.cost ));
-				
-            if(row - 1 != -1 && !traversed[row-1][col]) 
-                pq.add(new Tuple(row-1, col, (grid[row][col] == 4 ? 0 : 1) + top.cost ));
-            
-        }
-        return 0;
+        }  
+        return -1;
     }
 }
