6fc940ec76ba5a79d2929a135215bde7bbdd65a9
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-07-12 22:19:30 +0530

    Added solution - LeetHub

--- Find the Maximum Flow - GFG/find-the-maximum-flow.java
+++ Find the Maximum Flow - GFG/find-the-maximum-flow.java
@@ -32,59 +32,85 @@ class GFG{
 
 
 //User function Template for Java
-class Solution{ 
-    int findMaxFlow(int N, int M, ArrayList<ArrayList<Integer>> Edges){ 
-        int g[][] = new int[N][N];
-        for(int i=0;i<Edges.size();i++){
-            int u = Edges.get(i).get(0)-1;
-            int v = Edges.get(i).get(1)-1;
-            int w = Edges.get(i).get(2);
-            g[u][v]+=w;
-            g[v][u]+=w;
+class Solution 
+{ 
+    int findMaxFlow(int N, int M, ArrayList<ArrayList<Integer>> Edges) 
+    { 
+        int mflow = 0;
+        
+        int[][] graph = new int[N + 1][N + 1];
+        int[][] rgraph = new int[N + 1][N + 1];
+        for(ArrayList<Integer> edge: Edges){
+            int u = edge.get(0);
+            int v = edge.get(1);
+            int w = edge.get(2);
+            
+            graph[u][v] += w;
+            graph[v][u] += w;
+            
+            rgraph[u][v] += w;
+            rgraph[v][u] += w;
         }
-        return ford(g,0,N-1,N);
-    }
-    int ford(int[][] g,int source,int sink,int n){
-        int[] p=new int[n];
-        Arrays.fill(p,-1);
-        int res=0;
-        while(bfs(g,p,source,sink,n)!=0){
-            int mn = bfs(g,p,source,sink,n);
-            res+=mn;
-            int v= sink;
-            while(v!=source){
-                int u=p[v];
-                g[u][v]-=mn;
-                g[v][u]+=mn;
-                v=p[v];
+        
+        int[] par = new int[N + 1];
+        while(pathFound(par, rgraph) == true){
+            int flow = Integer.MAX_VALUE;
+            
+            int v = N;
+            while(v != 1){
+                int pv = par[v];
+                int price = rgraph[pv][v];
+                if(price < flow){
+                    flow = price;
+                }
+                
+                v = pv;
             }
+            
+            v = N;
+            while(v != 1){
+                int pv = par[v];
+                rgraph[pv][v] -= flow;
+                rgraph[v][pv] += flow;
+                
+                v = pv;
+            }
+            
+            
+            mflow += flow;
         }
-        return res;
+        
+        return mflow;
     }
-    int bfs(int[][] g,int[] p,int source,int sink,int n){
-        int mn=Integer.MAX_VALUE;
-        Arrays.fill(p,-1);
-        boolean[] vis=new boolean[n];
-        Queue<Integer> q=new LinkedList<>();
-        q.add(source);
-        vis[source]=true;
-        p[source]=-1;
-        while(!q.isEmpty()){
-            int u = q.remove();
-            for(int v=0;v<n;v++){
-                if(vis[v]==false && g[u][v]!=0){
-                    if(v==sink){
-                        p[v]=u;
-                        mn = Math.min(mn,g[u][v]);
-                        return mn;
-                    }
-                    q.add(v);
-                    vis[v]=true;
-                    mn=Math.min(mn,g[u][v]);
-                    p[v]=u;
+    
+    boolean pathFound(int[] par, int[][] rgraph){
+        ArrayDeque<Integer> queue = new ArrayDeque<>();
+        boolean[] vis = new boolean[par.length];
+        
+        queue.add(1);
+        while(queue.size() != 0){
+            int rem = queue.remove();
+            
+            if(vis[rem]){
+                continue;
+            }
+            vis[rem] = true;
+            
+            if(rem == par.length - 1){
+                return true;
+            }
+            
+            for(int n = 1; n < rgraph.length; n++){
+                if(rgraph[rem][n] > 0 && !vis[n]){
+                    queue.add(n);
+                    par[n] = rem;
                 }
             }
         }
-        return 0;
+        
+        
+        return false;
     }
+    
+    
 }
