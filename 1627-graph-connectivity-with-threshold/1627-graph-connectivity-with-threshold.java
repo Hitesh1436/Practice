9e9c0a30885e85a5b6beb505a67b4ce33f46cfbc
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-07-12 12:47:31 +0530

    Time: 13 ms (62.89%), Space: 90.6 MB (25.77%) - LeetHub

--- 1627-graph-connectivity-with-threshold/1627-graph-connectivity-with-threshold.java
+++ 1627-graph-connectivity-with-threshold/1627-graph-connectivity-with-threshold.java
@@ -1,50 +1,50 @@
 class Solution {
-    public int find(int x){
-        if(parent[x] == x)
-            return x;
-        //Optimising by placing the same parent for all the elements to reduce reduntant calls
-        return parent[x] = find(parent[x]);
-    }
-    public void union(int a, int b){
-        a = find(a);
-        b = find(b);
-        //Using Rank optimisation
-        if(rank[a] > rank[b]){
-            parent[b] = a;
-            rank[a] += rank[b];
-        }
-        else{
-            parent[a] = b;
-            rank[b] += rank[a];
-        }
-        //parent[b] = a;
-    }
-    int parent[];
-    int rank[];
     public List<Boolean> areConnected(int n, int threshold, int[][] queries) {
-        List<Boolean> ans = new ArrayList<Boolean>();
-        parent = new int[n+1];
-        rank = new int[n+1];
-        for(int i=1; i<=n; i++){
-            //Each element is its own parent
-            parent[i] = i;
-            //At beginning each element has rank 1
-            rank[i] = 1;
+        p = new int[n + 1];
+        r = new int[n + 1];
+        for(int i = 0; i < p.length; i++){
+            p[i] = i;
+            r[i] = 0;
         }
-        // Finding the possible divisors with pairs above given threshold
-        for(int th = threshold+1; th<=n; th++){
-            
-            int mul = 1;
-            while(mul * th <= n){
-                //If possible pair then making a union of those paired element
-                union(th, mul*th);
-                mul++;
+        for(int div = threshold + 1; div <= n; div++){
+            for(int m = 1; div * m <= n; m++){
+                union(div, div * m);
             }
         }
-        //Generating ans for all possible queries
-        for(int[] query : queries){
-            ans.add((find(query[0]) == find(query[1])));
-        }
-        return ans;
+        ArrayList<Boolean> res = new ArrayList<>();
+        
+        for(int[] query: queries){
+            int n1 = query[0];
+            int n2 = query[1];
+            boolean flag = (find(n1) == find(n2));
+            res.add(flag);
+        } 
+        return res;
     }
+    int[] p;
+    int[] r;
+    int find(int x){
+        if(p[x] == x){
+            return x;
+        } else {
+            p[x] = find(p[x]);
+            return p[x];
+        }
+    }
+    void union(int X, int Y){
+        int x = find(X);
+        int y = find(Y);
+        if(x == y){
+            return;
+        }
+        if(r[x] < r[y]){
+            p[x] = y;
+        } else if(r[y] < r[x]){
+            p[y] = x;
+        } else {
+            p[y] = x;
+            r[x]++;
+        }
+    }
+    
 }
