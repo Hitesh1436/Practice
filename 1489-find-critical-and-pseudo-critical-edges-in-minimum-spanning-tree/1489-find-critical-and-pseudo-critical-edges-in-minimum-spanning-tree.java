5bf1aabc22d7a33c8ffe73b4785fa02924f8d54a
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-07-14 15:44:58 +0530

    Time: 208 ms (9.46%), Space: 42.7 MB (96.62%) - LeetHub

--- 1489-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree/1489-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree.java
+++ 1489-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree/1489-find-critical-and-pseudo-critical-edges-in-minimum-spanning-tree.java
@@ -1,113 +1,132 @@
 class Solution {
-	public List<List<Integer>> findCriticalAndPseudoCriticalEdges(int n, int[][] edges) {
-		int[][][] g = new int[n][n][2]; //{w,ind}
-		for(int i = 0; i < edges.length; i ++) {
-			int[] e = edges[i];
-			int f = e[0];
-			int t = e[1];
-			int w = e[2];
-			g[f][t][0] = w;
-			g[t][f][0] = w;
-			g[f][t][1] = i;
-			g[t][f][1] = i;
-		}
-
-		List<Integer>[] mst = new List[n];
-		for(int i = 0; i < n; i ++) {
-			mst[i] = new LinkedList<>();
-		}
-
-		boolean[] mstSet = new boolean[edges.length];
-
-        Arrays.sort(edges, (a,b) -> Integer.compare(a[2], b[2]));
+    static class UnionFind {
+        int[] p;
+        int[] r;
+        int comps = 0;
         
-        buildMST(n, edges, mstSet, mst, g);
-		
-		List<List<Integer>> ans = new ArrayList<>(2);
-		Set<Integer> pce = new HashSet<>();
-		List<Integer> ce = new LinkedList<>();
-		
-		// psuedo critical edges
-		for(int i = 0; i < edges.length; i ++) {
-            int f = edges[i][0], t = edges[i][1];
-            int w = edges[i][2];
-            int ind = g[f][t][1];
-			if(!mstSet[ind]) {
-                Set<Integer> cur = new HashSet<>();
-				boolean p = path(f, t, w, -1, mst, g, cur);
-                if(p && cur.size() > 0) {
-                    pce.addAll(cur);
-                    pce.add(ind);
-                }
-				if(!p) {
-					System.out.println("Should not reach here");
-				}
-			}
-			
-		}
-		// critical edges
-		for(int i = 0; i < edges.length; i ++) {
-            int f = edges[i][0], t = edges[i][1];
-            int w = edges[i][2];
-            int ind = g[f][t][1];
-			if(mstSet[ind] && !pce.contains(ind)) {
-				ce.add(ind);
-			}
-		}
-		ans.add(ce);
-		ans.add(new LinkedList<>(pce));
-		return ans;
-	}
-	boolean path(int f, int t, int w, int p, List<Integer>[] mst, int[][][] g, Set<Integer> ind) {
-		if(f == t) {
-			return true; 
-		}
-		for(int nbr: mst[f]) {
-			if(p != nbr) {
-				if(path(nbr, t, w, f, mst, g, ind)) {
-					if(g[f][nbr][0] == w) {
-						ind.add(g[f][nbr][1]);
-					}
-					return true;
-				}
-				
-			}
-		}
-		return false;
-	}
-    private void buildMST(int n, int[][] edges, boolean[] mste, List<Integer>[] mstg, int[][][] g){
-        int weight = 0; 
-        DisjointSet ds = new DisjointSet(n);
-
-        for (int i = 0; i < edges.length; i++) {
-            if (ds.union(edges[i][0], edges[i][1])) {
-                weight += edges[i][2];
-                int[] edge = edges[i];
-                mstg[edge[0]].add(edge[1]);
-                mstg[edge[1]].add(edge[0]);
-                mste[g[edge[0]][edge[1]][1]] = true;
+        UnionFind(int n){
+            p = new int[n];
+            r = new int[n];
+            comps = n;
+            for(int i = 0; i < p.length; i++){
+                p[i] = i;
+                r[i] = 0;
             }
         }
+        
+        int find(int x){
+            if(p[x] == x){
+                return x;
+            } else {
+                p[x] = find(p[x]);
+                return p[x];
+            }
+        }
+        
+        boolean union(int X, int Y){
+            int x = find(X);
+            int y = find(Y);
+            if(x == y){
+                return false;
+            }
+            
+            if(r[x] < r[y]){
+                p[x] = y;
+            } else if(r[y] < r[x]){
+                p[y] = x;
+            } else {
+                p[y] = x;
+                r[x]++;
+            }
+            comps--;
+            
+            return true;
+        }
+        
+        boolean isConnected(){
+            return comps == 1;
+        }
     }
-}
-class DisjointSet {
-    int[] parent;
     
-    public DisjointSet(int n) {
-        parent = new int[n];
-        for (int i = 0; i < n; i++) {
-            parent[i] = i;
+    static class Edge implements Comparable<Edge> {
+        int u;
+        int v;
+        int wt;
+        
+        Edge(int u, int v, int wt){
+            this.u = u;
+            this.v = v;
+            this.wt = wt;
+        }
+        
+        public int compareTo(Edge o){
+            return this.wt - o.wt;
         }
     }
-    public int find(int i) {
-        return (i == parent[i]) ? i : (parent[i] = find(parent[i]));
-    }
-    public boolean union(int u, int v) {
-        int pu = find(u), pv = find(v);
-        if (pu == pv) {
-            return false;
+    
+    public int buildMST(int n, int[][] edges, int[] edgeToSkip, int[] edgeToPick){
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        
+        for(int[] edge: edges){
+            if(edge == edgeToSkip){
+                continue;
+            } else if(edge == edgeToPick){
+                continue;
+            }
+            
+            int u = edge[0];
+            int v = edge[1];
+            int wt = edge[2];
+            pq.add(new Edge(u, v, wt));
         }
-        parent[pu] = pv;
-        return true;
+        
+        UnionFind uf = new UnionFind(n);
+        int cost = 0;
+        
+        if(edgeToPick != null){
+            uf.union(edgeToPick[0], edgeToPick[1]);
+            cost += edgeToPick[2];
+        }
+        
+        while(pq.size() > 0){
+            Edge rem = pq.remove();
+            if(uf.union(rem.u, rem.v) == true){
+                cost += rem.wt;
+            }
+        }
+        
+        if(uf.isConnected() == true){
+            return cost;
+        } else {
+            return Integer.MAX_VALUE;
+        }
+    }
+    
+    public List<List<Integer>> findCriticalAndPseudoCriticalEdges(int n, int[][] edges) {
+        int mstCost = buildMST(n, edges, null, null);
+        
+        ArrayList<Integer> critical = new ArrayList<>();
+        ArrayList<Integer> pcritical = new ArrayList<>();
+        
+        for(int i = 0; i < edges.length; i++){
+            int[] edge = edges[i];
+            
+            int mstCostWithoutEdge = buildMST(n, edges, edge, null);
+            if(mstCostWithoutEdge > mstCost){
+                critical.add(i);
+            } else {
+                int mstCostWithEdge = buildMST(n, edges, null, edge);
+                if(mstCostWithEdge > mstCost){
+                    // redundant
+                } else {
+                    pcritical.add(i);
+                }
+            }
+        }
+        
+        List<List<Integer>> res = new ArrayList<>();
+        res.add(critical);
+        res.add(pcritical);
+        return res;
     }
 }
