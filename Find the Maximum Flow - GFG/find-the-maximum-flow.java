113612e83381513483ff99b07d9d5701033208ad
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-07-14 15:40:33 +0530

    Added solution - LeetHub

--- Find the Maximum Flow - GFG/find-the-maximum-flow.java
+++ Find the Maximum Flow - GFG/find-the-maximum-flow.java
@@ -34,83 +34,71 @@ class GFG{
 //User function Template for Java
 class Solution 
 { 
-    int findMaxFlow(int N, int M, ArrayList<ArrayList<Integer>> Edges) 
-    { 
-        int mflow = 0;
+    int findMaxFlow(int N, int M, ArrayList<ArrayList<Integer>> Edges){  
+        int mflow = 0;                        // maximum flow
         
-        int[][] graph = new int[N + 1][N + 1];
-        int[][] rgraph = new int[N + 1][N + 1];
-        for(ArrayList<Integer> edge: Edges){
-            int u = edge.get(0);
-            int v = edge.get(1);
-            int w = edge.get(2);
+        int [][] graph = new int[N+1][N+1];   // original graph
+        int [][] rgraph = new int[N+1][N+1];  // residual graph 
+        for(ArrayList<Integer>e : Edges){
+            int u = e.get(0);
+            int v = e.get(1);
+            int w = e.get(2);
             
             graph[u][v] += w;
             graph[v][u] += w;
             
             rgraph[u][v] += w;
             rgraph[v][u] += w;
-        }
-        
-        int[] par = new int[N + 1];
-        while(pathFound(par, rgraph) == true){
-            int flow = Integer.MAX_VALUE;
-            
-            int v = N;
-            while(v != 1){
-                int pv = par[v];
-                int price = rgraph[pv][v];
-                if(price < flow){
-                    flow = price;
+        }  
+            int []par = new int[N+1];  // parent dkhne ke ki kis kiske bich mn edge h
+            while(pathFound(par,rgraph)==true){
+                int flow = Integer.MAX_VALUE;
+                
+                int v = N;  // last vertex pr rkha 
+                while(v!= 1){
+                    int pv = par[v];  // pv-> parent of v niklege ese
+                    int price = rgraph[pv][v];
+                    if(price < flow){
+                        flow = price;
+                    }
+                    v = pv;
+                }
+                v = N;
+                // ulta jatey hue sbki capacity bhi km krni hogi  and ulti direction mn add krenge capacity
+                while(v != 1){
+                    int pv = par[v];  // pv-> parent of v niklege ese
+                   rgraph[pv][v] -= flow;
+                   rgraph[v][pv] += flow;
+                   
+                   v = pv;
                 }
                 
-                v = pv;
+                mflow += flow;
             }
-            
-            v = N;
-            while(v != 1){
-                int pv = par[v];
-                rgraph[pv][v] -= flow;
-                rgraph[v][pv] += flow;
-                
-                v = pv;
-            }
-            
-            
-            mflow += flow;
-        }
-        
         return mflow;
     }
-    
-    boolean pathFound(int[] par, int[][] rgraph){
-        ArrayDeque<Integer> queue = new ArrayDeque<>();
-        boolean[] vis = new boolean[par.length];
+    boolean pathFound(int []par,int [][]rgraph){
+        ArrayDeque<Integer>qu = new ArrayDeque<>();
+        boolean []vis = new boolean[par.length];
         
-        queue.add(1);
-        while(queue.size() != 0){
-            int rem = queue.remove();
+        qu.add(1);
+        while(qu.size()>0){
+            int rem = qu.remove();
             
             if(vis[rem]){
                 continue;
             }
             vis[rem] = true;
-            
-            if(rem == par.length - 1){
+            if(rem == par.length-1){
                 return true;
             }
-            
-            for(int n = 1; n < rgraph.length; n++){
-                if(rgraph[rem][n] > 0 && !vis[n]){
-                    queue.add(n);
+            for(int n=1;n<rgraph.length;n++){
+                if(rgraph[rem][n]>0 && !vis[n]){
+                    qu.add(n);
                     par[n] = rem;
                 }
             }
         }
-        
-        
         return false;
     }
-    
-    
 }
