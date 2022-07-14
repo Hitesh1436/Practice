a4a4980d11ed84d1f69d4d6b9c09473ca05f7ede
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-07-14 23:17:32 +0530

    Time: 93 ms (31.66%), Space: 53.8 MB (27.31%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -1,53 +1,86 @@
-class Solution{ 
-    public int findTheCity(int n, int[][] edges, int distanceThreshold){ 
-    //T->  O( V^3 ),  S-> O( V^2 ) 
-        int[][] D= new int[n][n];//our matrix, containing all vertex shortest path cost 
-        for ( int[] row: D ){
-            //filling each row of the matrix with +infinity as the distance is unreachable till now, since we have not accesed the edge list
-            //when we access the edge list the actual cost will be added to the matrix cell
-             Arrays.fill(row, Integer.MAX_VALUE);
+class Solution {
+    public int findTheCity(int N, int[][] edges, int distanceThreshold) {
+        Map<Integer, List<int[]>> graph = new HashMap<>(); // Initialize the Graph
+
+        for(int[] edge : edges){ // Add Each edge entry in both Cities as graph is bidirectional
+
+            graph.putIfAbsent(edge[0], new ArrayList<>()); // Add the first City if not already present
+            graph.putIfAbsent(edge[1], new ArrayList<>()); // Add the second City if not already present
+            graph.get(edge[0]).add(new int[]{edge[1], edge[2]}); // Add edge from City A to City B
+            graph.get(edge[1]).add(new int[]{edge[0], edge[2]}); // Add edge from City B to City A
+
         }
-        for ( int[] edge: edges){
-        //adding the cost to the matrix cell   
-            int from= edge[0];
-            int to= edge[1];
-            int wt= edge[2];
-            
-            //undirected edge 
-            D[from][to]= wt;
-            D[to][from]= wt;
-        }
-    
-        //D -> D0 -> D1 -> D2 -> D3 -> D4 -> ......Dk//At every stage minimizing the cost 
-        for ( int k= 0; k< n; k++ ){//evaluating each vertex shortest path //intermediate vertex 
-            for ( int i= 0; i< n; i++ ){//Exploring all the starting vertex 
-                for ( int j= 0; j< n; j++ ){//Exploring all the ending vertex 
-                    if ( D[i][k] == Integer.MAX_VALUE || D[k][j] == Integer.MAX_VALUE)//if we cannot reach the k vertex from ith vertex(source) or if we cannot reach j(dest) from k, we just move on, means thats its ureachable 
-                        continue;
-                    else if ( ( D[i][k]+ D[k][j] ) < D[i][j] )//if including the vertex k reduce the current cost we replace it with the current cost( arr[i][j] ) //Greedy Approach 
-                        D[i][j]= D[i][k] + D[k][j];  
+
+        int res = Integer.MAX_VALUE; // res represents the least count seen in the loop
+        int city = Integer.MIN_VALUE; // city represents the city with the least count
+
+        for(int i = 0; i < N; ++i){ // We implement Dijkstra's for each City from 0 to (N - 1)
+
+            /* count for the current city i.
+               We start from -1 to not count the current city we are in
+             */
+            int count = -1;
+
+            // Priority Queue must compare the weights, which is the second param in our passed int[] array
+            PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+
+            // Holds minimum Distances from the i'th city to all other cities. Initially will be filled with Int Max
+            int[] minDistances = new int[N];
+            Arrays.fill(minDistances, Integer.MAX_VALUE);
+
+            // Starting city's (i) distance will be Zero
+            minDistances[i] = 0;
+
+            // int[i][0] represents int[startingCity][startingWeight]
+            pq.add(new int[]{i, 0});
+
+            while(!pq.isEmpty()){
+                int[] curr = pq.poll();
+                int currCity = curr[0], currWeight = curr[1];
+
+                /* if the currWeight > minDistance it means this city can
+                   be skipped as we already have a more optimal solution.
+                   This check also acts as a check for whether a city has already been
+                   visited or not and hence we need not use a boolean[] visited array
+                 */
+                if(currWeight > minDistances[currCity]) continue;
+
+                if(!graph.containsKey(currCity)) continue; // Check if graph contains currCity or not
+
+                // If the currWeight is less than or equal to threshold, we can increment the count
+                if(currWeight <= distanceThreshold) count ++;
+
+                if(!graph.containsKey(currCity)) continue;
+
+                /* For Each Neighbouring city from the currCity,
+                   we implement the Relaxation part of Dijkstra's Algorithm
+                 */
+                for(int[] neighbours : graph.get(currCity)){
+
+                    // Get the Neighbouring City and the weight of the edge connecting it to currCity
+                    int newCity = neighbours[0], newWeight = neighbours[1];
+
+                    /* Check if the currWeight and the weight to the next city(newCity) < value of distance to
+                       reach the new City present in minDistances Array
+                     */
+                    if(newWeight + currWeight < minDistances[newCity]){
+
+                        // Update minDistances Array
+                        minDistances[newCity] = newWeight + currWeight;
+
+                        // Add a new entry to the Priority Queue
+                        // PS : PriorityQueue is an unbounded Queue, so the methods .add() and .offer() are Synonymous
+                        pq.add(new int[]{newCity, newWeight + currWeight});
+                    }
                 }
             }
-        }
-        
-        int minNoOfCity= Integer.MAX_VALUE;//Identity of minimum 
-        int res= -1;//worst case, if we cannot find any city or vertex in the vicinity of our current vertex  
-        
-        //every row in our matrix consist of 1 vertex(row number) and it includes min cost to all other vertex as it is a n*n matrix
-        
-        for ( int i= 0; i< n; i++ ){//Starting vertex 
-            int noCityCount= 0;
-            for ( int j= 0; j< n; j++ ){//Ending vertex 
-                 if ( i == j ) //not consider the city inside city case 
-                      continue;
-               if ( D[i][j] <= distanceThreshold)//city within the our threshold value 
-                   noCityCount+= 1;//increasing the city count for the particular vertex 
-            }
-            if ( noCityCount <= minNoOfCity ){//updating the minimum city count vertex every time 
-                minNoOfCity= noCityCount;
-                res= i;//current vertex with min no city
+
+			// If curent city's count is less than least seen count(res), update res and city
+            if(count <= res){
+                res = count;
+                city = i;
             }
         }
-        return res;//returning the vertex with min no of city
+        return city;
     }
 }
