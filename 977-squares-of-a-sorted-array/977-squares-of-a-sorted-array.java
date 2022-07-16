8d1ceed013455d08a727795a66244004c441ec67
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-07-16 09:44:50 +0530

    Time: 1 ms (100.00%), Space: 43.5 MB (99.06%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
@@ -1,22 +1,17 @@
 class Solution {
-    public int[] sortedSquares(int[] A){
-	int[] res = new int[A.length];
-	int lo = 0; int hi = A.length - 1;
-	//iterate from n to 0
-	for (int i = A.length - 1; i >= 0; i--) {
-		//check if abs left is less than or equal to abs right
-		if (Math.abs(A[lo]) >= Math.abs(A[hi])) {
-			//add left squared to result array
-			res[i] = A[lo] * A[lo];
-			//increment left pointer
-			lo++;
-		} else {
-			//add right squared to result array
-			res[i] = A[hi] * A[hi];
-			//decrement right pointer
-			hi--;
-		}
-	}
-	return res;
-  }
+    public int[] sortedSquares(int[] nums) {
+        int l = 0;
+        int r = nums.length -1;
+        int []ans = new int[nums.length];
+        for(int i=nums.length-1 ;i>=0;i--){
+            if(Math.abs(nums[l])>=Math.abs(nums[r])){
+                ans[i] = nums[l]*nums[l];
+                l++;
+           }else{
+                ans[i] = nums[r] * nums[r];
+                r--;
+            }
+        }
+        return ans;
+    }
 }
