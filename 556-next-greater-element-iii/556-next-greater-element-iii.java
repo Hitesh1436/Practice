10a9f742126001e67467ae943609153c8eda102b
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-07-16 10:02:13 +0530

    Time: 0 ms (100.00%), Space: 38.7 MB (99.61%) - LeetHub

--- 556-next-greater-element-iii/556-next-greater-element-iii.java
+++ 556-next-greater-element-iii/556-next-greater-element-iii.java
@@ -1,32 +1,39 @@
 class Solution {
     public int nextGreaterElement(int n) {
-      String str = Integer.toString(n);
-    char [] arr = str.toCharArray();
-    // To find the dip             //45132 - At pos: of 1 dip occurs
-    int i = arr.length - 2;
-    while(i >= 0 && arr[i] >= arr[i + 1]){
-        i--;
-    }
-    if(i == -1)
-        return -1;  
-    //To Find the index of greater element than at idx i
-    int j = arr.length - 1;
-    while(arr[i] >= arr[j]){
-        j--;
-    }
-    //Swaping
-    char temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;          //45231
         
-    Arrays.sort(arr, i+1, arr.length);           //45213
-    //Result Concat in String
-    String res = new String(arr);
-    // To long
-    long sol = Long.parseLong(res);
-    if(sol <= Integer.MAX_VALUE){
-        return (int) sol;
+        int[] freq = new int[10];
+        
+        long temp = n;
+        int prev = 0;
+        while(prev <= temp%10){
+            prev = (int) temp%10;
+            freq[prev]++;
+            temp = temp/10;
+        }
+        if(temp == 0 ){
+            return -1;
+        }
+        int toReplace = (int) temp%10;
+        temp = temp/10;
+        freq[toReplace]++;
+        
+        for(int i=toReplace+1; i<10; i++){
+            if(freq[i]>0){
+                temp = temp*10 + i;
+                freq[i]--;
+                break;
+            }
+        }
+        for(int i=0;i<freq.length; i++){
+            while(freq[i]>0){
+                temp = temp*10 + i;
+                freq[i]--;
+            }
+        }
+        
+        if(temp>Integer.MAX_VALUE){
+            return -1;
+        }
+        return (int)temp;
     }
-    return -1;
-   }
 }
