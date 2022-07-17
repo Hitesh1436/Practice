8a9f64be83af3dc0d839e8bbb69b58d31e49aae7
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-07-17 23:21:25 +0530

    Time: 3 ms (17.75%), Space: 44.2 MB (47.49%) - LeetHub

--- 807-max-increase-to-keep-city-skyline/807-max-increase-to-keep-city-skyline.java
+++ 807-max-increase-to-keep-city-skyline/807-max-increase-to-keep-city-skyline.java
@@ -1,23 +1,27 @@
 class Solution {
     public int maxIncreaseKeepingSkyline(int[][] grid) {
-        if(grid.length ==0) return 0;
-        int n = grid.length;
-        int []maxrow = new int[n];
-         int []maxcol = new int[n];
+        // the highest each point can be is min(rowmax, colmax).
         
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                maxrow[i] = Math.max(maxrow[i],grid[i][j]);
-                maxcol[j] = Math.max(maxcol[j],grid[i][j]);
-            }
-         }   
-            int count =0;
-            for(int i=0;i<n;i++){
-                for(int j=0;j<n;j++){
-                    count +=(Math.min(maxrow[i],maxcol[j])-grid[i][j]);
-                }
-            }
+        int[] rowmaxes = new int[grid.length];
+        int[] colmaxes = new int[grid[0].length];
         
-        return count;
+        // get row and col maxima
+        for (int i = 0; i < grid.length; i++) {
+            rowmaxes[i] = grid[i][0];
+            for (int j = 0; j < grid[0].length; j++) {
+                if (i == 0) colmaxes[j] = grid[0][j];
+                if (grid[i][j] > rowmaxes[i]) rowmaxes[i] = grid[i][j];
+                if (grid[i][j] > colmaxes[j]) colmaxes[j] = grid[i][j];           
+            }
+        }  
+        // Sum the differences
+        int total = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                total += Math.min(rowmaxes[i], colmaxes[j]) - grid[i][j];
+            }
+        }
+        
+        return total;
     }
 }
