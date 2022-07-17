82f867e00a4ada6daeb72b2faca497d8d96432d0
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-07-17 17:08:48 +0530

    Time: 24 ms (93.98%), Space: 43.5 MB (87.95%) - LeetHub

--- 629-k-inverse-pairs-array/629-k-inverse-pairs-array.java
+++ 629-k-inverse-pairs-array/629-k-inverse-pairs-array.java
@@ -1,25 +1,20 @@
 class Solution {
     public int kInversePairs(int n, int k) {
-        if (k > n * (n - 1) / 2) // n numbers can generate at most n * (n - 1) / 2 inverse pairs
-            return 0;
-        
-        if (k == n * (n - 1) / 2 || k == 0)
-            return 1;
-        
-        int mod = 1000000007;
-        int[][] dp = new int[n + 1][k + 1];
-        
-        for (int i = 1; i < n + 1; i++) {
-            dp[i][0] = 1; // deal with j = 0
-            for (int j = 1; j < Math.min(k, i * (i - 1) / 2) + 1; j++) {
-                dp[i][j] = (dp[i][j - 1] + dp[i - 1][j] - (j >= i ? dp[i - 1][j - i] : 0)) % mod;
-                // all dp[i][j] modulo 10^9 + 7
-                // so dp[i - 1][j - 1] might bigger than dp[i][j - 1] + dp[i - 1][j]
-                if (dp[i][j] < 0) 
-                    dp[i][j] += mod;
+        int MOD = 1000000007;
+        int[][] opt = new int[k + 1][n];
+        for (int i = 0; i <= k; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0) {
+                    opt[i][j] = 1;
+                } else if (j > 0) {
+                    opt[i][j] = (opt[i - 1][j] + opt[i][j - 1]) % MOD;
+                    if (i >= j + 1) {
+                        opt[i][j] = (opt[i][j] - opt[i - j - 1][j - 1] + MOD) % MOD;
+                    }
+                }
             }
         }
-        
-        return dp[n][k];
+
+        return opt[k][n - 1];
     }
 }
