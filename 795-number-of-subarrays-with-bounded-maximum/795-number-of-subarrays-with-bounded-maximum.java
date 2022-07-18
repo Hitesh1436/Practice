208c647c0e85edd30b83e665783775add31d1a4b
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-07-18 22:32:22 +0530

    Time: 3 ms (100.00%), Space: 72 MB (8.61%) - LeetHub

--- 795-number-of-subarrays-with-bounded-maximum/795-number-of-subarrays-with-bounded-maximum.java
+++ 795-number-of-subarrays-with-bounded-maximum/795-number-of-subarrays-with-bounded-maximum.java
@@ -1,22 +1,41 @@
 class Solution {
-    public int numSubarrayBoundedMax(int[] A, int L, int R) {
-        int i=0;
-        int j=0;
-        int ans=0;
-        int smaller=0;
-        
-        while(i!=A.length){
-            if(A[i]>=L && A[i]<=R){
-                smaller=i-j+1;
-                ans+=smaller;
-            }else if(A[i]<L){
-                ans+=smaller;
-            }else{
-                j=i+1;
-                smaller=0;
+    public int numSubarrayBoundedMax(int[] nums, int left, int right) {
+        int leftPointer = 0, rightPointer = 0;
+        int n = nums.length;
+        int count = 0;
+        int number = 0;
+        while(leftPointer<n && rightPointer<n) {
+                if(nums[rightPointer]>=left && nums[rightPointer]<=right) {
+					/* variable "number" signifies the last element greater satisfying this condition. 
+					Why we are keeping this count is if a number less than left is there, then this number can be a part of that array.
+					Example: 
+					[79,69,33,52,68,5,78] , left = 32, right = 80
+					 for number 5 present in 5th index of the array, the possible combinations will include 
+					 [79,69,33,52,68,5],
+					 [69,33,52,68,5],
+					 [33,52,68,5],
+					 [52,68,5]
+					 [68,5]
+					 
+					 if you see clearly here, the number of combinations that include 5 are the same as the number 
+					 of combinations that are possible with 68( the last element till 5 which is greater than 32 and less than 80).
+					 To keep track of this count we have stored this in variable "number".
+					*/
+				
+                    count+=(rightPointer-leftPointer+1);
+                    number = (rightPointer-leftPointer+1); 
+                } else if(nums[rightPointer]<left) {count+=number;}
+             else {
+                if(nums[rightPointer]>right) {           
+					// reset everything if the array element is greater than right.
+                    rightPointer++;  
+                    leftPointer = rightPointer;
+                    number = 0;
+                    continue;
+                }
             }
-            i++;
+            rightPointer++;
         }
-        return ans;
+        return count;
     }
 }
