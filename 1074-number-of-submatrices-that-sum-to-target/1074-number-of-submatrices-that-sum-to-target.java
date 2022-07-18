4806a0d393caa62f0003028514a25038dd48f9d8
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-07-18 09:49:01 +0530

    Time: 282 ms (41.44%), Space: 118.1 MB (7.44%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.java
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.java
@@ -1,21 +1,36 @@
 class Solution {
-    public int numSubmatrixSumTarget(int[][] M, int T) {
-        int xlen = M[0].length, ylen = M.length, ans = 0;
-        Map<Integer, Integer> res = new HashMap<>();
-        for (int[] r : M)
-            for (int j = 1; j < xlen; j++)
-                r[j] += r[j-1];
-        for (int j = 0; j < xlen; j++)
-            for (int k = j; k < xlen; k++) {
-                res.clear();
-                res.put(0,1);
-                int csum = 0;
-                for (int i = 0; i < ylen; i++) {
-                    csum += M[i][k] - (j > 0 ? M[i][j-1] : 0);
-                    ans += res.getOrDefault(csum - T, 0);
-                    res.put(csum, res.getOrDefault(csum, 0) + 1);
+    public int numSubmatrixSumTarget(int[][] matrix, int target) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int res = 0;
+        //int[][] sum = new int[row][col];
+        
+        for(int i = 0; i < row; i++){
+            for(int j = 1; j < col; j++){
+                //sum[i][j] =sum[i][j-1] + matrix[i][j-1];
+                matrix[i][j] += matrix[i][j-1];
+            }
+        }  
+        for(int start = 0; start < col; start++){
+            for(int end = start; end < col; end++){
+                int subMatrixSum = 0;
+                
+                Map<Integer, Integer> countElm = new HashMap<Integer, Integer>();
+                countElm.put(0,1);
+                
+                for(int k = 0; k < row; k++){
+                    //subMatrixSum += sum[k][end] - sum[k][start];
+                    int prefixSum = start == 0 ? 0:matrix[k][start-1];
+                    subMatrixSum += matrix[k][end] - prefixSum;
+                    
+                    if(countElm.containsKey(subMatrixSum - target))
+                        res += countElm.get(subMatrixSum - target);
+                    
+                    int r = countElm.getOrDefault(subMatrixSum, 0);
+                    countElm.put(subMatrixSum, r+1);
                 }
             }
-        return ans;
+        }
+        return res;
     }
 }
