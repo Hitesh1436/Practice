1f64e069ce825e7472d0d6d7c4a789505afef3e4
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-07-18 09:51:02 +0530

    Time: 162 ms (92.80%), Space: 44 MB (82.38%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.java
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.java
@@ -1,36 +1,23 @@
 class Solution {
     public int numSubmatrixSumTarget(int[][] matrix, int target) {
-        int row = matrix.length;
-        int col = matrix[0].length;
-        int res = 0;
-        //int[][] sum = new int[row][col];
-        
-        for(int i = 0; i < row; i++){
-            for(int j = 1; j < col; j++){
-                //sum[i][j] =sum[i][j-1] + matrix[i][j-1];
-                matrix[i][j] += matrix[i][j-1];
-            }
-        }  
-        for(int start = 0; start < col; start++){
-            for(int end = start; end < col; end++){
-                int subMatrixSum = 0;
-                
-                Map<Integer, Integer> countElm = new HashMap<Integer, Integer>();
-                countElm.put(0,1);
-                
-                for(int k = 0; k < row; k++){
-                    //subMatrixSum += sum[k][end] - sum[k][start];
-                    int prefixSum = start == 0 ? 0:matrix[k][start-1];
-                    subMatrixSum += matrix[k][end] - prefixSum;
-                    
-                    if(countElm.containsKey(subMatrixSum - target))
-                        res += countElm.get(subMatrixSum - target);
-                    
-                    int r = countElm.getOrDefault(subMatrixSum, 0);
-                    countElm.put(subMatrixSum, r+1);
+        int ans = 0;
+
+        for(int i = 0; i<matrix[0].length;i++){//left col
+            int[] sum = new int[matrix.length];
+            for(int m = i; m<matrix[0].length;m++){//right col
+                for(int j = 0; j<matrix.length;j++){
+                    sum[j]+=matrix[j][m];//add left to right
+                    if(sum[j]==target)ans++;            
+                }
+                for(int j = 0; j<matrix.length;j++){//up row
+                    int summe=0;    
+                    for(int n = j; n<matrix.length;n++){//down row
+                        summe+=sum[n];  //add up to down
+                        if(n!=j&&summe==target)ans++;
+                    }
                 }
             }
         }
-        return res;
+        return ans;
     }
 }
