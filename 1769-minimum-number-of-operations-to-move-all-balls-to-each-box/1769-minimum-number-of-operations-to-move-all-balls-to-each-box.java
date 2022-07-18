9ba5cc848ee58e366e2d3d2e161778c6a5a69e3d
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-07-18 10:14:08 +0530

    Time: 3 ms (95.29%), Space: 43.1 MB (82.57%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -1,16 +1,29 @@
 class Solution {
     public int[] minOperations(String boxes) {
-       int n = boxes.length();
-        int []ans = new int[n];
-        for(int i=0;i<n;i++){
-            int cost =0;
-            for(int j=0;j<n;j++){
-                if(boxes.charAt(j)=='1'){
-                    cost += Math.abs(i-j);
-                }
-            }
-            ans[i] = cost;
+        if(boxes == null || boxes.length()==0) return new int[]{};
+        int n = boxes.length();
+        
+        int[] left = new int[n];
+        int[] right = new int[n];
+        int[] result = new int[n];
+        
+        // Left to Right
+        int balls = boxes.charAt(0) == '1' ? 1 : 0;
+        for(int i=1;i<n;i++) {
+            left[i] = left[i-1] + 1 * balls;
+            balls += boxes.charAt(i) == '1' ? 1 : 0;
         }
-        return ans;
+        
+        // Right to Left
+        balls = boxes.charAt(n-1) == '1' ? 1 : 0;
+        for(int i=n-2;i>=0;i--) {
+            right[i] = right[i+1] + 1 * balls;
+            balls += boxes.charAt(i) == '1' ? 1 : 0;
+        }
+        
+        for(int i=0;i<n;i++) {
+            result[i] = left[i] + right[i];
+        }
+        return result;
     }
 }
