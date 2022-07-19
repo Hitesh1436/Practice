fbd3db00627517bf30ffb2816c13d4bbb55badfe
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-07-19 09:23:39 +0530

    Time: 8 ms (66.36%), Space: 53.6 MB (70.25%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.java
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.java
@@ -1,24 +1,30 @@
 class Solution {
-    public List<List<Integer>> groupThePeople(int[] gs) {
-        List<List<Integer>> ans = new ArrayList<>();
-        for(int i=0;i<gs.length;i++){
-            if(gs[i] !=0){
-            List<Integer> sub = new ArrayList<>();
-            int size = gs[i];
-            int num = size;
-            for(int j = i;j<gs.length;j++){
-                if(size == 0){
-                    break;
+    public List<List<Integer>> groupThePeople(int[] groupSizes) {
+        List<List<Integer>> res = new ArrayList();
+        HashMap<Integer, List<Integer>> hm = new HashMap();
+		// Approach is to create the Integer - List hashmap since we have size of group of current "i" and we have to return the list of lists.
+		// We simply add index I to the List corresponding to the given size and if it get filled we add the current completed list to the final answer and empties it.
+       
+	   for(int i = 0; i < groupSizes.length; i++){
+            if(hm.containsKey(groupSizes[i])){                 
+                if(hm.get(groupSizes[i]).size() < groupSizes[i]){
+                    hm.get(groupSizes[i]).add(i);
                 }
-                if(gs[j] == num){
-                    size--;
-                    sub.add(j);
-                    gs[j] = 0;
-                }
+                else{
+                    res.add(hm.get(groupSizes[i]));
+                    hm.put(groupSizes[i], new ArrayList());
+                    hm.get(groupSizes[i]).add(i);
+                } 
             }
-            ans.add(sub);
+            else{
+                List<Integer> first = new ArrayList();
+                first.add(i);
+                hm.put(groupSizes[i], first);
+            }
         }
-      }
-        return ans;
+        for(int key : hm.keySet()){
+            res.add(hm.get(key));
+        }
+        return res;
     }
 }
