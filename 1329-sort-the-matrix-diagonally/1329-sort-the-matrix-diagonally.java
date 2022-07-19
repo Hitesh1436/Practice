cf5d4fcfceb94420fda17201adb4752ce9bb8693
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-07-19 23:02:27 +0530

    Time: 32 ms (5.51%), Space: 50.2 MB (20.65%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
@@ -1,20 +1,29 @@
 class Solution {
-    public int[][] diagonalSort(int[][] mat) {
-        int m = mat.length;
-        int n = mat[0].length;
-        HashMap<Integer,PriorityQueue<Integer>> map = new HashMap<>();
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                PriorityQueue<Integer> pq = map.getOrDefault(i-j,new PriorityQueue<>());
-                pq.add(mat[i][j]);
-                map.put(i-j,pq);
+  public int[][] diagonalSort(int[][] mat) {
+    Map<Integer, List<Integer>> map = new HashMap<Integer, List<Integer>>();      //initialize the hashMap
+    
+    for(int i=0;i<mat.length;i++){
+        for(int j=0;j<mat[i].length;j++){
+            int k = i-j;                                       //get the index of the hashmap where to store this index value
+            /*
+			every diagonal will have the unique value when doing (row-col)
+			this will help to uniquically indentify the diagonal
+			*/
+            if(map.get(k)!=null){
+                map.get(k).add(mat[i][j]);                        //if index K is present then insert the value and
+                Collections.sort(map.get(k));                     //then sort the list
+            }else{
+                List<Integer> list = new ArrayList<Integer>();   //if index k is not present then 
+                list.add(mat[i][j]);                             //add element in the list and
+                map.put(k,list);                                 //then create the new value in the map
             }
         }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                mat[i][j] = map.get(i-j).remove();
-            }
-        }
-        return mat;
     }
+    for(int i=0;i<mat.length;i++){
+        for(int j=0;j<mat[i].length;j++){
+             mat[i][j] = (map.get(i-j)).remove(0);      //now retireve the value last value by removing the 0th index everytime
+        }
+    }
+    return mat;  
+  }
 }
