898281cbe5981cd64ec29e06b9ea939ff2986269
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-07-20 22:41:16 +0530

    Time: 6 ms (6.06%), Space: 58 MB (30.20%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.java
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -1,17 +1,16 @@
-// Approach 2: O(N) time + O(1) space
+// Approach 3: O(N) time + O(1) space, single loop
 
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
         int[] result = new int[n];
-        result[0] = 1;
-        for (int i = 1; i < n; i++)
-            result[i] = result[i - 1] * nums[i - 1];
-        
-        int right = nums[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            result[i] = result[i] * right;
-            right *= nums[i];
+        Arrays.fill(result, 1);
+        int left = 1, right = 1;
+        for (int i = 0; i < n; i++) {
+            result[i] *= left;
+            left *= nums[i];
+            result[n - i - 1] *= right;
+            right *= nums[n - i - 1];
         }
         return result;
     }
