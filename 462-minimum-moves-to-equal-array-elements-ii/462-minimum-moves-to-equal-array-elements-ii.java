ce02ca4091d52dc4ee01d16b41cad594397f3241
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-07-20 21:32:57 +0530

    Time: 91 ms (6.54%), Space: 47.6 MB (6.63%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
@@ -1,28 +1,32 @@
-/*Time Complexity: O(nlogn)
-Step 1: Sort the array
-Step 2: Find the middle element 
-Step 3: Keep adding the difference between middle element and other array elements to count.
-Since we have to give the minimum count of moves required to make all elements equal, we sort and choose the middle element and keep adding for count of moves for every element to become equal to middle element,i.e., difference between the gives the number of moves.
-
-eg: [1,10,2,9]
-Step 1: [1,2,9,10]
-Step 2: middle=9
-Step 3: count+=Math.abs(9-1)=8
-        count+=Math.abs(9-2)=8+7=15
-        count+=Math.abs(9-9)=15+0=15
-        count+=Math.abs(9-10)=15+1=16
-Ans=16
-*/
-
-
 class Solution {
     public int minMoves2(int[] nums) {
-        Arrays.sort(nums);                              //Step1
-        int count=0;
-        int middle=nums[nums.length/2];                 //Step2
-        for(int i=0;i<nums.length;i++){                  //Step3
-            count+=Math.abs(middle-nums[i]);
-        }
-        return count;
+        int operations = 0, mid = quickSelect(nums, 0, nums.length-1, nums.length/2);
+        for (int num: nums) operations += Math.abs(mid - num);
+        return operations;
+    } 
+    private int quickSelect(int[] nums, int left, int right, int k) {
+        if (left == right) return nums[left];
+
+        int pIndex = new Random().nextInt(right - left + 1) + left;
+        pIndex = partition(nums, left, right, pIndex);
+
+        if (pIndex == k) return nums[k];
+        else if (pIndex < k) return quickSelect(nums, pIndex+1, right, k);
+        return quickSelect(nums, left, pIndex-1, k);
+    }
+    private int partition(int[] nums, int left, int right, int pIndex) {
+        int pivot = nums[pIndex];
+        swap(nums, pIndex, right);
+        pIndex = left;
+
+        for (int i=left; i<=right; i++) 
+            if (nums[i] <= pivot) swap(nums, i, pIndex++);
+
+        return pIndex - 1;
+    }
+    private void swap(int[] nums, int x, int y) {
+        int temp = nums[x];
+        nums[x] = nums[y];
+        nums[y] = temp;
     }
 }
