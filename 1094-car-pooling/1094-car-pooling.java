3f56abc73ba8ed04d739175a0b96d7ee69c2616c
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-07-21 09:48:50 +0530

    Time: 15 ms (8.83%), Space: 45.6 MB (13.13%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.java
+++ 1094-car-pooling/1094-car-pooling.java
@@ -1,21 +1,19 @@
+// T(C) -> O(nlogn)
 class Solution {
     public boolean carPooling(int[][] trips, int capacity) {
-        // Because from and to is between 0 and 1000. Idea is to store counts in an array of size 1001.
-        int lengthOfTrip[] = new int[1001];
-        for (int trip[] : trips){
-            lengthOfTrip[trip[1]] += trip[0]; // Increment when passenger is on board
-            lengthOfTrip[trip[2]] -= trip[0]; // decrement when they depart
+        Map<Integer, Integer> m = new TreeMap<>();
+        for (int[] v: trips){
+            int num_passengers = v[0];
+            int start = v[1];
+            int end = v[2];
+            m.put(start, m.getOrDefault(start, 0) + num_passengers); // baith rha h toh add kia pass
+            m.put(end, m.getOrDefault(end, 0) - num_passengers);// utrega rha h toh minus kia pass
         }
-		// Count total passenger for each bus top
-        int carLoad = 0;
-        // we have the count array, we perform a line sweep from 0 to 1000 and track its total
-        for (int i = 0; i < 1001; i++){
-            carLoad += lengthOfTrip[i];
-			// Reject when the car is overloaded somewhere
-            if(carLoad > capacity){
-                 return false;
-            }
+        int count=0;
+        for (int c: m.values()){
+            count+=c;
+            if(count>capacity) return false;
         }
-        return true; // Accept only if all trip is safe
+        return true;
     }
 }
