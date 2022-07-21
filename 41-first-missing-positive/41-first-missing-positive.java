70b34c252e6ed6e9d2bd1e7bca6bd4ef34a9d1ca
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-07-21 21:45:17 +0530

    Time: 2 ms (99.75%), Space: 99.2 MB (96.25%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.java
+++ 41-first-missing-positive/41-first-missing-positive.java
@@ -1,19 +1,23 @@
 class Solution {
-    public int firstMissingPositive(int[] A) {
+    public int firstMissingPositive(int[] nums) {
         int i = 0;
-        while(i < A.length){
-            if(A[i] == i+1 || A[i] <= 0 || A[i] > A.length) i++;
-            else if(A[A[i]-1] != A[i]) swap(A, i, A[i]-1);
-            else i++;
+        while( i < nums.length){
+            if(nums[i] > 0 && nums[i] <= nums.length && nums[i] != nums[nums[i] - 1]){
+                swap(nums,i,nums[i] - 1);
+            }else{
+                i++;
+            }
         }
-        i = 0;
-        while(i < A.length && A[i] == i+1) i++;
-        return i+1;
+        for(i = 0;i<nums.length;i++){
+            if(nums[i] != i + 1){
+                return i + 1;
+            }
+        }
+        return nums.length + 1;
     }
-    
-    private void swap(int[] A, int i, int j){
-        int temp = A[i];
-        A[i] = A[j];
-        A[j] = temp;
+    private void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
