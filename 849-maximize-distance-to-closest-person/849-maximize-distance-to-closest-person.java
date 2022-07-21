7c0855ec14cbbf3eb42f77853bfe566b3f6b76ae
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-07-21 16:15:44 +0530

    Time: 3 ms (70.40%), Space: 49.1 MB (60.06%) - LeetHub

--- 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.java
+++ 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.java
@@ -5,15 +5,16 @@ class Solution {
         for(int i = 0; i < seats.length; i++){
             if(seats[i] == 1){
                 int ans = 0;
-                if(j < 0){
+                if(j < 0){  // mtlb phli baar 1 mila h 
                     ans = i;
                 } else {
-                    ans = (i - j) / 2;
+                    ans = (i - j) / 2; // mtlb 2 baar milgya toh dono ke index minus /2 hoga
                 }
                 res = Math.max(res, ans);
                 j = i;
             }
         }
+        // yh ab last mn 1 ke baad 0 hn toh uske liye
         int ans = seats.length - 1 - j;
         res = Math.max(res, ans);
         return res;
