18c7459a150af99dc99344324728c17819e4c3fa
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-07-21 15:36:45 +0530

    Time: 2 ms (30.44%), Space: 40.4 MB (80.37%) - LeetHub

--- 670-maximum-swap/670-maximum-swap.java
+++ 670-maximum-swap/670-maximum-swap.java
@@ -1,13 +1,14 @@
 class Solution {
     public int maximumSwap(int num) {
-        char[] arr = (num + "").toCharArray();
+        char[] arr = (num + "").toCharArray();// number to char array conversion
         int[] right = new int[arr.length];
         for(int i = arr.length - 1; i >= 0; i--){
             if(i == arr.length - 1){
-                right[i] = -1;
-            } else if(i == arr.length - 2){
+                right[i] = -1;  // agr sbse bdi value last mn h toh uske right mn koi ni h toh -1 daldia right[i]pr
+            } else if(i == arr.length - 2){ // second last pr h toh last vle ka idx dldo
                 right[i] = arr.length - 1;
             } else {
+    // bki phle h toh usse next value compare krenge arr ke right ke i+1 idx vale se
                 if(arr[i + 1] > arr[right[i + 1]]){
                     right[i] = i + 1;
                 } else {
