8321fd5c6bb188adbfe59a5af79125c93788031e
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-07-22 15:16:49 +0530

    Time: 4 ms (70.57%), Space: 128.1 MB (33.24%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.java
+++ 41-first-missing-positive/41-first-missing-positive.java
@@ -1,24 +1,27 @@
 class Solution {
-    public int firstMissingPositive(int[] nums){
+    public int firstMissingPositive(int[] nums) {
         int n = nums.length;
-        int i = 0;
-        while( i < n){
-            if(nums[i] > 0 && nums[i] <= n && nums[i] != nums[nums[i] - 1]){
-                swap(nums,i,nums[i] - 1);
-            }else{
-                i++;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] <= 0 || nums[i] > n){
+                nums[i] = n + 1;
             }
         }
-        for(i = 0;i<n;i++){
-            if(nums[i] != i + 1){
+        for(int i = 0; i < nums.length; i++){
+            int val = Math.abs(nums[i]);
+            
+            if(val <= n){
+                int idx = val - 1;
+                
+                if(nums[idx] > 0){
+                    nums[idx] = -1 * nums[idx];
+                }
+            }
+        }
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] > 0){
                 return i + 1;
             }
         }
         return n + 1;
-    }
-    private void swap(int[] nums, int i, int j){
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
     }
 }
