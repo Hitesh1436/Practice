41348fd262e33dc2c9a3a3e5f94cd5c85c6d098e
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-07-22 15:36:20 +0530

    Time: 10 ms (29.76%), Space: 45 MB (38.46%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.java
+++ 1094-car-pooling/1094-car-pooling.java
@@ -1,32 +1,41 @@
 class Solution {
-    public boolean carPooling(int[][] trips, int capacity) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        ArrayList<Integer> stops = new ArrayList<>(); 
-        for(int[] trip: trips){
-            int people = trip[0];
-            int from   = trip[1];
-            int to     = trip[2];
-            if(map.containsKey(from) == false){
-                map.put(from, +people);
-                stops.add(from);
+    class Pair implements Comparable<Pair> {
+        int time;
+        int delta;
+        Pair(int time, int delta){
+            this.time = time;
+            this.delta = delta;
+        }
+        public int compareTo(Pair o){
+            if(this.time != o.time){
+                return this.time - o.time;
             } else {
-                map.put(from, map.get(from) + people);
-            }
-            if(map.containsKey(to) == false){
-                map.put(to, -people);
-                stops.add(to);
-            } else {
-                map.put(to, map.get(to) - people);
+                if(this.delta < 0){
+                    return -1;
+                } else if(o.delta < 0){
+                    return +1;
+                } else {
+                    return 0;
+                }
             }
         }
-        Collections.sort(stops);
-        int pic = 0;
-        for(int stop: stops){
-            int delta = map.get(stop);
-            pic += delta;
+    }
+    public boolean carPooling(int[][] trips, int capacity) {
+        int[] map = new int[1001];
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        for(int[] trip: trips){
+            pq.add(new Pair(trip[1], +trip[0]));
+            pq.add(new Pair(trip[2], -trip[0]));
+        }
+        int pic = 0;  // pic-> passenger in car
+        while(pq.size() > 0){
+            Pair rem = pq.remove();
+            pic += rem.delta;
             
             if(pic > capacity){
                 return false;
+            } else if(pic < 0){
+                pic = 0;
             }
         }
         return true;
