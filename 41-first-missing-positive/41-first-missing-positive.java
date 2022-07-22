7e135c673883de0e30e9c9c24b9a331eab0b404a
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-07-22 15:33:00 +0530

    Time: 3 ms (92.14%), Space: 126.5 MB (70.30%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.java
+++ 41-first-missing-positive/41-first-missing-positive.java
@@ -1,11 +1,13 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
         int n = nums.length;
+        // jhn val -ve h ya length se bdi h vhn usse ek bdi value daldi
         for(int i = 0; i < nums.length; i++){
             if(nums[i] <= 0 || nums[i] > n){
                 nums[i] = n + 1;
             }
         }
+// agr value ns hoti ya brabr h toh val -1 pr jyuga and usse -ve krdunga
         for(int i = 0; i < nums.length; i++){
             int val = Math.abs(nums[i]);
             
@@ -17,6 +19,7 @@ class Solution {
                 }
             }
         }
+    //jo phle +ve number milega uske idx nikalo +1 krke answer miljyga hume 
         for(int i = 0; i < nums.length; i++){
             if(nums[i] > 0){
                 return i + 1;
