566a486a750b078e28a38f0c5bf88f3df18bb95b
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-07-22 12:17:31 +0530

    Time: 2 ms (95.24%), Space: 47.9 MB (59.95%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
@@ -1,24 +1,40 @@
 class Solution {
-    public int[][] diagonalSort(int[][] mat) {
-    HashMap<Integer,PriorityQueue<Integer>> map = new HashMap<>();
-        int m = mat.length;
-        int n = mat[0].length;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                int gap = i-j;
-                
-                if(map.containsKey(gap)==false){
-                    map.put(gap,new PriorityQueue<>());
-                }
-                map.get(gap).add(mat[i][j]);
-            }
-        }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                int gap = i-j;
-               mat[i][j] = map.get(gap).remove();
-            }
-        }
-        return mat;
+    public int[][] diagonalSort(int[][] mat) {	
+    int m = mat.length;		
+    int n = mat[0].length;		
+	//start from top right to top left and sort each diagonal
+    for(int i = n-1;i>=0;i--){
+        sort(0,i,m,n,mat);
     }
+	//then start from top left to bottom left and sort each diagonal
+    for(int i = 1;i<m;i++){
+        sort(i,0,m,n,mat);
+    }
+    return mat;
+}
+void sort(int x, int y, int rows, int cols, int[][] mat){
+	//count sort implementation
+    int[] arr = new int[101];
+    int s= x,e= y;
+	//store the value of each diagonal element in arr
+    while(x < rows && y < cols){
+        arr[mat[x][y]]++;
+        x++;
+        y++;
+    }
+    //write sorted value to each diagonal element
+    for(int i = 0;i<arr.length;){
+        if(arr[i] <= 0){
+            i++;
+            continue;
+        }
+        if(arr[i] > 1){
+            mat[s++][e++] = i;      
+            arr[i]--;
+        }
+        else{
+            mat[s++][e++] = i++;
+        }  
+    } 
+  }
 }
