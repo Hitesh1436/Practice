0763685d86ed2431f6d84ee6011a6dd9bef11f16
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-07-25 22:58:27 +0530

    Time: 4 ms (10.62%), Space: 40.8 MB (59.12%) - LeetHub

--- 754-reach-a-number/754-reach-a-number.java
+++ 754-reach-a-number/754-reach-a-number.java
@@ -1,12 +1,19 @@
 class Solution {
-    public int reachNumber(int tar) {
-        int moves =0;
-        int sum =0;
-        tar = Math.abs(tar);
-     while(sum<tar || (sum-tar)%2!=0) {
-        moves++;
-        sum+=moves;
-    }
-    return moves;
+    public int reachNumber(int target) {
+        int sum = 0, jump = 1;
+		// to make every target positive
+        target = Math.abs(target);
+        while(sum < target){
+            sum += jump;
+            jump++;
+        }
+        
+        if((sum - target)%2 == 0){
+            return jump-1;
+        } 
+        else if((sum+jump-target)%2 == 0){
+            return jump;
+        } 
+        else    return jump+1;
     }
 }
