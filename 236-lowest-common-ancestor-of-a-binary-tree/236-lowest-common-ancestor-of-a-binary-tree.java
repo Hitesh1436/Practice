208ad7a868ad15a5eca913016a2f8c9d601e4443
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-07-26 09:26:31 +0530

    Time: 9 ms (66.53%), Space: 48.1 MB (16.08%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -1,49 +1,24 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 class Solution {
+    TreeNode ans=null;
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        ArrayList<TreeNode> pN2R = nodeToRootPath(root,p.val);  // pN2R -> p ka node to root path
-        ArrayList<TreeNode> qN2R = nodeToRootPath(root,q.val); // qN2R -> q ka node to root path
+        helper(root,p,q);
+        return this.ans;
+    }
+    
+    public TreeNode helper(TreeNode node,TreeNode p, TreeNode q){
+        if(node==null){
+            return null;
+        }
         
-        int i = pN2R.size()-1;
-        int j = qN2R.size()-1;
-        TreeNode lca = null;
-        while(i>=0 && j>=0){
-            if(pN2R.get(i).val != qN2R.get(j).val){
-                break;
-            }
-            lca = pN2R.get(i);
-            i--;
-            j--;
+        TreeNode leftNode=helper(node.left,p,q);
+        TreeNode rightNode=helper(node.right,p,q);
+        if(((node==p || node==q)&&(leftNode!=null || rightNode!=null))||(leftNode!=null &&rightNode!=null)){
+            this.ans=node;
+            return node;
+        }else if((leftNode!=null || rightNode!=null)||(node==p || node==q)){
+            return node;
+        }else{
+            return null;
         }
-        return lca;
     }
-    private ArrayList<TreeNode> nodeToRootPath(TreeNode root,int data){
-        if(root == null){
-            return new ArrayList<>();
-        }
-        if(root.val == data){
-            ArrayList<TreeNode> base = new ArrayList<>();
-            base.add(root);
-            return base;
-        }
-        ArrayList<TreeNode> leftAns = nodeToRootPath(root.left,data);
-        if(leftAns.size()>0){
-            leftAns.add(root);
-            return leftAns;
-        }
-        ArrayList<TreeNode> rightAns = nodeToRootPath(root.right,data);
-        if(rightAns.size()>0){
-            rightAns.add(root);
-            return rightAns;
-    }
-        return new ArrayList<>();
-   }
 }
