a95afe76d47b0ef6ead71844fc0f78a1d57734b0
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-07-26 14:35:44 +0530

    Time: 5 ms (78.48%), Space: 42.8 MB (53.31%) - LeetHub

--- 763-partition-labels/763-partition-labels.java
+++ 763-partition-labels/763-partition-labels.java
@@ -1,21 +1,23 @@
 class Solution {
     public List<Integer> partitionLabels(String s) {
-        HashMap<Character,Integer> map = new HashMap<>();
-        for(int i=0;i<s.length();i++){
+        int[] map = new int[26];
+        Arrays.fill(map, -1);
+        for(int i = 0; i < s.length(); i++){
             char ch = s.charAt(i);
-            map.put(ch,i);
+            map[ch - 'a'] = i;  // idx ke smne char konsa h store krdia esa krke yh rhega jse a akhri bar kis idx pr aaya 
         }
-        List<Integer> ans = new ArrayList<>();
-        int prev = -1;
-        int max = 0;
-        for(int i=0;i<s.length();i++){
+        int start = 0; 
+        int end = 0; 
+        ArrayList<Integer> res = new ArrayList<>();
+        for(int i = 0; i < s.length(); i++){
             char ch = s.charAt(i);
-            max = Math.max(max,map.get(ch));
-            if(max == i){
-                ans.add(max - prev);
-                prev = max;
+            end = Math.max(end, map[ch - 'a']);  // end nikalajse a 8th tk jyga and so on
+            if(i == end){  // jse hi start 8th pr aya jhn end tha
+                int len = end - start + 1;  // len nikalli vhn tk ki 
+                res.add(len);
+                end = start = i + 1;  // end start ek agy idx pr dlkr firse vhi kaam kro
             }
         }
-        return ans;
+        return res;
     }
 }
