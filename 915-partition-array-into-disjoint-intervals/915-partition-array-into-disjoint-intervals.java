4bf7e5279074b7003997358677f2b909eda469ae
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-07-26 15:07:40 +0530

    Time: 4 ms (72.57%), Space: 96.8 MB (40.76%) - LeetHub

--- 915-partition-array-into-disjoint-intervals/915-partition-array-into-disjoint-intervals.java
+++ 915-partition-array-into-disjoint-intervals/915-partition-array-into-disjoint-intervals.java
@@ -1,18 +1,17 @@
+// T(C) -> O(n) S(C) -> O(1)
 class Solution {
     public int partitionDisjoint(int[] nums) {
-        int pi = 0;
-        int mtp = nums[0];
-        int mts = nums[0];
+        int pi = 0;         // pi -> partition index jo alag krega dono ko 
+        int mtp = nums[0];  // mtp -> maximum till partition
+        int mts = nums[0];  // mts -> maximum till scanned
         
         for(int i = 1; i < nums.length; i++){
-            mts = Math.max(nums[i], mts);
-            
-            if(nums[i] < mtp){
-                pi = i;
-                mtp = mts;
+            mts = Math.max(nums[i], mts);  // abtk max aya vhn mts ayga harr baar
+            if(nums[i] < mtp){ 
+                pi = i;  // pi chnge hota rhega agr less val ati rhi 
+                mtp = mts;  // pi change hua toh mts and mtp bhi same krdenge
             }
         }
-        
         return pi + 1;
     }
 }
