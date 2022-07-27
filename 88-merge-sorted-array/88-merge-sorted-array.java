f6ca6bae88bcfa156013e6021f00801e248bbc2d
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-07-27 11:27:30 +0530

    Time: 0 ms (100.00%), Space: 43 MB (29.70%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -1,16 +1,18 @@
+// Initially p1 = m-1 , p2 = n-1 , i = m+n-1
+// So lets takes two pointers that mark the end of 2 arrays and i ,that would be the index at which we are going to write the value , now we will compare the values at p1 and p2 and write the value that is larger at index i , after writing we decrement the respective indexes .
+// And the another edge case is that if any one of the array is empty arr[i] = [0] , index i-1 and another one is arr[j] = [1,2,3] , index = j-1
+// So if we consider the index as i-1 then it becomes negative , which is ArrayIndexOutOfBounds ,so we have to take index >=0 .
+
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-       int i = m-1,j=n-1,k=(m+n)-1;
-        while(i>=0 && j>=0){
-            if(nums1[i]<nums2[j]){
-                nums1[k--]= nums2[j--];
-            }else{
-                nums1[k--] = nums1[i--];
+        int p1 = m-1 , p2 = n-1 ,i = m+n-1;
+        while(p2 >=0 ){
+            if(p1 >=0 && nums1[p1] > nums2[p2]){
+                nums1[i--] = nums1[p1--];
+            } 
+            else{
+                nums1[i--] = nums2[p2--];
             }
         }
-         //If length of nums2 is more than length of nums1
-        while(j>=0){
-            nums1[k--] = nums2[j--];
-        }
-    }
+     }
 }
