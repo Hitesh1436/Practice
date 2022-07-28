808acef6a1ae071914ad7350476c16f2ca40cb1e
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-07-28 09:54:25 +0530

    Time: 4 ms (85.70%), Space: 44 MB (49.16%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,15 +1,17 @@
 class Solution {
-    public boolean isAnagram(String s, String t) {
-        int[] charCount = new int[128];
-        for(char c : s.toCharArray()){
-            charCount[c]++;
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
         }
-        int[] charCountT = new int[128];
-        for(char c : t.toCharArray()){
-            charCountT[c]++;
+        int len = s.length();
+        int[] sCnt = new int[26];
+        int[] tCnt = new int[26];
+        for (int i = 0; i < len; i++) {
+            sCnt[s.charAt(i) - 'a']++;
+            tCnt[t.charAt(i) - 'a']++;
         }
-        for(int i=0; i<128; i++){
-            if(charCount[i] != charCountT[i]){
+        for (int i = 0; i < 26; i++) {
+            if (sCnt[i] != tCnt[i]) {
                 return false;
             }
         }
