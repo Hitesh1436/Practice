b2ccd1b6b6c200a350f40987ae9f16129c19a29d
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-07-30 17:00:53 +0530

    Time: 35 ms (59.76%), Space: 90 MB (43.50%) - LeetHub

--- 916-word-subsets/916-word-subsets.java
+++ 916-word-subsets/916-word-subsets.java
@@ -1,27 +1,29 @@
-// T.C-> O(N^2) , S.>CO(N)
 class Solution {
     public List<String> wordSubsets(String[] words1, String[] words2) {
-        List<String> al=new ArrayList<>();
-        int target[]=new int[26];
-        for(String b:words2){
-            int temp[]=new int[26];
-            for(char ch:b.toCharArray()){
-                temp[ch-'a']++;
-                target[ch-'a']=Math.max(target[ch-'a'],temp[ch-'a']);
+        List<String> ans =new ArrayList<>();
+        int target[] = new int[26];
+        for(String w:words2){
+            int temp[] = new int[26];
+            for(char c:w.toCharArray()){
+                temp[c-'a']++;
+                target[c-'a'] = Math.max(target[c-'a'] , temp[c-'a']);
             }
         }
-        for(String word:words1){
-            int freq[]=new int[26];
-            for(char ch:word.toCharArray()) freq[ch-'a']++;
-            if(isSubset(freq,target)){
-                al.add(word);
+        for(String w:words1){
+            int temp[] = new int[26];
+            for(char c:w.toCharArray()){
+                temp[c-'a']++;
+                
             }
-        }
-        return al;
+            if(subset(temp,target))ans.add(w);
+        }              
+        return ans;
     }
-    static boolean isSubset(int freq[],int target[]){
-        for(int i=0;i<26;i++){
-            if(freq[i]<target[i]) return false;
+    private boolean subset(int[] source , int[] dest){
+        for(int i = 0 ; i<26 ; i++){
+            if(dest[i]>source[i]){
+                return false;
+            }
         }
         return true;
     }
