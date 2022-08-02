dc36747d43b6b3d8c04f90dbc26f00fec351a13d
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-08-02 15:25:30 +0530

    Time: 196 ms (33.21%), Space: 83.7 MB (70.37%) - LeetHub

--- 853-car-fleet/853-car-fleet.java
+++ 853-car-fleet/853-car-fleet.java
@@ -1,21 +1,31 @@
 class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
-        TreeMap<Integer,Integer> map = new TreeMap<>(Collections.reverseOrder());
+        PriorityQueue<Car> pq = new PriorityQueue<>();
         for(int i=0;i<position.length;i++){
-            map.put(position[i],speed[i]);
+            Car c = new Car(position[i],speed[i]);
+            pq.add(c);
         }
-        double time =0;
         int groups =0;
-        for(Map.Entry<Integer,Integer>car : map.entrySet()){
-            int pos = car.getKey();  // pos -> position
-            int sp = car.getValue(); // sp -> speed
-            int dist = target - pos;
-            double currTime = dist * 1.0 / sp;
-            if(currTime > time){
-                time = currTime;
+        double time =0;
+        while(pq.size()>0){
+            Car rem = pq.remove();  // cTime -> current time 
+            double cTime = (target -rem.pos)*1.0 /rem.speed;
+            if(cTime > time){
+                time = cTime;
                 groups++;
             }
         }
         return groups;
+    }
+    class Car implements Comparable<Car>{
+        int pos;
+        int speed;
+        Car(int pos,int speed){
+            this.pos = pos;
+            this.speed = speed;
+        }
+        public int compareTo(Car c){
+            return c.pos - this.pos;
+        }
     }
 }
