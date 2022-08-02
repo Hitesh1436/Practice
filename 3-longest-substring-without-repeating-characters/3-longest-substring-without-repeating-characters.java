5573b2baccd329a4236c2d9985194ca2a9bdd09b
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-08-02 21:36:59 +0530

    Time: 10 ms (69.88%), Space: 47.2 MB (18.49%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -1,50 +1,16 @@
-// Using Sliding Window Concept
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        if (s == null) {
-            throw new IllegalArgumentException("Input string is null");
-        }
-
-        int len = s.length();
-        if (len <= 1) {
-            return len;
-        }
-
-        HashMap<Character, Integer> map = new HashMap<>();
-        int start = 0;
-        int maxLen = 0;
-
-        for (int end = 0; end < len; end++) {
-            char eChar = s.charAt(end);
-            if (map.containsKey(eChar)) {
-                start = Math.max(start, map.get(eChar) + 1);
+        int max = 0;
+        StringBuilder nonRepeated = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char t = s.charAt(i);
+            int matchIndex = nonRepeated.indexOf(Character.toString(t));
+            if (matchIndex !=-1) {
+                max = Math.max(max, nonRepeated.length());
+                nonRepeated.delete(0,matchIndex+1);
             }
-            map.put(eChar, end);
-            maxLen = Math.max(maxLen, end - start + 1);
+            nonRepeated.append(t);
         }
-        return maxLen;
+        return Math.max(max, nonRepeated.length());
     }
 }
-
-//  Using Basic String Functions
-
-// class Solution {
-//     public int lengthOfLongestSubstring(String s) {
-//         if(s.length()==0) return 0;
-//         char ch = s.charAt(0);
-//         String ans = "";
-//         ans += ch;
-//         int max =1;
-//         for(int i=1;i<s.length();i++){
-//             ch = s.charAt(i);
-//             int idx = ans.indexOf(ch);
-//             if(idx == -1){
-//                 ans += ch;
-//                 max = Math.max(max,ans.length());
-//             }else{
-//                 ans = ans.substring(idx+1)+ ch;
-//             }
-//         }
-//         return max;
-//     }
-// }
