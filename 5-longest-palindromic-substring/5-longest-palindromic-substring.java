8c66044d75c74fd956b56db825bb2d590cc51619
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-08-03 09:14:37 +0530

    Time: 4 ms (99.79%), Space: 42 MB (97.09%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,26 +1,46 @@
 class Solution {
     public String longestPalindrome(String s) {
-        char[] charArray = s.toCharArray();
-        int start = 0;
+        // code line +8 to line +15
+        int strLen = 2 * s.length() + 3;
+        char[] sChars = new char[strLen];
+        /*
+         * To ignore special cases at the beginning and end of the array
+         * "abc" -> @ # a # b # c # $
+         * "" -> @#$
+         * "a" -> @ # a # $
+         */
+        sChars[0] = '@';
+        sChars[strLen - 1] = '$';
+        int t = 1;
+        for (char c : s.toCharArray()) {
+            sChars[t++] = '#';
+            sChars[t++] = c;
+        }
+        sChars[t] = '#';
         int maxLen = 0;
-        for (int i = 0; i < charArray.length; i++) {
-            for (int len = 0; i + len < charArray.length; len++) {
-                if (isPalindrome(charArray, i, len) && len + 1 > maxLen) {
-                    maxLen = len + 1;
-                    start = i;
-                }
+        int start = 0;
+        int maxRight = 0;
+        int center = 0;
+        int[] p = new int[strLen]; // i's radius, which not includes i
+        for (int i = 1; i < strLen - 1; i++) {
+            if (i < maxRight) {
+                p[i] = Math.min(maxRight - i, p[2 * center - i]);
+            }
+            // expand center
+            while (sChars[i + p[i] + 1] == sChars[i - p[i] - 1]) {
+                p[i]++;
+            }
+            // update center and its bound
+            if (i + p[i] > maxRight) {
+                center = i;
+                maxRight = i + p[i];
+            }
+            // update ans
+            if (p[i] > maxLen) {
+                start = (i - p[i] - 1) / 2;
+                maxLen = p[i];
             }
         }
         return s.substring(start, start + maxLen);
-    }
-    public boolean isPalindrome(char[] charArray, int start, int len) {
-        while (len > 0) {
-            if (charArray[start] != charArray[start + len]) {
-                return false;
-            }
-            start++;
-            len -= 2;
-        }
-        return true;
     }
 }
