a7f86801f16657475164ec33db6c2e073cf98d5d
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-08-03 21:54:39 +0530

    Time: 319 ms (56.07%), Space: 117.1 MB (15.25%) - LeetHub

--- 686-repeated-string-match/686-repeated-string-match.java
+++ 686-repeated-string-match/686-repeated-string-match.java
@@ -1,21 +1,15 @@
+/*
+n: length of a, m: length of b , k = n / m ~= constant 
+TC: O(nm * k) ~= O(nm), each iteration StringBuilder content size will grow at most n size but it will repeat constant times. 
+Then we can asuume time complexity will be O(nm)
+SC: O(kn) ~= O(n)
+*/
 class Solution {
-    public int repeatedStringMatch(String A, String B) {
-    // create string builder for storing A
-    StringBuilder sb = new StringBuilder(A);
-    int i=1;
-// Add in A until it is less then B
-    while(sb.length()<B.length()){
-        i++;
-        sb.append(A);
+    public int repeatedStringMatch(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i <= b.length() / a.length() + 2; i++) //starting 1 and ending with maximum 2 repeations is enough 
+            if(sb.append(a).toString().contains(b))
+                return i;
+        return -1;
     }
-// now check after adding in A b is substring of A or not   
-    if(sb.indexOf(B)!=-1){
-        return i;
-    }
- //  else add one more time   
-    sb.append(A);
-    i++;
- // check after appended B is substring of A or not if yes then      return i else return -1;
-    return sb.indexOf(B)!=-1?i:-1;
-  }
 }
