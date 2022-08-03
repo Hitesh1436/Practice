e145f23a5484b8cccf34728afc34e5c191273d4f
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-08-03 21:58:49 +0530

    Time: 10 ms (95.43%), Space: 43.1 MB (57.97%) - LeetHub

--- 686-repeated-string-match/686-repeated-string-match.java
+++ 686-repeated-string-match/686-repeated-string-match.java
@@ -1,15 +1,45 @@
-/*
-n: length of a, m: length of b , k = n / m ~= constant 
-TC: O(nm * k) ~= O(nm), each iteration StringBuilder content size will grow at most n size but it will repeat constant times. 
-Then we can asuume time complexity will be O(nm)
-SC: O(kn) ~= O(n)
-*/
 class Solution {
-    public int repeatedStringMatch(String a, String b) {
-        StringBuilder sb = new StringBuilder();
-        for(int i = 1; i <= b.length() / a.length() + 2; i++) //starting 1 and ending with maximum 2 repeations is enough 
-            if(sb.append(a).toString().contains(b))
-                return i;
+    private int kmp(String t, String p) {
+        int m = p.length();
+        int n = t.length();
+        int[] table = new int[m + 1];
+        table[0] = -1;
+        int i = 0, k = -1;
+        for(i = 1; i <= m; ++i) {
+            while(k >= 0 && p.charAt(k) != p.charAt(i - 1)) {
+                k = table[k];
+            }
+            table[i] = ++k;
+        }
+        k = 0;
+        for(i = 1; i <= n; ++i) {
+            while(k >= 0 && p.charAt(k) != t.charAt(i - 1)) {
+                k = table[k];
+            }
+            ++k;
+            if(k == m) {
+                return i-m;
+            }
+        }
         return -1;
+    }
+    public int repeatedStringMatch(String a, String b) {
+        int m = a.length();
+        int n = b.length();
+        int ans = (n-1)/m;
+        StringBuilder sb = new StringBuilder(ans + 2);
+        sb.append(a);
+        sb.append(a);
+        for(int i = 0; i < ans; ++i) {
+            sb.append(a);
+        }
+        int x = kmp(sb.toString(), b); // x denoted starting of the match
+        if(x == -1)
+            return -1;
+        if(x != 0)
+            ++ans;
+        if((x+n-1) >= (ans)*m)
+            ++ans;
+        return ans;
     }
 }
