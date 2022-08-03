208091ce032f00b54a6295cc42a9c74e46f67c4a
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-08-03 09:49:37 +0530

    Time: 11 ms (45.05%), Space: 46.8 MB (33.45%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.java
+++ 13-roman-to-integer/13-roman-to-integer.java
@@ -1,23 +1,22 @@
-// Time: O(n)O(n)
-// Space: O(128) = O(1)O(128)=O(1)
 class Solution {
-  public int romanToInt(String s) {
-    int ans = 0;
-    int[] roman = new int[128];
-    roman['I'] = 1;
-    roman['V'] = 5;
-    roman['X'] = 10;
-    roman['L'] = 50;
-    roman['C'] = 100;
-    roman['D'] = 500;
-    roman['M'] = 1000;
-    for (int i = 0; i + 1 < s.length(); ++i)
-      if (roman[s.charAt(i)] < roman[s.charAt(i + 1)]){
-          ans -= roman[s.charAt(i)];
-      }
-      else{
-          ans += roman[s.charAt(i)];
-      }
-    return ans + roman[s.charAt(s.length() - 1)];
-   }
+    public int romanToInt(String s) {
+        HashMap<Character, Integer> map= new HashMap<>();
+        map.put('I',1);
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
+        int sum=0;
+        for (int i=0;i<s.length();i++){
+            if(i<s.length()-1 && map.get(s.charAt(i))<map.get(s.charAt(i+1))){
+                sum+=map.get(s.charAt(i+1))-map.get(s.charAt(i));
+                i++;
+            }else{
+                sum+=map.get(s.charAt(i));
+            }
+        }
+    return sum;
+    }
 }
