dab265a421d0393a62019e456d4f8ae997a21374
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-08-03 08:44:01 +0530

    Time: 21 ms (29.09%), Space: 54 MB (27.89%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
@@ -1,18 +1,31 @@
+// Time Complexity : O(n)
+// Space Complexity : O(1)
 class Solution {
     public String reverseWords(String s) {
-        s = s.trim();//triming all the extra spaces in front or back of the string!
-        String str[] = s.split("\\s+");//Splitting String where words are separated by regular whitespace or multiple whitespaces or tabs!
-        Stack<String> st = new Stack<>();
-        for(String sus: str){
-            st.push(sus);//pushsing all string inside the stack ==> as stack is filo so it will reverse the order of the string!
+         int i = 0; // i will be looking for non-space
+         int n = s.length();
+        String result = new String();
+        while(i<n){
+            while(i<n && s.charAt(i) == ' '){
+                i++;
+            }
+            if(i>=n){
+                break;
+            }
+            int j = i+1; // j is looking for space
+            while(j<n && s.charAt(j) != ' '){
+                j++;
+            }
+            String sub = s.substring(i, j);
+            if(result.length() == 0){
+                result = sub;
+            }
+            else{
+                result = sub + " " + result;
+            }
+            i = j+1; 
         }
-        Stack<String> sus2 = new Stack<>();
-        String ans = "";//to store our ans!
-        while(!st.isEmpty()){
-            ans+=(st.pop());//adding all strings to our ans from our st stack!
-            ans+=" ";//ans simultaniously adding regular whitespaces
-        }
-        ans = ans.trim();//at last eliminating trailing space
-        return ans;
+        return result;
     }
 }
+
