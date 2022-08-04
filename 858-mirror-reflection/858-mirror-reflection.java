53e8aaa2fb8c832d41f517d9be9c8e88c2b17e6a
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-08-04 09:36:09 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (52.78%) - LeetHub

--- 858-mirror-reflection/858-mirror-reflection.java
+++ 858-mirror-reflection/858-mirror-reflection.java
@@ -1,9 +1,9 @@
 class Solution {
-     public int mirrorReflection(int p, int q) {
-        int m = 1, n = 1;
-        while(m * p != n * q){
-            n++;
-            m = n * q / p;
+    public int mirrorReflection(int p, int q) {
+        int m = q, n = p;
+        while(m % 2 == 0 && n % 2 == 0){
+            m /= 2;
+            n /= 2;
         }
         if (m % 2 == 0 && n % 2 == 1) return 0;
         if (m % 2 == 1 && n % 2 == 1) return 1;
