26eb8b6a0af1f12ee323e9fcefb85b8d43dcfb1b
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-08-05 09:52:04 +0530

    Time: 3 ms (36.49%), Space: 41.4 MB (61.50%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.java
+++ 377-combination-sum-iv/377-combination-sum-iv.java
@@ -1,21 +1,18 @@
+// 3 Dp - Tabulation Approach
 class Solution {
-    public int combinationSum4(int[] nums, int target) {
-        return permutations(nums,target,new Integer[target +1]);
-    }
-    int permutations(int []arr,int tar,Integer []dp){
-        if(tar == 0){
-            return 1;
+   public int combinationSum4(int[] nums, int target) {
+        int n = nums.length;
+        int dp[] = new int[target+1];
+        dp[0]=1;
+        for(int i=1;i<=target;i++){
+            int comb=0;
+            for(int j=0;j<nums.length;j++){
+                if(nums[j]<=i){
+                    comb+=dp[i-nums[j]];
+                }
+            }
+            dp[i]=comb;
         }
-        if(tar <0){
-            return 0;
-        }
-        if(dp[tar] != null){
-            return dp[tar];
-        }
-        int count =0;
-        for(int i=0;i<arr.length;i++){
-            count += permutations(arr,tar - arr[i],dp);
-        }
-        return dp[tar]= count;
+        return dp[target];
     }
 }
