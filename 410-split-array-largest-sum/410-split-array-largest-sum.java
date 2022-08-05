42f6c133d6cecd167dee70ab2f18da37561b856e
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-08-05 21:46:32 +0530

    Time: 1 ms (87.18%), Space: 42.3 MB (22.75%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.java
+++ 410-split-array-largest-sum/410-split-array-largest-sum.java
@@ -1,42 +1,38 @@
-//  Top-down recursion + memoization. ~O(N^2 * M) time
 class Solution {
     public int splitArray(int[] nums, int m) {
-        int[][] memo = new int[nums.length][m+1];
-        for (int i = 0; i < memo.length; i++) {
-            Arrays.fill(memo[i], -1);
+        int start=0;
+        int end=0;
+        for(int i=0;i<nums.length;i++){
+            start=Math.max(start,nums[i]);
+            end+=nums[i];//it keeps the max sum the array can have if only one piece i.e., m is allowed so our ans will lie in the range start to end as it indicates the min possible answer and max possible answer
+            
         }
-        return walk(nums, memo, 0, m);
-    }
-    private int walk(int[] nums, int[][] memo, int start, int rem) { 
-	    // base case
-        if (rem == 0 && start == nums.length) {
-            return 0;
-        }
-        if (rem == 0 || start == nums.length) {
-		    // if we reach the end and have not used up all patitions
-			// or have used up all partitions and have not reached the end,
-			// we do not want to count the current way of partitioning.
-			// Return MAX_VALUE so that we don't contribute to the return value.
-            return Integer.MAX_VALUE;
-        }
-        int n = nums.length;
-        int ret = Integer.MAX_VALUE;
-        int curSum = 0;
-        if (memo[start][rem] != -1) {
-            return memo[start][rem];
-        }
-		// try all positions to end the current partition.
-        for (int i = start; i < n; i++) {
-            curSum += nums[i];
-			// answer for partitioning the subarray to the right of the current partition,
-			// with one less partition number allowance, because we already used one
-			// for the current partition. i.e. (rem - 1).
-            int futureSum = walk(nums, memo, i + 1, rem - 1);
+//start keeps the max element in the array as if n numbers are there and m i.e pieces==n        then it is the ans
 
-            // we want to minimum of the largest sum of the partitions.
-            ret = Math.min(ret, Math.max(curSum, futureSum));
+        while(start<end){
+//since now we have taken range of min to max we applied binary search here to find the answer as it must be sorted
+            int mid=start+(end-start)/2;
+            int pieces=1;
+            int sum=0;
+            for(int num:nums){
+                if(sum+num>mid){
+                    sum=num;  
+//if sum exceeds mid then we must make other subarray for other elements so sum is reset to next element as it symbolize that another subarray is formed here
+                    pieces++;
+                }else{
+                    sum+=num;
+                }
+            }
+            if(pieces>m){   
+// if we see we have divided the array into more pieces than required it is obvious thet we can increase the number of element we take into the subarray to decrease pieces to m so we can expect a larger value than current mid as ans so we did start =mid+1
+                start=mid+1;
+            }
+            else{
+                end=mid;
+                //if we see subarrays formed are larger in size than required we must shorten it to achieve m so mid must be  greater than the ans expected here so we must decrese mid so end =mid
+            }
         }
-        memo[start][rem] = ret;
-        return ret;
+        return start;
+        //here mid==start==end==ans ;) so anything can be returned here
     }
 }
