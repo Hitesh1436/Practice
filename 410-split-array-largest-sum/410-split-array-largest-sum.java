80bd8ce7c6cf7c80b973a40a0d10fdab819004e7
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-08-05 21:45:01 +0530

    Time: 339 ms (5.02%), Space: 42.1 MB (32.59%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.java
+++ 410-split-array-largest-sum/410-split-array-largest-sum.java
@@ -1,53 +1,42 @@
+//  Top-down recursion + memoization. ~O(N^2 * M) time
 class Solution {
     public int splitArray(int[] nums, int m) {
-        if (nums == null || nums.length == 0 || m == 0 ) {
+        int[][] memo = new int[nums.length][m+1];
+        for (int i = 0; i < memo.length; i++) {
+            Arrays.fill(memo[i], -1);
+        }
+        return walk(nums, memo, 0, m);
+    }
+    private int walk(int[] nums, int[][] memo, int start, int rem) { 
+	    // base case
+        if (rem == 0 && start == nums.length) {
             return 0;
         }
-        int max = 0;
-        int sum = 0;
-		// the lower boundary will be max and upper bounder will be sum for the binary search
-        for ( int num : nums ) {
-            sum = sum + num;
-            max = Math.max(num, max);
+        if (rem == 0 || start == nums.length) {
+		    // if we reach the end and have not used up all patitions
+			// or have used up all partitions and have not reached the end,
+			// we do not want to count the current way of partitioning.
+			// Return MAX_VALUE so that we don't contribute to the return value.
+            return Integer.MAX_VALUE;
         }
-		// base checks where we do not need to apply binary search
-        if ( m == nums.length ) {
-            return max;
-        } else if ( m == 1 ) {
-            return sum;
-        } else {
-            int ans = 0;
-            int lo = max;
-            int hi = sum;
-            while ( lo <= hi ) {
-			    //remember that we are not using the array index here, do not use (lo+hi)/2
-                int mid = lo + ( hi-lo )/2;
-				//check if it is possible to form m subarrays with the given mid
-                if( isPossible(nums, mid, m) ) {   
-				    //if yes, store the answer and reduce the upper boundary
-                    ans = mid;
-                    hi = mid - 1; 
-                } else {
-				    //if no, increase the lower boundary to get a higher mid
-                    lo = mid + 1;
-                }
-            }
-            return ans;  
+        int n = nums.length;
+        int ret = Integer.MAX_VALUE;
+        int curSum = 0;
+        if (memo[start][rem] != -1) {
+            return memo[start][rem];
         }
-    }
-    public static boolean isPossible ( int[] nums, int mid, int m ) {
-        int sum = 0;
-        int requiredSubarrays = 1;
-        for ( int i=0; i < nums.length; i++ ) {
-            sum = sum+nums[i];
-            if ( sum > mid ) {
-                requiredSubarrays++;
-                sum = nums[i];
-            }
+		// try all positions to end the current partition.
+        for (int i = start; i < n; i++) {
+            curSum += nums[i];
+			// answer for partitioning the subarray to the right of the current partition,
+			// with one less partition number allowance, because we already used one
+			// for the current partition. i.e. (rem - 1).
+            int futureSum = walk(nums, memo, i + 1, rem - 1);
+
+            // we want to minimum of the largest sum of the partitions.
+            ret = Math.min(ret, Math.max(curSum, futureSum));
         }
-        if ( requiredSubarrays <= m ) {
-            return true;
-        }
-        return false;
+        memo[start][rem] = ret;
+        return ret;
     }
 }
