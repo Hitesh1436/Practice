76d8b31221c56c95cb96d0c6554e3d5730214a0a
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-08-13 22:13:34 +0530

    Time: 10 ms (99.20%), Space: 43.1 MB (91.15%) - LeetHub

--- 1392-longest-happy-prefix/1392-longest-happy-prefix.java
+++ 1392-longest-happy-prefix/1392-longest-happy-prefix.java
@@ -1,21 +1,22 @@
 class Solution {
     public String longestPrefix(String s) {
-        if (s.length() <= 1)
-            return "";
         int[] lps = new int[s.length()];
         int len = 0;
         int i = 1;
-        lps[0] = 0;
-        while (i < s.length()) {
-            if (s.charAt(i) == s.charAt(len)) {
-                lps[i++] = ++len;
-            } else if(len ==0){
-                lps[i] =0;
+        while(i < lps.length){
+            if(s.charAt(i) == s.charAt(len)){
+                lps[i] = len + 1;            
+                len++;
                 i++;
-            }else{
-                 len = lps[len - 1];
-                }
+            } else if(len == 0){
+                lps[i] = 0;
+                len = 0;
+                i++;
+            } else {
+                len = lps[len - 1];
             }
-        return s.substring(0, lps[lps.length-1]);
+        }
+        int k = lps[s.length() - 1];
+        return s.substring(0, k);
     }
 }
