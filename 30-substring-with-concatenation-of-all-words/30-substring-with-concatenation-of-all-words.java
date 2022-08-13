6d1523210653db99c39143e5639b4ba5396ddab5
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-08-13 09:40:15 +0530

    Time: 406 ms (22.48%), Space: 118.7 MB (10.83%) - LeetHub

--- 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.java
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.java
@@ -1,30 +1,46 @@
 class Solution {
     public List<Integer> findSubstring(String s, String[] words) {
-        final Map<String, Integer> counts = new HashMap<>();
-        for (final String word : words) {
-            counts.put(word, counts.getOrDefault(word, 0) + 1);
+       int n = words.length;
+        HashMap<String, Integer> map = new HashMap<>();
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            map.put(words[i],map.getOrDefault(words[i],0)+1);
+            //This map contains strings present in the array as a key and their occurrence as value.
+            //getOrDefault method is used here. If the string is already present, it will give its occurrence value else it will give 0 by default.
+            //We add 1 to whatever is returned by this function
+            /*
+            for words = {"bar","foo","the"}
+            map-->
+            key - "bar" value = 1;
+            key - "foo" value = 1;
+            key - "the" value = 1;
+             */
         }
-        final List<Integer> indexes = new ArrayList<>();
-        final int n = s.length(), num = words.length, len = words[0].length();
-        for (int i = 0; i < n - num * len + 1; i++) {
-            final Map<String, Integer> seen = new HashMap<>();
-            int j = 0;
-            while (j < num) {
-                final String word = s.substring(i + j * len, i + (j + 1) * len);
-                if (counts.containsKey(word)) {
-                    seen.put(word, seen.getOrDefault(word, 0) + 1);
-                    if (seen.get(word) > counts.getOrDefault(word, 0)) {
-                        break;
-                    }
-                } else {
-                    break;
+        int totalLength = words[0].length() * words.length;//This is the total length of the string that we have to check
+        //Length of "foobarthe" in this case
+
+        int subStrLength = words[0].length(); //== length of each element in the array that is to be checked in the string
+        int i = 0, j = 0;
+        while (j < s.length()) {
+            if (j - i + 1 == totalLength) {
+                String sub = s.substring(i, j + 1);//j+1 is important as the endIndex is exclusive for substring method
+                // It will give a string of length  = totalLength , ex barfoofoo
+                HashMap<String, Integer> map2 = new HashMap<>();//Another hashmap to check for the string
+                int index = 0;
+                while (index < sub.length()) {
+                    String temp = sub.substring(index, index + subStrLength);
+                    // It will give a string of length  = subStrLength , ex bar
+                    map2.put(temp,map2.getOrDefault(temp,0)+1);
+                    index += subStrLength;//Checking for the next substring by adding subStrLength
                 }
-                j++;
+                if (map.equals(map2)){
+                    //Adds the index if both the maps are equal
+                    ans.add(i);
+                }
+                i++;
             }
-            if (j == num) {
-                indexes.add(i);
-            }
+            j++;
         }
-        return indexes;
+        return ans;
     }
 }
