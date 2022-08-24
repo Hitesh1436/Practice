be3f029cdb2854a155a061306070ca1d02ebce20
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-08-24 09:28:14 +0530

    Time: 0 ms (100.00%), Space: 64.5 MB (39.55%) - LeetHub

--- 189-rotate-array/189-rotate-array.java
+++ 189-rotate-array/189-rotate-array.java
@@ -4,9 +4,9 @@ class Solution {
         if(k <0){
             k += nums.length;
         }
-        reverse(nums,0,nums.length -k-1);
-        reverse(nums,nums.length -k,nums.length -1);
-        reverse(nums,0,nums.length -1);
+        reverse(nums,0,nums.length -k-1);  // k se phle tk reverse
+        reverse(nums,nums.length -k,nums.length -1); // k se end tk reverse
+        reverse(nums,0,nums.length -1); // pura jo aaya usse reverse
     }
     public void reverse(int []nums,int i,int j){
         int li = i;
