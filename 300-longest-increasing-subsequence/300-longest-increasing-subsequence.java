1b15dc1c3e2ccc4c041c942fc211a710376665df
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-08-25 18:04:15 +0530

    Time: 4 ms (92.41%), Space: 44.3 MB (64.00%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,21 +1,21 @@
-// T(C) -> O(n^2)
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int []lis = new int[nums.length];
-        lis[0] =1;
-        for(int i=1;i<lis.length;i++){
-            int max =0;
-            for(int j=0;j<i;j++){
-                if(nums[j]<nums[i]){
-                    max = Math.max(max,lis[j]);
-                }
+        int[] tails = new int[nums.length];
+         int len = 1;  
+        tails[0] = nums[0];  
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] > tails[len - 1]){
+                len++;
+                tails[len - 1] = nums[i];
+            } else {
+                int idx = Arrays.binarySearch(tails, 0, len - 1, nums[i]);
+                if(idx < 0){
+                    idx = -idx;
+                    idx = idx - 1;
+                }  
+                tails[idx] = nums[i];
             }
-            lis[i] = max +1;
-        }
-        int len =0;
-        for(int val : lis){
-            len = Math.max(val,len);
-        }
+        }    
         return len;
     }
 }
