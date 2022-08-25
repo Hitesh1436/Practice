f5a86c820fbee6fa40094ebd8402e9f3803bcefa
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-08-25 12:20:14 +0530

    Time: 80 ms (44.61%), Space: 44.5 MB (49.87%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,19 +1,21 @@
+// T(C) -> O(n^2)
 class Solution {
     public int lengthOfLIS(int[] nums) {
-    int[] tails = new int[nums.length];
-    int size = 0;
-    for (int x : nums) {
-        int i = 0, j = size;
-        while (i != j) {
-            int m = (i + j) / 2;
-            if (tails[m] < x)
-                i = m + 1;
-            else
-                j = m;
+        int []lis = new int[nums.length];
+        lis[0] =1;
+        for(int i=1;i<lis.length;i++){
+            int max =0;
+            for(int j=0;j<i;j++){
+                if(nums[j]<nums[i]){
+                    max = Math.max(max,lis[j]);
+                }
+            }
+            lis[i] = max +1;
         }
-        tails[i] = x;
-        if (i == size) ++size;
+        int len =0;
+        for(int val : lis){
+            len = Math.max(val,len);
+        }
+        return len;
     }
-    return size;
-  }
 }
