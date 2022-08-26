5c67594f273f59698ec33a5c1911b170da547182
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-08-26 12:44:17 +0530

    Time: 8 ms (69.72%), Space: 47.3 MB (66.98%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.java
+++ 740-delete-and-earn/740-delete-and-earn.java
@@ -1,30 +1,18 @@
-
-// for example:
-	// After we sum up fpr each particular number, we can get
-	// sum1,  sum2,  sum3,  sum4, sum5...
-	//    1,     6,     3,     8,    10
-	
-	// if we reach to sum3,  prev1 = 6 , prev2=1
-	// (prev2+sum3)=4  <  prev1=6
-	// so new prev1 = 6,  new prev2 = prev1 =6
-    // if we reach to sum4, (prev2+sum4)=6+8=14 < prev1=6
-	// so new prev1 =14 , new prev2=prev1=6
-	// if we reahc to sum5, (prev2+sum5)=6+10=16 > prev1=14
-	// finally, new prev1=16 which is the largest sum
-	// the key is to compare previous sum(prev1) and the sum before prev1(prev2)+current value
 class Solution {
     public int deleteAndEarn(int[] nums) {
-        int []dp = new int[10001];
-        for(int num : nums){
-            dp[num] += num;
+        int include = 0;
+        int exclude = 0;
+        int [] count = new int[10001];
+        for(int val : nums){
+            count[val]++;
         }
-        int prev1 =0;
-        int prev2 =0;
-        for(int val : dp){
-            int temp = prev1;
-            prev1 = Math.max(prev2+val ,prev1);
-            prev2 = temp;
+        for(int i=0;i<10001;i++){
+            int newInclude = exclude + count[i] * i;
+            int newExclude = Math.max(include,exclude);
+            
+            include = newInclude;
+            exclude = newExclude;
         }
-        return prev1;
+        return Math.max(include,exclude);
     }
 }
