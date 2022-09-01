e74aa48ba664ce20b9158a5921fa5c29e7af1c4d
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-09-01 16:56:18 +0530

    Time: 3 ms (100.00%), Space: 50.3 MB (95.40%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,10 +1,10 @@
 class Solution {
     public int maxProfit(int[] prices, int fee) {
-        int obsp = -prices[0];  // obsp-> old bought stock price
-        int ossp = 0; //obsp-> old stock sell price
+        int obsp = -prices[0]; 
+        int ossp = 0;
         for(int i = 1; i < prices.length; i++){
-            int nbsp = Math.max(ossp - prices[i], obsp);  // new bsp
-            int nssp = Math.max(obsp + prices[i] - fee, ossp);  // new ssp
+            int nbsp = Math.max(ossp - prices[i], obsp);
+            int nssp = Math.max(obsp + prices[i] - fee, ossp);
             obsp = nbsp;
             ossp = nssp;
         }
