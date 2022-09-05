25627bae7c36307a6d0f4a5d8d802761a3bf30fa
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-09-05 21:07:21 +0530

    Time: 3 ms (55.59%), Space: 41.7 MB (82.60%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.java
+++ 844-backspace-string-compare/844-backspace-string-compare.java
@@ -1,36 +1,28 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        int i=s.length()-1, j=t.length()-1, count;
-        while(i>=0 || j>=0){
-            while(i>=0 && s.charAt(i) == '#'){
-                count = 0;
-                while(i >= 0){
-                    if(s.charAt(i) == '#')
-                        count++;
-                    else count--;
-                    if(count == 0) break;
-                    i--;
-                }
-                i--;
-            }
-            while(j>=0 && t.charAt(j) == '#'){
-                count = 0;
-                while(j >= 0){
-                    if(t.charAt(j) == '#')
-                        count++;
-                    else count--;
-                    if(count == 0) break;
-                    j--;
-                }
-                j--;
-            }
-            if(i<0 && j<0) return true;
-            else if((i>=0 && j<0) || (i<0 && j>=0) || (s.charAt(i) != t.charAt(j)))
+        Stack<Character> st1 = new Stack<>();
+        Stack<Character> st2 = new Stack<>();
+        for(Character c : s.toCharArray()){
+            if(c=='#'){
+                if(!st1.empty())
+                    st1.pop();
+            } else
+                st1.push(c);
+        }
+        for(Character c : t.toCharArray()){
+            if(c=='#'){
+                if(!st2.empty())
+                    st2.pop();
+            } else
+                st2.push(c);
+        }
+        while(!st1.empty()&&!st2.empty()){
+            if(st1.pop()!=st2.pop()){
                 return false;
-            else{
-                i--;
-                j--;
             }
+        }
+        if(!st1.empty()||!st2.empty()){
+            return false;
         }
         return true;
     }
