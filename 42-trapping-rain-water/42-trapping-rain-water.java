ddefc6904474396a6b4f53305f8a107a60d7c03b
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-09-07 09:20:45 +0530

    Time: 2 ms (59.73%), Space: 49.8 MB (8.98%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,5 +1,6 @@
 class Solution {
     public int trap(int[] height) {
+    // left se max nikala
         int[] left = new int[height.length];
         for(int i = 0; i < left.length; i++){
             if(i == 0){
@@ -8,6 +9,7 @@ class Solution {
                 left[i] = Math.max(height[i], left[i - 1]);
             }
         }
+        // right se max nikala
         int[] right = new int[height.length];
         for(int i = right.length - 1; i >= 0; i--){
             if(i == right.length - 1){
@@ -17,6 +19,7 @@ class Solution {
             }
         } 
         int val = 0;
+// left right mn se jiski jyda h usme se jo min hoga vhn tk bhrega
         for(int i = 0; i < height.length; i++){
             val += Math.min(left[i], right[i]) - height[i];
         }
