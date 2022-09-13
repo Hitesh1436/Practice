89ac084662372ea91cd78ea5062f594a0fcd9a2a
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-09-13 09:29:58 +0530

    Time: 20 ms (53.61%), Space: 127.8 MB (84.98%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.java
+++ 131-palindrome-partitioning/131-palindrome-partitioning.java
@@ -1,30 +1,49 @@
 class Solution {
-    public List<List<String>> partition(String s) {
-        List<List<String>> ans = new ArrayList<>();
-        List<String> sub = new ArrayList<>();
-        helper(0,s,sub,ans);
-        return ans;
-    }
-    void helper(int idx,String s,List<String>sub,List<List<String>>ans){
-        if(idx == s.length()){
-            ans.add(new ArrayList<>(sub));
-            return;
-        }
-        for(int i=idx;i<s.length();i++){
-            if(isPalindrome(s,idx,i) == true){
-                sub.add(s.substring(idx,i+1));
-                // System.out.println(ans);
-                helper(i+1,s,sub,ans);
-                sub.remove(sub.size()-1);
+    boolean[][] ip;
+    void IsPalindrome(String s){
+        ip = new boolean[s.length()][s.length()];
+        for(int g= 0; g < ip[0].length; g++){
+            for(int i = 0, j = g; j < ip[0].length; i++, j++){
+                if(g == 0){
+                    ip[i][j] = true;
+                } else if(g == 1){
+                    ip[i][j] = s.charAt(i) == s.charAt(j);
+                } else {
+                   ip[i][j] = s.charAt(i) == s.charAt(j) && ip[i + 1][j - 1] == true; 
+                }
             }
         }
     }
-    boolean isPalindrome(String s,int st,int end){
-        while(st <=end){
-            if(s.charAt(st++)!=s.charAt(end--)){
-                return false;
+     public List<List<String>> partition(String s) {
+        IsPalindrome(s);
+        List<List<String>>[] dp = new List[s.length()];
+        return helper(s, s.length() - 1, dp);
+    }
+    public List<List<String>> helper(String s, int j, List[] dp){
+        if(j < 0){
+            List<List<String>> bres = new ArrayList<>();
+            List<String> blist = new ArrayList<>();
+            bres.add(blist);
+            return bres;
+        } 
+        if(dp[j] != null){
+            return (List<List<String>>)dp[j];
+        }
+        List<List<String>> mres = new ArrayList<>();
+        
+        for(int i = j; i >= 0; i--){
+            if(ip[i][j] == true){
+                String palin = s.substring(i, j + 1);
+                
+                List<List<String>> rres = helper(s, i - 1, dp);
+                for(List<String> rlist: rres){
+                    ArrayList<String> mlist = new ArrayList<>(rlist);
+                    mlist.add(palin);
+                    mres.add(mlist);
+                }
             }
         }
-        return true;
+        dp[j] = mres;
+        return mres;
     }
-}
+  }
