288eec4de29c8531e6cd96766010b099fa8bc213
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-09-17 12:15:31 +0530

    Time: 11 ms (45.32%), Space: 42.9 MB (38.60%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.java
+++ 10-regular-expression-matching/10-regular-expression-matching.java
@@ -1,28 +1,39 @@
- class Solution {
+class Solution {
     public boolean isMatch(String s, String p) {
-        boolean[][] dp = new boolean[s.length()+1][p.length()+1];
-        dp[0][0] = true;
-        for (int j = 1; j<=p.length(); j++){
-            if(p.charAt(j-1) == '*' && dp[0][j-2]){
-                dp[0][j] = true;
+        boolean [][]dp = new boolean[s.length()+1][p.length()+1];
+        
+        int n = dp.length -1;
+        int m = dp[0].length -1;
+        for(int i=n;i>=0;i--){
+            for(int j=m;j>=0;j--){
+                if(i ==n && j==m){
+                    dp[i][j] = true;
+                }else if(j == m){
+                   dp[i][j] = false; 
+                }else if(i ==n){
+                    if(j+1 < p.length() && p.charAt(j+1)=='*'){
+                        dp[i][j] = dp[i][j+2];
+                    }else{
+                        dp[i][j] = false;
+                    }
+                }else{
+                    if(j+1 < p.length() && p.charAt(j+1)=='*'){
+                        if(s.charAt(i) == p.charAt(j) || p.charAt(j) == '.'){
+                            dp[i][j] = dp[i+1][j] || dp[i][j+2];  // phla equal ho tb hoskta h 
+                                                        // ya . * ho jo ..* bnkr ktega
+                        }else{
+                             dp[i][j] = dp[i][j+2];
+                        }  
+                    }else{
+                        if(s.charAt(i) == p.charAt(j) || p.charAt(j) == '.'){
+                            dp[i][j] = dp[i+1][j+1];  // diagonal pr dkhoo
+                        }else{
+                            dp[i][j] = false;
+                        }  
+                    }
+                }
             }
         }
-        for (int i = 1; i<=s.length(); i++){
-            for (int j = 1; j<=p.length(); j++){
-                if(p.charAt(j-1) == s.charAt(i-1) || p.charAt(j-1) == '.'){
-                    dp[i][j] = dp[i-1][j-1];
-                }
-                else if(p.charAt(j-1) == '*'){
-                    if(p.charAt(j-2) != s.charAt(i-1) && p.charAt(j-2)!='.'){
-                        dp[i][j] = dp[i][j-2];
-                    }
-                    else{
-                        dp[i][j] = (dp[i][j-2] || dp[i][j-1] || dp[i-1][j]);
-                    }
-                }
-            }
-        }
-        return dp[s.length()][p.length()];
-
+        return dp[0][0];
     }
 }
