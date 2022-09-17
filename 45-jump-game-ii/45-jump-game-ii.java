e2263271147b81324162728f7388ca2968314ddd
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-09-17 15:29:31 +0530

    Time: 1 ms (99.92%), Space: 42.9 MB (90.74%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -1,15 +1,24 @@
-// T(C)->O(n), S(C)-> O(n)
 class Solution {
     public int jump(int[] nums) {
-        int start = 0,end =0,farthest =0;
-        int jump =0;
-        for(int i=0;i<nums.length -1;i++){
-            farthest = Math.max(farthest,i + nums[i]);
-            if(i == end){
-                jump++;
-                end = farthest;
-            }
+        if(nums.length <= 1){
+            return 0;
         }
-        return jump;
+        int ans = 0;
+        int curr =0;
+        while(curr < nums.length){
+            if(curr + nums[curr] >= nums.length -1){
+                ans++;
+                break;
+            }
+            int inter = curr;  // inter -> intermediate
+            for(int j= curr+1;j<=curr + nums[curr] && j<nums.length ;j++){
+                if(j + nums[j] > inter + nums[inter]){
+                    inter = j;
+                }
+            }
+            curr = inter;
+            ans++;
+        }
+        return ans;
     }
 }
