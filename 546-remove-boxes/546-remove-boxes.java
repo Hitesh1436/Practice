2933fce782935a673b1f4b76682a98a380705731
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-09-20 15:49:28 +0530

    Time: 92 ms (69.23%), Space: 79.6 MB (59.89%) - LeetHub

--- 546-remove-boxes/546-remove-boxes.java
+++ 546-remove-boxes/546-remove-boxes.java
@@ -1,26 +1,31 @@
 class Solution {
     public int removeBoxes(int[] boxes) {
         int n = boxes.length;
-        int[][][] memo = new int[n][n][n];
-        return removeBoxesSub(boxes, 0, n - 1, 0, memo);
+        int[][][] dp = new int[n][n][n];
+        return helper(boxes, 0, n - 1, 0, dp);
     }
-    private int removeBoxesSub(int[] boxes, int i, int j, int k, int[][][] memo) {
-        if (i > j) return 0;
-        if (memo[i][j][k] > 0) return memo[i][j][k];
-    
-        int tmpi = i, tmpk = k;
-        while (i + 1 <= j && boxes[i + 1] == boxes[i]) {
-            i++;
-            k++;
+    public int helper(int[] boxes, int i, int j, int k, int[][][] dp){
+        if(i > j){
+            return 0;
         }
-        int res = (k + 1) * (k + 1) + removeBoxesSub(boxes, i + 1, j, 0, memo);
-
-        for(int m = i + 1; m <= j; m++) {
-            if (boxes[i] == boxes[m]) {
-                res = Math.max(res, removeBoxesSub(boxes, i + 1, m - 1, 0, memo) + removeBoxesSub(boxes, m, j, k + 1, memo));
+        if(dp[i][j][k] != 0){
+            return dp[i][j][k];
+        }
+        int ii = i;
+        int kk = k;
+        while(ii + 1 <= j && boxes[ii + 1] == boxes[ii]){
+            ii++;
+            kk++;
+        }
+        int res = (kk + 1) * (kk + 1) + helper(boxes, ii + 1, j, 0, dp);
+        for(int m = ii + 1; m <= j; m++){
+            if(boxes[m] == boxes[ii]){
+                int fact = helper(boxes, ii + 1, m - 1, 0, dp) + 
+                           helper(boxes, m, j, kk + 1, dp);
+                res = Math.max(res, fact);
             }
         }
-        memo[tmpi][j][tmpk] = res;
+        dp[i][j][k] = res;
         return res;
     }
 }
