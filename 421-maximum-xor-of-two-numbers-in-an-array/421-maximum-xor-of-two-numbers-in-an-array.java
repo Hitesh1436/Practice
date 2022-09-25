14b41f6a6d67b47a0b3051646f67b9f131c49c5c
Hitesh <86163830+Hitesh1436@users.noreply.github.com>
2022-09-25 09:16:13 +0530

    Time: 141 ms (78.61%), Space: 85 MB (53.55%) - LeetHub

--- 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.java
+++ 421-maximum-xor-of-two-numbers-in-an-array/421-maximum-xor-of-two-numbers-in-an-array.java
@@ -1,41 +1,39 @@
 class Solution {
-    class Trie {
-        Trie[] children;
-        public Trie() {
-            children = new Trie[2];
-        }
+    class Node{
+        Node []children = new Node[2];  // bcz ya toh 0 ya 1 ki side jaskte hn bas
     }
-     public int findMaximumXOR(int[] nums) {
-        if(nums == null || nums.length == 0) {
-            return 0;
-        }
-        // Init Trie.
-        Trie root = new Trie();
-        for(int num: nums) {
-            Trie curNode = root;
-            for(int i = 31; i >= 0; i --) {
-                int curBit = (num >>> i) & 1;
-                if(curNode.children[curBit] == null) {
-                    curNode.children[curBit] = new Trie();
+    public int findMaximumXOR(int[] nums) {
+        Node root = new Node();
+        for(int num : nums){
+            Node temp = root;
+            for(int i=30 ;i>=0 ;i--){
+// yh check krne ke kaam ayga ki ith bit on hai ya off
+// 1st bit krni  toh AND krdo this 01000 to that and check kr 1 ayga toh on , 0 toh off
+                int bit = (num & (1 << i)) == 0 ? 0 : 1;
+                if(temp.children[bit] == null){
+                    temp.children[bit] = new Node();
                 }
-                curNode = curNode.children[curBit];
+                temp = temp.children[bit];
             }
         }
-        int max = Integer.MIN_VALUE;
-        for(int num: nums) {
-            Trie curNode = root;
-            int curSum = 0;
-            for(int i = 31; i >= 0; i --) {
-                int curBit = (num >>> i) & 1;
-                if(curNode.children[curBit ^ 1] != null) {
-                    curSum += (1 << i);
-                    curNode = curNode.children[curBit ^ 1];
-                }else {
-                    curNode = curNode.children[curBit];
+        int res = 0;
+        for(int num : nums){
+            int max_xor = 0;
+            Node temp = root;
+            
+            for(int i=30 ;i>=0 ;i--){
+                int bit = (num & (1 << i)) == 0 ? 0 : 1;
+                int cbit = Math.abs(bit -1); // cbit -> complimentary bit
+                
+                if(temp.children[cbit] != null){
+                    temp = temp.children[cbit];
+                    max_xor = max_xor | (1 << i);
+                }else{
+                    temp = temp.children[bit];
                 }
             }
-            max = Math.max(curSum, max);
+            res = Math.max(res,max_xor);
         }
-        return max;
+        return res;
     }
 }
